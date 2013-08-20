901ead84c4083e1a67a8aeb713c92ae3e8f32f49
RobVanMieghem <rob@awingu.com>
2013-08-20 14:14:28 +0200

    Added script for jenkins to reinstall a server using nocps

---
+++ ComputeBox/test/nocps.py
@@ -0,0 +1,34 @@
+import xmlrpclib
+import time
+
+def installFreshComputeNode(nocps_url):
+    print 'Installing Ubuntu 13.04 on the compute node'
+    
+    ps = xmlrpclib.ServerProxy(nocps_url)
+    
+    serverdetails = {
+                     'adminuser': 'cs',
+                     'hostname': 'cloudscale02',
+                     'mac': '00:e0:81:b2:89:f9',    #Physical machine identifier
+                     'profile': 403,                #Ubuntu 13.04
+                     'rebootmethod': 'auto',
+                     'rootpassword': 'R00t3r',
+                     'rootpassword2': 'R00t3r',
+                     'userpassword': 'cs',
+                     'userpassword2': 'cs'}
+    
+    provisioning_result = ps.PXE_API.provisionHost(serverdetails)
+    if not provisioning_result['success']:
+        raise provisioning_result
+    
+    while ps.PXE_API.getProvisioningStatusByServer('00:e0:81:b2:89:f9'):
+        print '.',
+        time.sleep(5)
+    
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u','--url',required=True, help='Url of nocps xmlrpc server like https://user:password@nocps.incubaid.com/xmlrpc.php')
+    args = parser.parse_args()
+    
+    installFreshComputeNode(args.url)
