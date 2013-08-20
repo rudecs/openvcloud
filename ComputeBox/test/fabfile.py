dd02d70426bff4f573fb7c9bd6e572ac169b350c
RobVanMieghem <rob@awingu.com>
2013-08-20 17:41:47 +0200

    A freshly installed ubuntu does not have a package index yet

--- ComputeBox/test/fabfile.py
+++ ComputeBox/test/fabfile.py
@@ -1,6 +1,7 @@
 from fabric.api import run
 
 def install_jumpscale_core():
+    run('apt-get update')
     run('apt-get install python-pip -y')
     run('pip install https://bitbucket.org/jumpscale/jumpscale_core/get/default.zip')
     
