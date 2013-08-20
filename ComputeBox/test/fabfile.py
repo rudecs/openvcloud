8ed6cabf0d644e841c7e37b7e49c4735bb61ae73
RobVanMieghem <rob@awingu.com>
2013-08-20 17:37:06 +0200

    First install pip

--- ComputeBox/test/fabfile.py
+++ ComputeBox/test/fabfile.py
@@ -1,5 +1,6 @@
 from fabric.api import run
 
 def install_jumpscale_core():
+    run('apt-get install python-pip -y')
     run('pip install https://bitbucket.org/jumpscale/jumpscale_core/get/default.zip')
     
