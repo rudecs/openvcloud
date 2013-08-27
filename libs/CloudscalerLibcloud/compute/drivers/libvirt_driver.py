cc32a3368e1d8b5c6a9b5ccefed4de381335315c
Hendrik Demolder <hendrik@awingu.com>
2013-08-27 18:48:20 +0200

    Initial commit for libcloud integration with libvirt and the cloudbroker, this is needed because we wan't to use the libcloud sizes and images. Both are not supported for libvirt. The usage is completly the same and normally the lib can be used seamless with libcloud. The interface is fully compatible. This is just the basic framework.TODO: * Add Calls to the cloudbroker to request the correct size and image data(now hardcoded), * integrate create * integrate correct connection management

---
+++ libs/CloudscalerLibcloud/compute/drivers/libvirt_driver.py
@@ -0,0 +1,107 @@
+#Add extra specific cloudscaler functions for libvirt libcloud driver
+
+from libcloud.compute.drivers.libvirt_driver import LibvirtNodeDriver
+from libcloud.compute.base import NodeImage, NodeSize
+
+
+class CSLibvirtNodeDriver(LibvirtNodeDriver):
+
+    CPUMAPPING = {1: 1700, 2: 3600, 3: 7200}
+
+
+    def list_sizes(self, location=None):
+        """
+        Libvirt doesn't has a idea of sizes, because of this we are using
+        the cloudscalers internal sizes api.
+        At this moment location is always None and can be neglected
+        @param location: Optional location, not used at the moment
+        @type: C{str}
+        @rtype: C{list} of L{NodeSize}
+        """
+        sizes = [{'CU': 2, 'disks': 40, 'guid':
+            '4da91a0d-18f5-47a5-ad97-7cf3b97cbc59', 'id': 1, 'name': u'BIG',
+            'referenceId': ''},{'CU': 1, 'disks': 20, 'guid':
+            '4da91a0d-18f5-47a5-ad97-7cf3b97cbc59', 'id': 2, 'name': u'SMALL',
+            'referenceId': ''}]
+
+        return [self._to_size(size) for size in sizes]
+
+    def _to_size(self, size):
+        return NodeSize(
+                id = size['id'],
+                name = size['name'],
+                ram = self.CPUMAPPING[size['CU']],
+                bandwidth = 0,
+                price = 0,
+                driver = self,
+                disk = size['disks'])
+
+
+
+    def list_images(self, location=None):
+        """
+        Libvirt doesn't has a idea of images, because of this we are using
+        the cloudscalers internal images api.
+        At this moment location is always None and can be neglected
+        @param location: Optional location, not used at the moment
+        @type: C{str}
+        @rtype: C{list} of L{NodeImage}
+        """
+        images = [{'UNCPath': 'file:///ISOS/ubuntu-2.iso', 
+            'description': '',
+            'guid': '3c655a10-7e04-4d93-8ea1-ec5536d9689b',
+            'id': 1,
+            'name': 'ubuntu-2',
+            'referenceId': '',
+            'size': 10,
+            'type': 'ubuntu'}, {'UNCPath': 'file:///ISOS/windows-2008.iso', 
+            'description': '',
+            'guid': '3c655a10-7e04-4d93-8ea1-ec5536d9689b',
+            'id': 2,
+            'name': 'windows-2008',
+            'referenceId': '',
+            'size': 20,
+            'type': 'WINDOWS'}]
+
+        return [self._to_image(image) for image in images]
+
+    def _to_image(self, image):
+        return NodeImage(
+                id = image['id'],
+                name = image['name'],
+                    driver = self,
+                extra = {'path': image['UNCPath'],
+                    'size': image['size'],
+                    'imagetype': image['type']}
+                )
+
+    def create_node(self, name, size, image, location, auth):
+        """
+        Creation in libcloud is based on sizes and images, libvirt has no
+        knowledge of sizes and images.
+        This create_node is specially build to create machines based on extra
+        data from the cloudscaler broker.
+
+        @keyword    name:   String with a name for this new node (required)
+        @type       name:   C{str}
+
+        @keyword    size:   The size of resources allocated to this node.
+                        (required)
+        @type       size:   L{NodeSize}
+   
+        @keyword    image:  OS Image to boot on node. (required)
+        @type       image:  L{NodeImage}
+
+        @keyword    location: Which data center to create a node in. If empty,
+                             undefined behavoir will be selected. (optional)
+        @type       location: L{NodeLocation}
+
+        @keyword    auth:   Initial authentication information for the node
+                           (optional)
+        @type       auth:   L{NodeAuthSSHKey} or L{NodeAuthPassword}
+
+        @return: The newly created node.
+        @rtype: L{Node}
+        """
+
+        return None 
