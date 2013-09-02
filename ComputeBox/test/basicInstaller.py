23b0f15a089601ad85dba651b99d497e6a96ae5a
Reem Khamis <khamisr@incubaid.com>
2013-09-02 18:05:14 +0200

    Get ISO from fileserver

--- ComputeBox/test/basicInstaller.py
+++ ComputeBox/test/basicInstaller.py
@@ -1,13 +1,12 @@
 from fabric.api import run, put
 
 def install_prereqs():
-    run('apt-get install python2.7 dialog nginx curl mc ssh mercurial python-gevent python-simplejson python-numpy byobu python-apt ipython python-pip python-imaging python-requests python-paramiko gcc g++ python-dev python-zmq msgpack-python python-mhash python-libvirt -y')
+    run('apt-get install python2.7 dialog nginx curl mc ssh mercurial python-gevent python-simplejson python-numpy byobu python-apt ipython python-pip python-imaging python-requests python-paramiko gcc g++ python-dev python-zmq msgpack-python python-mhash python-libvirt wget -y')
     run('yes w | pip install urllib3 ujson blosc pycrypto pylzma')
     run('apt-get update')
     run('apt-get install mercurial ssh python2.7 python-apt openssl ca-certificates -y')
     run('mkdir -p /home/ISO')
-    run('echo "ALL : 10.101.190.1 : allow" >> /etc/hosts.allow')
-    run('echo "R00t3r" | scp -o StrictHostKeyChecking=no root@10.101.190.1:/home/ISO/ubuntu-13.04-server-amd64.iso /home/ISO/')
+    run('wget -P /home/ISO/ http://files.incubaid.com/iaas/ubuntu-13.04-server-amd64.iso')
     put('libvirt_no_sparse.patch', '/usr/share/pyshared/')
     put('raring.patch', '/tmp/')
     run('python /opt/jumpscale/shellcmds/jpackage_install --package installEnv')
