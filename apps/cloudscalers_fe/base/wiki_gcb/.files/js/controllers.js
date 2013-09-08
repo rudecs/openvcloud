4b48e0b7d6c42706dc22b7bc22fd92b9ead3da50
mtayseer <devnull@localhost>
2013-09-08 07:36:31 -0400

    Doing some of Arvid's comments

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers.js
@@ -9,6 +9,17 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
 
     .controller('BucketListCtrl', ['$scope', 'Buckets', function($scope, Buckets) {
         $scope.buckets = Buckets.getAll();
+        
+        $scope.numOfDataLocations = function(bucket) {
+            var numOfLocations = 0;
+            for (var i = 0; i < bucket.region.length; i++) {
+                if (bucket.region[i]) {
+                    numOfLocations++;
+                }
+            }
+            return numOfLocations;
+        };
+
         $scope.$watch('buckets', function() {
             for (var i = 0; i < $scope.buckets.length; i++) {
                 try {
@@ -33,7 +44,7 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
             memory: 1,
             storage: 10,
             
-            region: '',
+            region: [false, false, false],
             status: 'Running',
             image: '',
             history: []
@@ -56,95 +67,20 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
         };
     }])
 
-    .controller('BucketEditCtrl', ['$scope', '$routeParams', '$location', 'Buckets', 'Snapshots', function($scope, $routeParams, $location, Buckets, Snapshots) {
+    .controller('BucketEditCtrl', ['$scope', '$routeParams', '$location', 'Buckets', 'Snapshots', '$timeout', function($scope, $routeParams, $location, Buckets, Snapshots, $timeout) {
         $scope.bucket = Buckets.getById(parseFloat($routeParams.bucketId));
         $scope.snapshots = Snapshots.getAll();
         $scope.newSnapshot = {name: '', date: getFormattedDate()};
         $scope.cloneName = '';
         $scope.selectedSnapshot = '';
 
-        $scope.bucket.chart = {
-            chart: {
-                type: 'gauge',
-                plotBackgroundColor: null,
-                plotBackgroundImage: null,
-                plotBorderWidth: 0,
-                plotShadow: false
-            },
-            title: {
-                text: 'CPU load'
-            },
-            pane: {
-                startAngle: -150,
-                endAngle: 150,
-                background: [{
-                    backgroundColor: {
-                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                        stops: [
-                            [0, '#FFF'],
-                            [1, '#333']
-                        ]
-                    },
-                    borderWidth: 0,
-                    outerRadius: '109%'
-                }, {
-                    backgroundColor: {
-                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                        stops: [
-                            [0, '#333'],
-                            [1, '#FFF']
-                        ]
-                    },
-                    borderWidth: 1,
-                    outerRadius: '107%'
-                }, {
-                    // default background
-                }, {
-                    backgroundColor: '#DDD',
-                    borderWidth: 0,
-                    outerRadius: '105%',
-                    innerRadius: '103%'
-                }]
-            },
-               
-            // the value axis
-            yAxis: {
-                min: 0,
-                max: 100,
-                
-                minorTickInterval: 'auto',
-                minorTickWidth: 1,
-                minorTickLength: 10,
-                minorTickPosition: 'inside',
-                minorTickColor: '#666',
-        
-                tickPixelInterval: 30,
-                tickWidth: 2,
-                tickPosition: 'inside',
-                tickLength: 10,
-                tickColor: '#666',
-                labels: {
-                    step: 2,
-                    rotation: 'auto'
-                },
-                title: {
-                    text: ''
-                },
-                plotBands: [{
-                    from: 0,
-                    to: 100,
-                    color: '#55BF3B' // green
-                }]        
-            },
-        
-            series: [{
-                name: 'CPU load',
-                data: [80],
-                tooltip: {
-                    valueSuffix: ''
-                }
-            }]
-        };
+        // AFAIK, this is the way to execute code when the view is finished loading. Here I use it to initialize the gauge
+        $timeout(function(){
+                    myGauge1.init();
+                    myGauge1.setValue(60)
+                    setInterval('randVal1()', 3000);
+
+        }, 0);
 
         $scope.$watch('plan + additionalStorage', function() {
             $scope.bucket.cpu = $scope.bucket.plan.cpu;
@@ -304,11 +240,16 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
             addNewUser: function() {
                 this.users.push(this.newUser);
                 this.newUser = { email: '', userType: '' };
+            },
+
+            isValid: function() {
+                return this.projectName && this.users.length > 0 && this.storage;
             }
         };
 
         $scope.create = function() {
             DesktopBucketService.add($scope.bucket);
+            location.href = "My Desktop Buckets";
         };
     }])
 
