bc7f24bb04d0e42c44043c30e16d3f2e89346a89
Reem Khamis <khamisr@incubaid.com>
2013-09-12 19:43:19 +0200

    pseudo terminal

--- ComputeBox/test/testing.py
+++ ComputeBox/test/testing.py
@@ -2,5 +2,5 @@ from fabric.api import run
 
 def startall():
 	run('/etc/init.d/elasticsearch restart')
-	run('cd /opt/jumpscale/apps/osis; python osisServerStart.py', pty=False)
-	run('cd /opt/jumpscale/apps/cloudbroker; python start_appserver.py', pty=False)
+	run('cd /opt/jumpscale/apps/osis; python osisServerStart.py')
+	run('cd /opt/jumpscale/apps/cloudbroker; python start_appserver.py')
