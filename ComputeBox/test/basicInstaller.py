a200a1af2a3bb588a5816d33d0f9986ba44db631
Reem Khamis <khamisr@incubaid.com>
2013-09-15 18:38:44 +0200

    Using deb package + reboot

--- ComputeBox/test/basicInstaller.py
+++ ComputeBox/test/basicInstaller.py
@@ -1,16 +1,8 @@
-from fabric.api import run, put
-from fabric.contrib.files import append, comment
+from fabric.api import run, put, reboot
 import os
 
 def install_prereqs():
-    run('apt-get install python2.7 dialog nginx curl mc ssh mercurial python-gevent python-simplejson python-numpy byobu python-apt ipython python-pip python-imaging python-requests python-paramiko gcc g++ python-dev python-zmq msgpack-python python-mhash python-libvirt wget mercurial ssh python2.7 python-apt openssl ca-certificates php5-cgi -y')
-    run('yes w | pip install urllib3 ujson blosc pycrypto pylzma')
-    run('apt-get update')
-    run('mkdir -p /home/ISO')
-    run('wget -P /home/ISO/ http://files.incubaid.com/iaas/ubuntu-13.04-server-amd64.iso')
     WORKSPACE = os.environ.get('WORKSPACE')
-    put(os.path.join(WORKSPACE, 'ComputeBox/test/libvirt_no_sparse.patch'), '/usr/share/pyshared/')
-    put(os.path.join(WORKSPACE, 'ComputeBox/test/raring.patch'), '/tmp/')
     run('mkdir -p /opt/jumpscale/cfg/jpackages/')
     run('touch /opt/jumpscale/cfg/jpackages/sources.cfg')
     run('mv /opt/jumpscale/cfg/jpackages/sources.cfg /opt/jumpscale/cfg/jpackages/sources.cfg.bak', pty=True)
@@ -25,10 +17,26 @@ def install_prereqs():
     run('mkdir -p /opt/jumpscale/var/jpackages/metadata/cloudscalers')
     run('hg clone --ssh "ssh -o StrictHostKeyChecking=no" ssh://hg@bitbucket.org/incubaid/jp_cloudscalers /opt/jumpscale/var/jpackages/metadata/cloudscalers')
     run('mv /opt/jumpscale/var/jpackages/metadata/cloudscalers/unstable/* /opt/jumpscale/var/jpackages/metadata/cloudscalers')
-    run('jpackage_install --name bootstrapper')
     run('mkdir -p /opt/jumpscale/var/jpackages/metadata/jpackagesbase')
     run('hg clone https://hg@bitbucket.org/jumpscale/jpackages_base /opt/jumpscale/var/jpackages/metadata/jpackagesbase/')
     run('mv /opt/jumpscale/var/jpackages/metadata/jpackagesbase/unstable/* /opt/jumpscale/var/jpackages/metadata/jpackagesbase')
+    run('hg clone https://hg@bitbucket.org/jumpscale/jp_test /opt/jumpscale/var/jpackages/metadata/test/')
+    run('mv /opt/jumpscale/var/jpackages/metadata/test/unstable/* /opt/jumpscale/var/jpackages/metadata/test')
+    run('jpackage_install --name test_os')
+    reboot(wait=120)
+
+
+    run('apt-get install python2.7 dialog nginx curl mc ssh mercurial python-gevent python-simplejson python-numpy byobu python-apt ipython python-pip python-imaging python-requests python-paramiko gcc g++ python-dev python-zmq msgpack-python python-mhash python-libvirt wget mercurial ssh python2.7 python-apt openssl ca-certificates php5-cgi -y')
+    run('yes w | pip install urllib3 ujson blosc pycrypto pylzma')
+    run('apt-get update')
+    run('mkdir -p /home/ISO')
+    run('wget -P /home/ISO/ http://files.incubaid.com/iaas/ubuntu-13.04-server-amd64.iso')
+    
+    put(os.path.join(WORKSPACE, 'ComputeBox/test/libvirt_no_sparse.patch'), '/usr/share/pyshared/')
+    put(os.path.join(WORKSPACE, 'ComputeBox/test/raring.patch'), '/tmp/')
+   
+
+    run('jpackage_install --name bootstrapper')
     run('jpackage_install --name cloudbroker')
     run('jpackage_install --name cloudscalers_fe')
     run('mv /opt/jumpscale/cfg/jpackages/sources.cfg.bak /opt/jumpscale/cfg/jpackages/sources.cfg', pty=True)
