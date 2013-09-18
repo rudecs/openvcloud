6fb0564467f29ec767a2f790e4e8e2053d188641
Reem Khamis <khamisr@incubaid.com>
2013-09-18 14:55:51 +0200

    Re-ordering

--- ComputeBox/test/fabfile.py
+++ ComputeBox/test/fabfile.py
@@ -9,6 +9,7 @@ def install_jumpscale_core():
     run('apt-get install python2.7 python-dev ssh mercurial ipython -y')
 
     # downlaod repos of packages
+    WORKSPACE = os.environ.get('WORKSPACE')
     run('mkdir -p ~/.ssh/')
     put(os.path.join(WORKSPACE, 'config/*'), '~/.ssh/')
     run('chmod 0600 ~/.ssh/*')
@@ -20,7 +21,8 @@ def install_jumpscale_core():
     run('mv /opt/jumpscale/var/jpackages/metadata/jpackagesbase/unstable/* /opt/jumpscale/var/jpackages/metadata/jpackagesbase')
     run('hg clone https://hg@bitbucket.org/jumpscale/jp_test /opt/jumpscale/var/jpackages/metadata/test/')
     run('mv /opt/jumpscale/var/jpackages/metadata/test/unstable/* /opt/jumpscale/var/jpackages/metadata/test')
-    WORKSPACE = os.environ.get('WORKSPACE')
+    
+    #Update sources.cfg to contain needed domains
     run('mkdir -p /opt/jumpscale/cfg/jpackages/')
     put(os.path.join(WORKSPACE, 'ComputeBox/test/sources.cfg'), '/opt/jumpscale/cfg/jpackages/')
     put(os.path.join(WORKSPACE, 'ComputeBox/test/sources.cfg'), '/usr/local/lib/python2.7/dist-packages/JumpScale/core/_defaultcontent/cfg/jpackages/')
