1d59af789796e5436d9fa642cbd8087eb717004a
Sherif Zain <zains@incubaid.com>
2013-09-18 14:40:31 +0200

    Restructured the fabfile to use the new jpackage compute_os_base Reviewer: Reem

--- ComputeBox/test/fabfile.py
+++ ComputeBox/test/fabfile.py
@@ -2,27 +2,13 @@ from fabric.api import run, put, reboot
 import os
 
 def install_jumpscale_core():
+    # install prerequisites
     run('apt-get update')
     run('apt-get install python-pip -y')
     run('pip install https://bitbucket.org/jumpscale/jumpscale_core/get/default.zip')
-    run('apt-get install python2.7 python-dev ssh mercurial -y')
+    run('apt-get install python2.7 python-dev ssh mercurial ipython -y')
 
-    debians = ('linux-headers-3.11.0-5_3.11.0-5.10_all.deb', 'linux-headers-3.11.0-5-generic_3.11.0-5.10_amd64.deb', 'linux-image-3.11.0-5-generic_3.11.0-5.10_amd64.deb', 'linux-image-extra-3.11.0-5-generic_3.11.0-5.10_amd64.deb', 'linux-tools-common_3.11.0-5.10_all.deb', 'bcache-tools-1.0.0_1.0.0-1_all.deb')
-    for deb in debians:
-        run('wget -P /tmp/ http://files.incubaid.com/iaas/CloudScalers/%s' % deb)
-        run('dpkg -i /tmp/%s' % deb)
-
-    run('make-bcache -B /dev/sdb')
-    run('make-bcache -C /dev/sdc')
-
-    reboot(wait=300)
-
-    WORKSPACE = os.environ.get('WORKSPACE')
-    run('mkdir -p /opt/jumpscale/cfg/jpackages/')
-    put(os.path.join(WORKSPACE, 'ComputeBox/test/sources.cfg'), '/opt/jumpscale/cfg/jpackages/')
-    put(os.path.join(WORKSPACE, 'ComputeBox/test/sources.cfg'), '/usr/local/lib/python2.7/dist-packages/JumpScale/core/_defaultcontent/cfg/jpackages/')
-
-
+    # downlaod repos of packages
     run('mkdir -p ~/.ssh/')
     put(os.path.join(WORKSPACE, 'config/*'), '~/.ssh/')
     run('chmod 0600 ~/.ssh/*')
@@ -34,8 +20,17 @@ def install_jumpscale_core():
     run('mv /opt/jumpscale/var/jpackages/metadata/jpackagesbase/unstable/* /opt/jumpscale/var/jpackages/metadata/jpackagesbase')
     run('hg clone https://hg@bitbucket.org/jumpscale/jp_test /opt/jumpscale/var/jpackages/metadata/test/')
     run('mv /opt/jumpscale/var/jpackages/metadata/test/unstable/* /opt/jumpscale/var/jpackages/metadata/test')
+    WORKSPACE = os.environ.get('WORKSPACE')
+    run('mkdir -p /opt/jumpscale/cfg/jpackages/')
+    put(os.path.join(WORKSPACE, 'ComputeBox/test/sources.cfg'), '/opt/jumpscale/cfg/jpackages/')
+    put(os.path.join(WORKSPACE, 'ComputeBox/test/sources.cfg'), '/usr/local/lib/python2.7/dist-packages/JumpScale/core/_defaultcontent/cfg/jpackages/')
+
+    run('jpackage_install --name compute_os_base')
+
+    reboot(wait=300)
+
     run('jpackage_install --name test_os')
-    
+
     run('mkdir -p /home/ISO')
     run('wget -P /home/ISO/ http://files.incubaid.com/iaas/ubuntu-13.04-server-amd64.iso')
 
