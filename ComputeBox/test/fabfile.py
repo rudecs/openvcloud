dab07d5ce2b144e9c9c62cd480ac349535d52a84
Sherif Zain <zains@incubaid.com>
2013-09-24 15:30:03 +0200

    Added bitbucket.cfg for jpackage_update to run + Removed config (not used anymore)

--- ComputeBox/test/fabfile.py
+++ ComputeBox/test/fabfile.py
@@ -4,26 +4,15 @@ import os
 def install_jumpscale_core():
     # install prerequisites
     run('apt-get update')
-    run('apt-get install python-pip -y')
+    run('apt-get install python-pip python2.7 python-dev ssh mercurial ipython -y')
     run('pip install https://bitbucket.org/jumpscale/jumpscale_core/get/default.zip')
-    run('apt-get install python2.7 python-dev ssh mercurial ipython -y')
-
-    # downlaod repos of packages
-    WORKSPACE = os.environ.get('WORKSPACE')
-    run('mkdir -p ~/.ssh/')
-    put(os.path.join(WORKSPACE, 'config/*'), '~/.ssh/')
-    run('chmod 0600 ~/.ssh/*')
-    run('mkdir -p /opt/jumpscale/var/jpackages/metadata/cloudscalers')
-    run('hg clone --ssh "ssh -o StrictHostKeyChecking=no" ssh://hg@bitbucket.org/incubaid/jp_cloudscalers /opt/jumpscale/var/jpackages/metadata/cloudscalers')
-    run('mv /opt/jumpscale/var/jpackages/metadata/cloudscalers/unstable/* /opt/jumpscale/var/jpackages/metadata/cloudscalers')
-    run('mkdir -p /opt/jumpscale/var/jpackages/metadata/jpackagesbase')
-    run('hg clone https://hg@bitbucket.org/jumpscale/jpackages_base /opt/jumpscale/var/jpackages/metadata/jpackagesbase/')
-    run('mv /opt/jumpscale/var/jpackages/metadata/jpackagesbase/unstable/* /opt/jumpscale/var/jpackages/metadata/jpackagesbase')
     
-    #Update sources.cfg to contain needed domains
-    run('mkdir -p /opt/jumpscale/cfg/jpackages/')
+    #Update sources.cfg and bitbucket.cfg
+    WORKSPACE = os.environ.get('WORKSPACE')
     put(os.path.join(WORKSPACE, 'ComputeBox/test/sources.cfg'), '/opt/jumpscale/cfg/jpackages/')
-    put(os.path.join(WORKSPACE, 'ComputeBox/test/sources.cfg'), '/usr/local/lib/python2.7/dist-packages/JumpScale/core/_defaultcontent/cfg/jpackages/')
+    put(os.path.join(WORKSPACE, 'ComputeBox/test/bitbucket.cfg'), '/opt/jumpscale/cfg/jsconfig/')
+
+    run('jpackage_update')
 
     run('mkdir -p /home/ISO')
     run('wget -P /home/ISO/ http://files.incubaid.com/iaas/ubuntu-13.04-server-amd64.iso')
