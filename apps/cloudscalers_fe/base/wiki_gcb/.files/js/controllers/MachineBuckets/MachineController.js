f7226e781697c6f02e2b11160855fc863159a6b9
mtayseer <devnull@localhost>
2013-10-12 04:54:45 +0200

    Fixed listing

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/MachineBuckets/MachineController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/MachineBuckets/MachineController.js
@@ -1,7 +1,9 @@
 
 cloudscalersControllers
-    .controller('MachineController', ['$scope', 'Machine', function($scope, Machine) {
+    .controller('MachineController', ['$scope', 'Machine', 'Size', 'Image', function($scope, Machine, Size, Image) {
         $scope.machines = Machine.list(0);
+        $scope.sizes = Size.list();
+        $scope.images = Image.list();
         
         $scope.numOfDataLocations = function(machine) {
             return _.filter(machine.region, function(x) { return x; }).length;
