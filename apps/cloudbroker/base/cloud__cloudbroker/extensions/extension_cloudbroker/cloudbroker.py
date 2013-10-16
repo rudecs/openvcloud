eea9dcd53d46bec7175099411c93d4431cb35966
Hendrik Demolder <hendrik@awingu.com>
2013-10-16 11:11:14 +0200

    Image type is in extra dict of a libcloud image object

--- apps/cloudbroker/base/cloud__cloudbroker/extensions/extension_cloudbroker/cloudbroker.py
+++ apps/cloudbroker/base/cloud__cloudbroker/extensions/extension_cloudbroker/cloudbroker.py
@@ -116,12 +116,12 @@ class CloudBroker(object):
                 image = cloudbroker.models.image.new()
                 image.name = pimage.name
                 image.referenceId = pimage.id
-                image.type = pimage.type
+                image.type = pimage.extra['imagetype']
             else:
                 image = cloudbroker.model_image_get(imageid)
                 image['name'] = pimage.name
                 image['referenceId'] = pimage.id
-                image['type'] = pimage.type
+                image['type'] = pimage.extra['imagetype']
             count += 1
             imageid = cloudbroker.model_image_set(image)
             if not imageid in stack['images']:
