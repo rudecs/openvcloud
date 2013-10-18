2821a9b0943783d3dbda91a3bca96a12ef3c8b1c
RobVanMieghem <rob@awingu.com>
2013-10-18 15:02:54 +0200

    Install the correct demo package

--- ComputeBox/test/fab_cloudbroker_demo_setup.py
+++ ComputeBox/test/fab_cloudbroker_demo_setup.py
@@ -2,4 +2,4 @@ from fabric.api import run
 
 def bootstrap():
     run('jpackage_update')
-    run('jpackage_install --name demo --domain cloudscalers --version 1.0')
+    run('jpackage_install --name cloudbroker_minimal_install --domain cloudscalers --version 1.0')
