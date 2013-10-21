6a5be03b023e1af93d89f0855114690f8459af3a
Jo De Boeck <deboeckj@incubaid.com>
2013-10-21 12:41:11 +0200

    Add hrd configuration for vncproxy

--- ComputeBox/test/fab_cloudbroker_demo_setup.py
+++ ComputeBox/test/fab_cloudbroker_demo_setup.py
@@ -1,5 +1,13 @@
-from fabric.api import run
-
+from fabric.api import run, env, put
+from StringIO import StringIO
 def bootstrap():
+    # write vncproxy hrd
+    vncproxyhrd = """
+vncproxy.libcloud.actor.host=127.0.0.1
+vncproxy.libcloud.actor.port=80
+vncproxy.libcloud.actor.secret=1234
+vncproxy.publicurl=http://%s:8081/vnc_auto.html?token=
+    """ % env['host']
+    put(StringIO(vncproxyhrd), '/opt/jumpscale/cfg/hrd/vncproxy.hrg')
     run('jpackage_update')
     run('jpackage_install --name cloudbroker_minimal_install --domain cloudscalers --version 1.0')
