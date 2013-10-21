f7e5bf8d78543c8590a12a625f72e49ec00ee6bd
Jo De Boeck <deboeckj@incubaid.com>
2013-10-21 14:22:27 +0200

    Fix vncproxy port

--- ComputeBox/test/fab_cloudbroker_demo_setup.py
+++ ComputeBox/test/fab_cloudbroker_demo_setup.py
@@ -6,7 +6,7 @@ def bootstrap():
 vncproxy.libcloud.actor.host=127.0.0.1
 vncproxy.libcloud.actor.port=80
 vncproxy.libcloud.actor.secret=1234
-vncproxy.publicurl=http://%s:8081/vnc_auto.html?token=
+vncproxy.publicurl=http://%s:8091/vnc_auto.html?token=
     """ % env['host']
     put(StringIO(vncproxyhrd), '/opt/jumpscale/cfg/hrd/vncproxy.hrd')
     run('jpackage_update')
