f174fdb1d79c931e64b56030d099d518a5cd22b4
RobVanMieghem <rob@awingu.com>
2013-10-31 11:53:03 +0100

    Fixed tests

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/MachineBuckets/ConsoleController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/MachineBuckets/ConsoleController.js
@@ -1,6 +1,6 @@
 
 cloudscalersControllers
-    .controller('ConsoleController', ['$scope','$routeParams', 'Machine'], function($scope, $routeParams, Machine) {
+    .controller('ConsoleController', ['$scope','$routeParams', 'Machine', function($scope, $routeParams, Machine) {
         $scope.machineConsoleUrlResult = Machine.getConsoleUrl($routeParams.machineId);
         $scope.novnc-connectioninfo = {}
         
