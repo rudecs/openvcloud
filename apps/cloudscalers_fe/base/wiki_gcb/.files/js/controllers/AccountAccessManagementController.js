bbcaaf10847d2a72a69403ca59b3d4d8efa4546c
mtayseer <devnull@localhost>
2013-11-28 14:58:10 +0100

    Moved cloudspace access to its correct page + some cleanup of unused JS files in default.wiki

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/AccountAccessManagementController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/AccountAccessManagementController.js
@@ -1,45 +1,4 @@
 angular.module('cloudscalers.controllers')
     .controller('AccountAccessManagementController', ['$scope', 'CloudSpace', 'Account', function($scope, CloudSpace, Account) {
-        $scope.resetUser = function() {
-            // For now, we will grant the new user all permissions. See http://<server>/specifications/Security
-            $scope.newUser = {
-                nameOrEmail: '', 
-                access: {
-                    R: true,
-                    X: true,
-                    C: true,
-                    D: true,
-                    U: true,
-                    A: true
-                }
-            };
-        };
-
-        $scope.loadSpaceAcl = function() {
-            return CloudSpace.get($scope.currentSpace.id).then(function(space) {
-                $scope.currentSpace.acl = space.acl;
-            });
-        };
-
-        $scope.resetUser();
-        $scope.loadSpaceAcl();
-        $scope.userError = false;
-
-        $scope.addUser = function() {
-            return CloudSpace.addUser($scope.currentSpace, $scope.newUser.nameOrEmail, $scope.newUser.access).then(function() {
-                $scope.loadSpaceAcl().then(function() {
-                    $scope.resetUser();
-                });
-                $scope.userError = false;
-            }, function(result) {
-                $scope.userError = true;
-            });
-        };
-
-        $scope.deleteUser = function(space, user) {
-            // TODO: ==================================
-            CloudSpace.deleteUser($scope.currentSpace, user.userGroupId).then(function() {
-                $scope.loadSpaceAcl();
-            });
-        };
+        
     }]);
