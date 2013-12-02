3cf0a03ddd3ef67b22605505e601e8eff6ad5abd
Hendrik Demolder <hendrik@awingu.com>
2013-12-02 17:08:26 +0100

    Add agent.hrd to the default configuration during a installation

--- ComputeBox/test/fabfile.py
+++ ComputeBox/test/fabfile.py
@@ -26,6 +26,7 @@ def install_compute_node(hostname, workspace):
     put(os.path.join(workspace, 'ComputeBox/test/configurations/grid.hrd'), '/opt/jumpscale/cfg/hrd/grid.hrd')
     put(os.path.join(workspace, 'ComputeBox/test/configurations/system_root_credentials.hrd'), '/opt/jumpscale/cfg/hrd/system_root_credentials.hrd')
     put(os.path.join(workspace, 'ComputeBox/test/configurations/', hostname, 'elasticsearch.hrd'), '/opt/jumpscale/cfg/hrd/elasticsearch.hrd')
+    put(os.path.join(workspace, 'ComputeBox/test/configurations/agent.hrd'), '/opt/jumpscale/cfg/hrd/agent.hrd')
 
     #install core first since computenode configure is not run in seperate context
     run('jpackage_install --name grid')
