369220cfa2120929ab826bd120deea189863fdf9
Jo De Boeck <deboeckj@incubaid.com>
2014-01-15 21:05:08 +0200

    Fix in vnccproxy

--- apps/vncproxy/utils/jumpscaleclient.py
+++ apps/vncproxy/utils/jumpscaleclient.py
@@ -2,5 +2,5 @@ from JumpScale import j
 import JumpScale.portal
 
 def getClient(ip):
-    cl = j.core.portal.getPortalClient(ip, 80, '1234')
+    cl = j.core.portal.getClient(ip, 9999, '1234')
     return cl.getActor('libcloud','libvirt')
