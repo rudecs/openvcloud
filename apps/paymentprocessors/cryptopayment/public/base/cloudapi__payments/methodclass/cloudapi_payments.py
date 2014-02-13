b956a2aab0ce781374ef9149fa8efdc7263844b0
RobVanMieghem <rob@awingu.com>
2014-02-13 14:22:47 +0100

    Actor for cryptopayment processor

--- apps/paymentprocessors/cryptopayment/public/base/cloudapi__payments/methodclass/cloudapi_payments.py
+++ apps/paymentprocessors/cryptopayment/public/base/cloudapi__payments/methodclass/cloudapi_payments.py
@@ -2,12 +2,20 @@ from JumpScale import j
 from cloudbrokerlib import authenticator
 import ujson
 
-
 class cloudapi_payments(object):
     """
     API Actor api for performing payments with cryptocurrency
 
     """
+    
+    def __init__(self):
+        osiscl = j.core.osis.getClient()
+
+        self.models = Class()
+        for ns in osiscl.listNamespaceCategories('cryptopayment'):
+            self.models.__dict__[ns] = (j.core.osis.getClientForCategory(osiscl, 'cryptopayment', ns))
+            self.models.__dict__[ns].find = models.__dict__[ns].search
+    
     
     @authenticator.auth(acl='R')
     def getPaymentInfo(self, accountId, coin, **kwargs):
@@ -18,4 +26,5 @@ class cloudapi_payments(object):
         param:coin the code of the currency you want to make a payment with (LTC or BTC currently supported)
         result:dict A json dict containing 'address', 'value' and 'coin':coin
         """
+        
         return {'address':'abcd','value':800.0, 'coin':coin}
