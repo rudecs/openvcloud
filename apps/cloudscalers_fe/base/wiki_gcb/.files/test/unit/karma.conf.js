6b5426acff8d69fe8ba2e1c65e666696e042eba5
Karim Ali <karim@codescalers.com>
2014-02-20 13:41:15 +0200

    Add login url to signup page

--- apps/cloudscalers_fe/base/wiki_gcb/.files/test/unit/karma.conf.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/test/unit/karma.conf.js
@@ -99,7 +99,7 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     //browsers: ['Chrome'],
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // If browser does not capture in given timeout [ms], kill it
