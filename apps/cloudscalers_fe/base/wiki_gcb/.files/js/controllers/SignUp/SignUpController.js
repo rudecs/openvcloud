6b1b8ec7ed6fcdf0d770bfc32c13ef52d6da2228
Karim Ali <karim@codescalers.com>
2014-03-06 13:14:30 +0200

    Remove password from signup form

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/SignUp/SignUpController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/SignUp/SignUpController.js
@@ -8,15 +8,17 @@ angular.module('cloudscalers.controllers')
         $scope.signUpResult = '';
 
         $scope.$watch('user.username + user.password + email + passwordConfirmation', function() {
-            $scope.canSignUp = $scope.user.username && $scope.email && $scope.user.password && $scope.passwordConfirmation;
+            $scope.canSignUp = $scope.user.username && $scope.email;
+             // && $scope.user.password && $scope.passwordConfirmation
         });
 
         $scope.signUp = function() {
             $scope.signUpResult = {};
 
-            $scope.isPasswordConfirmed = $scope.user.password == $scope.passwordConfirmation && 
-                $scope.user.password && 
-                $scope.passwordConfirmation;
+            $scope.isPasswordConfirmed = $scope.user.password == $scope.passwordConfirmation;
+            // && 
+                // $scope.user.password && 
+                // $scope.passwordConfirmation;
 
             if ($scope.isPasswordConfirmed) {
                 $scope.signUpResult = User.signUp($scope.user.username, $scope.email, $scope.user.password);
