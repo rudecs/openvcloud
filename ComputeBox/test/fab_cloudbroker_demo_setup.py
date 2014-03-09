33267673521d3ec6b39afecf897170cefbb0470a
Jo De Boeck <deboeckj@incubaid.com>
2014-03-09 14:41:31 +0200

    demosetup: add stack and account

--- ComputeBox/test/fab_cloudbroker_demo_setup.py
+++ ComputeBox/test/fab_cloudbroker_demo_setup.py
@@ -15,3 +15,24 @@ vncproxy.publicurl=http://%s:8091/vnc_auto.html?token=
 def templates():
     run('jpackage install --name image_ubuntu1310 --domain cloudscalers')
     run('jpackage install --name image_fedora20 --domain cloudscalers')
+
+def demodata():
+    pythoncode = '''
+from JumpScale import j
+import JumpScale.grid.osis
+import JumpScale.portal
+j.core.portal.getClient('127.0.0.1', 9999, '1234') #lazy load actors
+cl = j.core.osis.getClient(user='root')
+ccl = j.core.osis.getClientForCategory(cl, 'cloudbroker', 'stack')
+stack = ccl.new()
+stack.apiUrl = 'qemu+ssh://%(host)s/system'
+stack.descr = 'Libvirt Stack'
+stack.type = 'LIBVIRT'
+stack.name ='vScalers demo'
+stack.referenceId = j.application.config.get('cloudscalers.compute.nodeid')
+ccl.set(stack)
+j.apps.cloud.cloudbroker.updateImages()
+j.apps.cloudapi.accounts.create('demo_account', 'admin')
+j.apps.cloudapi.cloudspaces.create(1, 'demo_space', 'admin')
+    ''' % env
+    run('python -c "%s"' % pythoncode)
