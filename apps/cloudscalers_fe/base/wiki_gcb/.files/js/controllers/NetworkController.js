695d5c6f33c44d99516a3c4c64864159c26bd90e
karimaliz <devnull@localhost>
2014-03-14 12:13:08 +0200

    Revert PortForwarding from Unstable branch

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/NetworkController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/NetworkController.js
@@ -1,7 +1,7 @@
 
 angular.module('cloudscalers.controllers')
-    .controller('NetworkController', ['$scope', 'NetworkBuckets', '$modal',
-    	function ($scope, NetworkBuckets, $modal) {
+    .controller('NetworkController', ['$scope', '$modal',
+    	function ($scope, $modal) {
             $scope.$watch('currentSpace.id',function(){
 	    		if ($scope.currentSpace){
 	    			$scope.managementui = "http://" + $scope.currentSpace.publicipaddress + "/webfig/";
