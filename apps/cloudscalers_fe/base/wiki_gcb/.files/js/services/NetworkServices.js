1ee5ea43354753074894ac2ee0cbaf952e676105
karimaliz <devnull@localhost>
2014-03-14 11:40:59 +0200

    Repush PortForwarding to feature-NetworkUI

---
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/services/NetworkServices.js
@@ -0,0 +1,38 @@
+
+angular.module('cloudscalers.services')
+     .factory('NetworkBuckets',function ($http, $q) {
+        return {
+            listPortforwarding: function() {
+                return $http.get(cloudspaceconfig.apibaseurl + '/portforwarding/list').then(
+                        function(result){
+                            return result.data;
+                        },
+                        function(reason){
+                            $q.defer(reason);
+                        }
+                    );
+            },
+            createPortforward: function(ip, puplicPort, vmName, localPort) {
+                return $http.get(cloudspaceconfig.apibaseurl + '/portforwarding/create?ip=' + encodeURIComponent(ip) + "&puplicPort=" +
+                    encodeURIComponent(puplicPort) + "&vmName=" + encodeURIComponent(vmName) + "&localPort=" + 
+                    encodeURIComponent(localPort)).then(
+                        function(result){
+                            return result;
+                        },
+                        function(reason){
+                            $q.defer(reason);
+                        }
+                    );
+            },
+            commonports: function() {
+                return $http.get(cloudspaceconfig.apibaseurl + '/commonports/list').then(
+                        function(result){
+                            return result.data;
+                        },
+                        function(reason){
+                            $q.defer(reason);
+                        }
+                    );
+            },
+        };
+    });
