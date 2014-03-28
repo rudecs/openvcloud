4079d1d7dc24fde70943bce684fd6676954fb258
RobVanMieghem <devnull@localhost>
2014-03-28 17:13:54 +0100

    Added cloudapi_consumption methodclass

---
+++ apps/cloudbroker/base/cloudapi__consumption/methodclass/cloudapi_consumption.py
@@ -0,0 +1,28 @@
+from JumpScale import j
+
+class cloudapi_consumption(j.code.classGetBase()):
+    """
+    API consumption Actor, this actor is the final api a enduser uses to get consumption details
+    
+    """
+    def __init__(self):
+        
+        self._te={}
+        self.actorname="consumption"
+        self.appname="cloudapi"
+        #cloudapi_consumption_osis.__init__(self)
+    
+
+        pass
+
+    def get(self, accountId, creditTransactionId, **kwargs):
+        """
+        Gets detailed consumption for a specific creditTransaction
+        The user needs write access rights on the space.
+        param:accountId id of the account
+        param:creditTransactionId id of the credit transaction
+        result bool
+        """
+        #put your code here to implement this method
+        raise NotImplementedError ("not implemented method get")
+    
