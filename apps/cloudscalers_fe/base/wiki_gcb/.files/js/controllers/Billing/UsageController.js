4ef3027009a316cd6aebaafd9dc8a97e031ae3eb
RobVanMieghem <devnull@localhost>
2014-03-31 16:26:42 +0200

    Provide a link to the usage details if the creditTransaction is DEBIT

---
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Billing/UsageController.js
@@ -0,0 +1,6 @@
+angular.module('cloudscalers.controllers')
+    .controller('UsageController', ['$scope', '$modal', 'Account', '$timeout', '$ErrorResponseAlert', function($scope, $modal, Account, $timeout, $ErrorResponseAlert) {
+
+
+    
+  });
