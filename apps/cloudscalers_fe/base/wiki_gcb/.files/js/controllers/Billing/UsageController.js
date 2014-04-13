4cf1cdc95c940e7a5a65c29ae2d5af9abd177983
karimaliz <devnull@localhost>
2014-04-13 17:47:53 +0200

    usage report page ui

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Billing/UsageController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Billing/UsageController.js
@@ -1,7 +1,8 @@
 angular.module('cloudscalers.controllers')
     .controller('UsageController', ['$scope', 'Account', '$ErrorResponseAlert', '$window',
     function($scope, Account, $ErrorResponseAlert, $window) {
-
+      $scope.orderByField = 'name';
+      $scope.reverseSort = false;
       $scope.$watch('currentAccount.id', function(){
         if ($scope.currentAccount && $scope.currentAccount.id) {
           var uri = new URI($window.location);
