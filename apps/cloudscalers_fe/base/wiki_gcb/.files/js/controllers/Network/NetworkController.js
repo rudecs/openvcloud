db5cb614d0efd26d7a30c4aa08ba6f9b770de42e
karimaliz <devnull@localhost>
2014-04-14 15:33:07 +0200

    fix portforwording

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Network/NetworkController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Network/NetworkController.js
@@ -61,12 +61,14 @@ angular.module('cloudscalers.controllers')
               $scope.editRule = [];
               Networks.listPortforwarding($scope.currentSpace.id).then(function(data) {
                 $scope.portforwardbyID = data;
+                // console.log($scope.portforwardbyID[index]);
+                console.log(index);
                 $scope.editRule = {
                     id: index.id,
-                    ip: $scope.portforwardbyID[index.id].publicIp,
-                    publicPort: $scope.portforwardbyID[index.id].publicPort,
-                    VM: {'name': $scope.portforwardbyID[index.id].vmName , 'id': $scope.portforwardbyID[index.id].vmid},
-                    localPort: $scope.portforwardbyID[index.id].localPort
+                    ip: index.publicIp,
+                    publicPort: index.publicPort,
+                    VM: index.vmName,
+                    localPort: index.localPort
                 };
               });
               $scope.update = function () {
