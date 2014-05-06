d708695c95dd9ed05d60e8ea681cdbcd0ff893f2
RobVanMieghem <devnull@localhost>
2014-05-06 16:39:57 +0000

    Since the api can not be used without an authkey anymore, reuse the authkey for the paypal callback

--- apps/cloudbroker/base/cloudapi__paypal/methodclass/cloudapi_paypal.py
+++ apps/cloudbroker/base/cloudapi__paypal/methodclass/cloudapi_paypal.py
@@ -108,7 +108,7 @@ class cloudapi_paypal(j.code.classGetBase()):
         payload = {
                    "intent":"sale",
                    "redirect_urls":{
-                                    "return_url":"%s/restmachine/cloudapi/paypal/confirmauthorization?id=%s" % (portalurl,credittransaction.id),
+                                    "return_url":"%s/restmachine/cloudapi/paypal/confirmauthorization?id=%s&authkey=%s" % (portalurl,credittransaction.id,kwargs['authkey']),
                                     "cancel_url":"%s/wiki_gcb/AccountSettings" % portalurl
                                    },
                    "payer":{
