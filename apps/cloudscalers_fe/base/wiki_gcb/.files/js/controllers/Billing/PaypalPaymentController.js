37a025ddc75c1d13275b541a1af8c8e48f5680b1
karimaliz <devnull@localhost>
2014-05-07 12:16:24 +0200

    make sure payment is not less than 50$

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Billing/PaypalPaymentController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Billing/PaypalPaymentController.js
@@ -1,17 +1,22 @@
 angular.module('cloudscalers.controllers')
-    .controller('PaypalPaymentController', ['$scope', '$modal', 'Account', '$timeout', '$ErrorResponseAlert', '$window', 'PaypalPayments',
-      function($scope, $modal, Account, $timeout, $ErrorResponseAlert,$window, PaypalPayments) {
+    .controller('PaypalPaymentController', ['$scope', '$modal', 'Account', '$timeout', '$ErrorResponseAlert', '$window', 'PaypalPayments', '$alert',
+      function($scope, $modal, Account, $timeout, $ErrorResponseAlert,$window, PaypalPayments, $alert) {
 
     $scope.payWithPayPal = function(){
-    	PaypalPayments.initiatePayment($scope.currentAccount.id,$scope.creditToAdd, 'USD').then(
-    				function(result){
-    					$window.location = result.paypalurl;
-    				},
-    				function(reason){
-    					$ErrorResponseAlert(reason);
-    				}
-    			);
-    	
+        if($scope.creditToAdd >= 50){
+            PaypalPayments.initiatePayment($scope.currentAccount.id,$scope.creditToAdd, 'USD').then(
+                        function(result){
+                            $window.location = result.paypalurl;
+                        },
+                        function(reason){
+                            $ErrorResponseAlert(reason);
+                        }
+                    );
+        }
+        else{
+            $alert("Sorry, Payment shouldn't be lower than 50$!");
+        }
+        
     }
     
 
