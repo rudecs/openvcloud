4803b719d7dd68d357aa712db03dfd36a5cb4e53
Sherif Zain <zains@incubaid.com>
2014-05-18 16:12:58 +0200

    Added vmachines macro

---
+++ apps/cbportal/base/CBGrid/.macros/page/vmachines/3_main.py
@@ -0,0 +1,32 @@
+import datetime
+
+def main(j, args, params, tags, tasklet):
+    page = args.page
+    modifier = j.html.getPageModifierGridDataTables(page)
+    stackid = args.getTag("stackid")
+    filters = dict()
+    if stackid:
+        filters['stackId'] = stackid
+
+    fieldnames = ['Name', 'Description', 'Status', 'Created at', 'Stack']
+
+    def makeTime(row, field):
+        time = datetime.datetime.fromtimestamp(row[field]).strftime('%m-%d %H:%M:%S') or ''
+        return time
+
+    def stackLinkify(row, field):
+        return '[%s|cbgrid/stack?id=%s]' % (row[field], row[field])
+
+    fieldids = ['name', 'descr', 'status', 'creationTime', 'stackId']
+    fieldvalues = ['name', 'descr', 'status', makeTime, stackLinkify]
+    tableid = modifier.addTableForModel('cloudbroker', 'vmachine', fieldids, fieldnames, fieldvalues, filters)
+    modifier.addSearchOptions('#%s' % tableid)
+    modifier.addSorting('#%s' % tableid, 0, 'desc')
+
+    params.result = page
+
+    return params
+
+
+def match(j, args, params, tags, tasklet):
+    return True
