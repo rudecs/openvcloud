f5b1c855b439e2823a41a6349fd4d32b14183ce8
Sherif Zain <zains@incubaid.com>
2014-06-08 11:26:03 +0200

    Added authorization decorator for iaas actor methods

--- apps/cbportal/base/cloudbroker__iaas/methodclass/cloudbroker_iaas.py
+++ apps/cbportal/base/cloudbroker__iaas/methodclass/cloudbroker_iaas.py
@@ -1,6 +1,7 @@
 from JumpScale import j
 import JumpScale.grid.osis
 json = j.db.serializers.ujson
+from JumpScale.portal.portal.auth import auth
 
 class cloudbroker_iaas(j.code.classGetBase()):
     """
@@ -19,6 +20,7 @@ class cloudbroker_iaas(j.code.classGetBase()):
             self._cb = j.apps.cloudbroker.iaas
         return self._cb
 
+    @auth(['level1', 'level2'])
     def updateImages(self, **kwargs):
         """
         Update the local images of the cloudbroker
@@ -27,7 +29,9 @@ class cloudbroker_iaas(j.code.classGetBase()):
         stacks = self.cbcl.stack.list()
         for stack in stacks:
             self.cb.extensions.imp.stackImportImages(stack)
+        return True
 
+    @auth(['level1', 'level2'])
     def setStackStatus(self, statckid, status, **kwargs):
         """
         Set different stack statusses, options are 'ENABLED(creation and actions on machines is possible)','DISABLED(Only existing machines are started)', 'OFFLINE(Machine is not available'
@@ -39,9 +43,9 @@ class cloudbroker_iaas(j.code.classGetBase()):
             stack = self.cbcl.stack.get(statckid)
             stack.status = status
             self.cbcl.stack.set(stack)
+            return True
         else:
             ctx = kwargs["ctx"]
             headers = [('Content-Type', 'application/json'), ]
             ctx.start_response("404", headers)
             return 'Stack ID not found'
-    
