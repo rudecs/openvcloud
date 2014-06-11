4751bb5b81ec979755045d399b6264173d0a36a2
karimaliz <devnull@localhost>
2014-06-11 13:53:44 +0200

    move change password to profile page

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/AccountController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/AccountController.js
@@ -1,7 +1,6 @@
 angular.module('cloudscalers.controllers')
     .controller('AccountController', ['$scope', 'Account', 'LoadingDialog','$ErrorResponseAlert', '$timeout',
     	function($scope, Account, LoadingDialog, $ErrorResponseAlert, $timeout) {
-    	$scope
       	$scope.$parent.$watch('currentAccount', function(){
 	      	$scope.accountNames = $scope.$parent.currentAccount.name;
 	      	$scope.preferredDataLocation = $scope.$parent.currentAccount.preferredDataLocation;
