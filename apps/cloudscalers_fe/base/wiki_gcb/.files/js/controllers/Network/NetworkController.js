c8aa7039b662445c939403ee05beb9a192bf89dd
karimaliz <devnull@localhost>
2014-06-17 11:15:23 +0200

    clean old code

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Network/NetworkController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Network/NetworkController.js
@@ -3,15 +3,7 @@ angular.module('cloudscalers.controllers')
         function ($scope, Networks, Machine, $modal, $timeout, $sce, CloudSpace, $ErrorResponseAlert) {
             $scope.search = "";
             $scope.portforwardbyID = "";
-            //$scope.$watch('currentSpace.id',function(){
-            //    if ($scope.currentSpace){
-            //        $scope.managementui = "http://" + $scope.currentSpace.publicipaddress + ":9080/webfig/";
-            //        Machine.list($scope.currentSpace.id).then(function(data) {
-            //          $scope.currentSpace.machines = data;
-            //        });
-            //    }
-            //});
-            //
+
             $scope.getDefenseShield = function() {
                 CloudSpace.getDefenseShield($scope.currentSpace.id).then(function(shieldobj) {
                     window.open(shieldobj.url, "autologin=" + shieldobj.user + "|" + shieldobj.password, "width=600, height=600");
@@ -22,24 +14,5 @@ angular.module('cloudscalers.controllers')
                 });
 		
             };
-
-
-            //var routerosController = function ($scope, $modalInstance) {
-            //    $scope.cancel = function () {
-            //        $modalInstance.dismiss('cancel');
-            //    };
-            //    // for load scope value in iframe
-            //    $scope.trustSrc = function(src) {
-            //        return $sce.trustAsResourceUrl(src);
-            //    }
-            //};
-            //$scope.routeros = function () {
-            //    var modalInstance = $modal.open({
-            //        templateUrl: 'routerosDialog.html',
-            //        controller: routerosController,
-            //        resolve: {},
-            //        scope: $scope
-            //    });
-            //};
         }
     ]);
