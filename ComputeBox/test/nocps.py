38b119e7eb4e1f99194f304271b1bf8723d88034
Jo De Boeck <deboeckj@codescalers.com>
2014-06-20 15:13:03 +0300

    Make it possible to pass the profile name

--- ComputeBox/test/nocps.py
+++ ComputeBox/test/nocps.py
@@ -1,8 +1,8 @@
 import xmlrpclib
 import time
 
-def installFreshComputeNode(nocps_url, macaddress, hostname, ip):
-    print 'Installing Ubuntu 14.04 on the compute node'
+def installFreshComputeNode(nocps_url, macaddress, hostname, ip, profilename):
+    print 'Installing %s on the compute node' % profilename
 
     ps = xmlrpclib.ServerProxy(nocps_url)
     if not macaddress or not ip:
@@ -13,11 +13,13 @@ def installFreshComputeNode(nocps_url, macaddress, hostname, ip):
                 break
     elif not macaddress:
         macaddress = ps.PXE_API.getServerByIP(ip)
+    profile = ps.PXE_API.getProfileByName(profilename)
+
     serverdetails = {
                      'adminuser': 'cs',
                      'hostname': hostname,
                      'mac': macaddress,    #Physical machine identifier
-                     'profile': 1005,                #Ubuntu 14.04, cloudscalers apt-get server
+                     'profile': profile,                #Ubuntu 14.04, cloudscalers apt-get server
                      'rebootmethod': 'auto',
                      'rootpassword': 'R00t3r',
                      'rootpassword2': 'R00t3r',
@@ -47,7 +49,8 @@ if __name__ == '__main__':
     parser.add_argument('-u','--url',required=True, help='Url of nocps xmlrpc server like https://user:password@nocps.incubaid.com/xmlrpc.php')
     parser.add_argument('-m','--mac',required=False, help='MAC address of the server to be reinstalled')
     parser.add_argument('-i','--ip',required=False, help='IP address of the server to be reinstalled')
+    parser.add_argument('-p','--profile',required=False, help='Profile to install', default="CloudScalers Ubuntu 14.04")
     parser.add_argument('--hostname', required=True, help='Hostname of the server to be reinstalled')
     args = parser.parse_args()
 
-    installFreshComputeNode(args.url, args.mac, args.hostname, args.ip)
+    installFreshComputeNode(args.url, args.mac, args.hostname, args.ip, args.profile)
