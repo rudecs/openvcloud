b01fb8431c65e3f838c6d850acaf8feb0855a90c
mtayseer <devnull@localhost>
2014-07-14 01:15:58 +0200

    Finished the tour, but it may need some styling

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Mothership1TourController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Mothership1TourController.js
@@ -1,10 +1,14 @@
 angular.module('cloudscalers.controllers')
     .controller('Mothership1TourController', ['$scope', 'ipCookie', function($scope, ipCookie) {
-        $scope.tourStep = /*ipCookie('tourStep') ||*/ 0;
-
+        $scope.tourStep = ipCookie('tourStep') || 0; 
+        $scope.portForwardTourStep = ipCookie('portForwardTourStep') || 0;
+        $scope.machineDetailTourStep = ipCookie('machineDetailTourStep') || 0;
+        
         // Saving tour progress in cookies
         $scope.postStepCallback = function() {
             ipCookie('tourStep', $scope.tourStep);
+            ipCookie('portForwardTourStep', $scope.portForwardTourStep);
+            ipCookie('machineDetailTourStep', $scope.machineDetailTourStep);
         };
-
+        
     }]);
