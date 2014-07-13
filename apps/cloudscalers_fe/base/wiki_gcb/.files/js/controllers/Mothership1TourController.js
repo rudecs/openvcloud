87e0a58f5833034aaf46f1304d93525ad7274919
mtayseer <devnull@localhost>
2014-07-13 14:09:35 +0200

    Tour tips in pages except at creating port forward

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Mothership1TourController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Mothership1TourController.js
@@ -1,6 +1,6 @@
 angular.module('cloudscalers.controllers')
     .controller('Mothership1TourController', ['$scope', 'ipCookie', function($scope, ipCookie) {
-        $scope.tourStep = ipCookie('tourStep') || 0;
+        $scope.tourStep = /*ipCookie('tourStep') ||*/ 0;
 
         // Saving tour progress in cookies
         $scope.postStepCallback = function() {
