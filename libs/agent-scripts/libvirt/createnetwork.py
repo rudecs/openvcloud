aceffc3057faa3c0c9ecd86462aa628c1ec9134d
Jo De Boeck <deboeckj@codescalers.com>
2014-07-19 22:48:08 +0200

    Harden createnetwork

--- libs/agent-scripts/libvirt/createnetwork.py
+++ libs/agent-scripts/libvirt/createnetwork.py
@@ -16,19 +16,16 @@ async = True
 
 def action(networkid):
     networkname = 'space_%s' % networkid
-    
     from CloudscalerLibcloud.utils.libvirtutil import LibvirtUtil
     connection = LibvirtUtil()
     networks = connection.connection.listNetworks()
     networkinformation = {'networkname':networkname}
+    from JumpScale.lib import ovsnetconfig
+    vxnet = j.system.ovsnetconfig.ensureVXNet(networkid, 'vxbackend')
     if networkname not in networks:
-    	#create the bridge if it does not exist
-    	from JumpScale.lib import ovsnetconfig
-    	vxnet = j.system.ovsnetconfig.ensureVXNet(networkid, 'vxbackend')
-    	bridgename = vxnet.bridge.name
-
-    	connection.createNetwork(networkname,bridgename)
-    	
+        #create the bridge if it does not exist
+        bridgename = vxnet.bridge.name
+        connection.createNetwork(networkname,bridgename)
     return networkinformation
 
 
