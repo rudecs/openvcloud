34a3199ebc77057a182d82bbd383cd8f0105c8f5
mtayseer <devnull@localhost>
2014-07-22 11:50:46 +0200

    Save tourtip state

--- apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Mothership1TourController.js
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/Mothership1TourController.js
@@ -1,16 +1,17 @@
 angular.module('cloudscalers.controllers')
     .controller('Mothership1TourController', ['$scope', 'ipCookie', function($scope, ipCookie) {
-        $scope.tourStep = /*ipCookie('tourStep') ||*/ 0; 
-        $scope.portForwardTourStep = /*ipCookie('portForwardTourStep') ||*/ 0;
-        $scope.machineDetailTourStep = /*ipCookie('machineDetailTourStep') ||*/ 0;
-        $scope.machineListTourStep = /*ipCookie('machineListTourStep') ||*/ 0;
+        $scope.steps = {};
+        $scope.steps['tourStep'] = ipCookie('tourStep') || 0; 
+        $scope.steps['portForwardTourStep'] = ipCookie('portForwardTourStep') || 0;
+        $scope.steps['machineDetailTourStep'] = ipCookie('machineDetailTourStep') || 0;
+        $scope.steps['machineListTourStep'] = ipCookie('machineListTourStep') || 0;
         
         // Saving tour progress in cookies
         $scope.postStepCallback = function() {
-            ipCookie('tourStep', $scope.tourStep);
-            ipCookie('portForwardTourStep', $scope.portForwardTourStep);
-            ipCookie('machineDetailTourStep', $scope.machineDetailTourStep);
-            ipCookie('machineListTourStep', $scope.machineListTourStep);
+            ipCookie('tourStep', $scope.steps['tourStep']);
+            ipCookie('portForwardTourStep', $scope.steps['portForwardTourStep']);
+            ipCookie('machineDetailTourStep', $scope.steps['machineDetailTourStep']);
+            ipCookie('machineListTourStep', $scope.steps['machineListTourStep']);
         };
         
     }]);
