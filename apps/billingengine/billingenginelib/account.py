70cd0876aa28d5f02a009dd24e90d6b03a067de8
RobVanMieghem <rob@incubaid.com>
2014-07-25 17:10:36 +0200

    Add the ability to check for paying customers to the billingenginelib

---
+++ apps/billingengine/billingenginelib/account.py
@@ -0,0 +1,28 @@
+from JumpScale import j
+from JumpScale import portal
+import ujson
+
+class account(object):
+    def __init__(self):
+
+        self.cloudbrokermodels = j.core.osis.getClientForNamespace('cloudbroker')
+
+    def isPayingCustomer(self, accountId):
+        query = {'fields':['accountId']}
+
+        query['query'] = {
+                          'filtered':{
+                                      "query" :{
+                                                "bool":{"must":[
+                                                                {"term":{"status":"credit"}},
+                                                                {"term" : { "accountId" : accountId }}
+                                                                ]
+                                                        }
+                                                },
+                                      "filter" : {"exists":{"field":"reference"}}
+                                      }
+                          }
+
+        queryresult = self.cloudbrokermodels.creddittransaction.find(ujson.dumps(query))['result']
+        payments = [res['fields'] for res in queryresult]
+        return len(payments) > 0
