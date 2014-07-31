de3f05f417699035759208a9ddcb18858ee553e3
RobVanMieghem <rob@incubaid.com>
2014-07-31 14:11:46 +0200

    Accountvalidationcontroller stub

---
+++ apps/cloudscalers_fe/base/wiki_gcb/.files/js/controllers/SignUp/AccountValidationController.js
@@ -0,0 +1,5 @@
+angular.module('cloudscalers.controllers')
+    .controller('AccountValidationController', ['$scope', 'User','$window', '$timeout', function($scope, User, $window, $timeout) {
+
+
+    }]);
