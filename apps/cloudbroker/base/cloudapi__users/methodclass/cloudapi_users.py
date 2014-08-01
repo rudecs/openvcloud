7aa61dac41374ebd7e6fd7d57d121924c9286b63
Hendrik Demolder <hendrik.demolder@gmail.com>
2014-08-01 14:36:38 +0200

    some simple fixes to the email

--- apps/cloudbroker/base/cloudapi__users/methodclass/cloudapi_users.py
+++ apps/cloudbroker/base/cloudapi__users/methodclass/cloudapi_users.py
@@ -21,12 +21,17 @@ def _send_signup_mail(**kwargs):
 
     Dear,<br>
     <br>
-    Thank you for registering with Mothership<sup>1</sup>. You can now log in into mothership1 with your username %(username)s and your chosen password.
+    Thank you for registering with Mothership<sup>1</sup>.
+    <br>
+    <br>
+    You can now log in into mothership1 with your username %(username)s and your chosen password.
+    <br>
     <br>
     All new users are granted a free test drive for 2 weeks.
-<br>
-In case you experience any issues in logging in or using the Mothership<sup>1 service, please contact us at support@mothership1.com or use the live chat function on mothership1.com
-<br>
+    <br>
+    <br>
+    In case you experience any issues in logging in or using the Mothership<sup>1</sup> service, please contact us at support@mothership1.com or use the live chat function on mothership1.com
+    <br>
     <br>
     Best Regards,<br>
     <br>
