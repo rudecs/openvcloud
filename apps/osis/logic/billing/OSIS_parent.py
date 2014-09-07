053815cdabda386f994acdaae7b677ec95b8d8c2
Jo De Boeck <deboeckj@codescalers.com>
2014-09-07 09:32:12 +0300

    OSIS: model changes to work with mongodb

--- apps/osis/logic/billing/OSIS_parent.py
+++ apps/osis/logic/billing/OSIS_parent.py
@@ -8,20 +8,21 @@ class mainclass(OSISStoreMongo):
     Default object implementation
     """
 
-    def set(self, key, value, waitIndex=False):
+    def set(self, key, value, waitIndex=False, session=None):
         id = value.get('id')
-        if id and self.exists(id):
-            orig = self.get(id, True)
+        db, counter = self._getMongoDB(session)
+        if id and self.exists(id, session=session):
+            orig = self.get(id, True, session=session)
             orig.update(value)
             value = orig
             changed = True
             new = False
         else:
             if not id:
-                id = self.incrId()
+                id = self.incrId(counter)
                 value['id'] = id
             changed = False
             new = True
         value['guid'] = id
-        self.client.save(value)
+        db.save(value)
         return [id, new, changed]
