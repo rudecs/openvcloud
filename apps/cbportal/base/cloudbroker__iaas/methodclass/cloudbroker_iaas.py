0c0fee32764bb28372f245e13c0bb43f61e7aacd
Jo De Boeck <deboeckj@codescalers.com>
2014-09-08 15:24:24 +0300

    Add methods to add publicips

--- apps/cbportal/base/cloudbroker__iaas/methodclass/cloudbroker_iaas.py
+++ apps/cbportal/base/cloudbroker__iaas/methodclass/cloudbroker_iaas.py
@@ -1,7 +1,15 @@
 from JumpScale import j
 import JumpScale.grid.osis
+import netaddr
 json = j.db.serializers.ujson
 from JumpScale.portal.portal.auth import auth
+
+def checkIPS(network, ips):
+    for ip in ips:
+        if netaddr.IPAddress(ip) not in network:
+            return False
+    return True
+
 
 class cloudbroker_iaas(j.code.classGetBase()):
     """
@@ -20,13 +28,62 @@ class cloudbroker_iaas(j.code.classGetBase()):
             self._cb = j.apps.cloudbroker.iaas
         return self._cb
 
-    def addPublicIPv4Subnet(self, subnet, **kwargs):
+    def addPublicIPv4Subnet(self, subnet, gateway, freeips, **kwargs):
         """
         Adds a public network range to be used for cloudspaces
         param:subnet the subnet to add in CIDR notation (x.x.x.x/y)
         """
-        #put your code here to implement this method
-        raise NotImplementedError ("not implemented method addPublicIPv4Subnet")
+        ctx = kwargs["ctx"]
+        net = netaddr.IPNetwork(subnet)
+        if not checkIPS(net, freeips):
+            ctx.start_response("400 Bad Request")
+            return "One or more IP Addresses %s is not in subnet %s" % (subnet)
+        if not checkIPS(net, [gateway]):
+            ctx.start_response("400 Bad Request")
+            return "Gateway Address %s is not in subnet %s" % (gateway, subnet)
+
+        pool = self.cbcl.publicipv4pool.new()
+        pool.id = subnet
+        pool.gateway = gateway
+        pool.subnetmask = str(net.netmask)
+        pool.network = str(net.network)
+        pool.pubips = list(set(freeips))
+        self.cbcl.publicipv4pool.set(pool)
+        return subnet
+
+    def addPublicIPv4IPS(self, subnet, freeips, **kwargs):
+        """
+        Add public ips to an existing range
+        """
+        ctx = kwargs["ctx"]
+        if not self.cbcl.publicipv4pool.exists(subnet):
+            ctx.start_response("404 Not Found")
+            return "Could not find PublicIPv4Pool with subnet %s" % subnet
+        net = netaddr.IPNetwork(subnet)
+        if not checkIPS(net, freeips):
+            ctx.start_response("400 Bad Request")
+            return "One or more IP Addresses %s is not in subnet %s" % (subnet)
+        pool = self.cbcl.publicipv4pool.get(subnet)
+        pool.pubips = list(set(pool.pubips).union(set(freeips)))
+        self.cbcl.publicipv4pool.set(pool)
+        return subnet
+
+    def removePublicIPv4IPS(self, subnet, freeips, **kwargs):
+        """
+        Remove public ips from an existing range
+        """
+        ctx = kwargs["ctx"]
+        if not self.cbcl.publicipv4pool.exists(subnet):
+            ctx.start_response("404 Not Found")
+            return "Could not find PublicIPv4Pool with subnet %s" % subnet
+        net = netaddr.IPNetwork(subnet)
+        if not checkIPS(net, freeips):
+            ctx.start_response("400 Bad Request")
+            return "One or more IP Addresses %s is not in subnet %s" % (subnet)
+        pool = self.cbcl.publicipv4pool.get(subnet)
+        pool.pubips = list(set(pool.pubips) - set(freeips))
+        self.cbcl.publicipv4pool.set(pool)
+        return subnet
 
     @auth(['level1', 'level2'])
     def syncAvailableImagesToCloudbroker(self, **kwargs):
--- apps/cbportal/base/cloudbroker__iaas/specs/actor.spec
+++ apps/cbportal/base/cloudbroker__iaas/specs/actor.spec
@@ -13,3 +13,19 @@
         Adds a public network range to be used for cloudspaces
         """
         var:subnet str,, the subnet to add in CIDR notation (x.x.x.x/y)
+        var:gateway str,, gateway of the subnet
+        var:freeips list,, list of ips to mark as free in the subnet
+
+    method:addPublicIPv4IPS
+        """
+        Adds a public network range to be used for cloudspaces
+        """
+        var:subnet str,, the subnet to add in CIDR notation (x.x.x.x/y)
+        var:freeips list,, list of ips to mark as free in the subnet
+
+    method:removePublicIPv4IPS
+        """
+        Adds a public network range to be used for cloudspaces
+        """
+        var:subnet str,, the subnet to add in CIDR notation (x.x.x.x/y)
+        var:freeips list,, list of ips to mark as free in the subnet
