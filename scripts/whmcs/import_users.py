d5bcd75f49a832a8c7cc0651b58d6fdb0a1a7020
RobVanMieghem <rob@incubaid.com>
2014-09-08 17:49:56 +0200

    Do not delete the users at whmcs sync but update existing ones

--- scripts/whmcs/import_users.py
+++ scripts/whmcs/import_users.py
@@ -4,37 +4,32 @@ from JumpScale import grid
 
 import whmcsusers
 
+def main():
+    cb = j.core.osis.getClientForNamespace('cloudbroker')
+    system = j.core.osis.getClientForNamespace('system')
+    
+    accounts = cb.account.list()
+    users = whmcsusers.list_users()
+    
+    for i in accounts:
+      acc = cb.account.get(i)
+      linked_users = system.user.simpleSearch({'id':acc.name})
+      if len(linked_users) != 1:
+        continue
+      linked_user = linked_users[0]
+      linked_groups = system.group.simpleSearch({'id':acc.name})
+      if len(linked_groups) != 1:
+        continue
+      print linked_user
+      password = ''
+      if 'passwd' in linked_user:
+          password =linked_user['passwd']
+      if not acc.name in users.keys():
+          whmcsusers.create_user(acc.name, acc.company, linked_user['emails'], password, acc.companyurl, acc.displayname, acc.creationTime)
+      else:
+          whmcsusers.update_user(acc.name, acc.company, linked_user['emails'], password, acc.companyurl, acc.displayname, acc.creationTime)
 
 
-
-
-
-
-
-
-
-
-cb = j.core.osis.getClientForNamespace('cloudbroker')
-system = j.core.osis.getClientForNamespace('system')
-
-
-
-accounts = cb.account.list()
-users = whmcsusers.list_users()
-for user in users:
-    whmcsusers.delete_user(users[user]['id'])
-for i in accounts:
-  acc = cb.account.get(i)
-  linked_users = system.user.simpleSearch({'id':acc.name})
-  if len(linked_users) != 1:
-    continue
-  linked_user = linked_users[0]
-  linked_groups = system.group.simpleSearch({'id':acc.name})
-  if len(linked_groups) != 1:
-    continue
-  print linked_user
-  password = ''
-  if 'passwd' in linked_user:
-      password =linked_user['passwd']
-  if not acc.name in users.keys():
-      whmcsusers.create_user(acc.name, acc.company, linked_user['emails'], password, acc.companyurl, acc.displayname, acc.creationTime)
+if __name__ == "__main__":
+    main()
+    
--- scripts/whmcs/whmcsusers.py
+++ scripts/whmcs/whmcsusers.py
@@ -1,50 +1,59 @@
 import requests
 import hashlib
 
-
-
 authenticationparams = dict(
-
                             username = 'api',
-
                             password = hashlib.md5('kmmlqwkerjoi324mmkkjhapl02bc').hexdigest(),
-
                             accesskey = 'mmqewnlzklpo89ka234mkm2o1287kmmzbpldgej3'
-
                             )
+
+def _call_whmcs_api(requestparams):
+    actualrequestparams = dict()
+    actualrequestparams.update(requestparams)
+    actualrequestparams.update(authenticationparams)
+    response = requests.post('http://whmcsdev/whmcs/includes/api.php',data=actualrequestparams)
+    return reponse
 
 def create_user(name, company, emails, password, companyurl, displayname, creationTime):
     create_user_request_params = dict(
 
                 action = 'addclient',
-
                 responsetype = 'json',
-
                 firstname = name,
-
                 lastname = "",
-
                 companyname = company,
-
                 email = emails,
-
                 password2 = password,
-
                 country = "unknown",
-
                 currency = "1",
                 customfields = [companyurl, displayname, creationTime],
                 noemail = True,
                 skipvalidation= True
 
                 )
-    create_user_request_params.update(authenticationparams)
-    response = requests.post('http://whmcsdev/whmcs/includes/api.php',data=create_user_request_params)
+    
+    response = _call_whmcs_api(create_user_request_params)
     return response.ok
 
 
-def update_user():
-    print
+def update_user(name, company, emails, password, companyurl, displayname, creationTime):
+    
+    user_request_params = dict(
+
+                action = 'updateclient',
+                responsetype = 'json',
+                firstname = name,
+                companyname = company,
+                email = emails,
+                password2 = password,
+                customfields = [companyurl, displayname, creationTime],
+                noemail = True,
+                skipvalidation= True
+
+                )
+    
+    response = _call_whmcs_api(user_request_params)
+    return response.ok
 
 def list_users():
     result_users = {}
@@ -53,8 +62,8 @@ def list_users():
                 limitnum = 10000000,
                 responsetype = 'json'
                 )
-    list_users_request_params.update(authenticationparams)
-    response = requests.post('http://whmcsdev/whmcs/includes/api.php',data=list_users_request_params)
+
+    response = _call_whmcs_api(list_users_request_params)
     if response.ok:
         users = response.json()
         if users['numreturned'] > 0:
@@ -71,8 +80,8 @@ def delete_user(userid):
                 clientid=userid,
                 responsetype = 'json'
                 )
-    delete_users_request_params.update(authenticationparams)
-    response = requests.post('http://whmcsdev/whmcs/includes/api.php',data=delete_users_request_params)
+    
+    response = _call_whmcs_api(delete_users_request_params)
     return response.ok
 
 
@@ -84,8 +93,7 @@ def add_credit(userid, description, amount):
                 amount=amount,
                 responsetype = 'json'
                 )
-    add_credit_request_params.update(authenticationparams)
-    response = requests.post('http://whmcsdev/whmcs/includes/api.php',data=add_credit_request_params)
+    response = _call_whmcs_api(add_credit_request_params)
     return response.ok
 
 
