fab4f7c5135a66ddbc308b402fc22ba74a27d5e6
hendrik demolder <hendrik.demolder@gmail.com>
2014-09-26 16:12:21 +0000

    use instance for cloudbroker configuration

--- libs/agent-scripts/cloudbroker/cloudbroker_deploycloudspace.py
+++ libs/agent-scripts/cloudbroker/cloudbroker_deploycloudspace.py
@@ -20,11 +20,7 @@ def action(cloudspaceId):
     import JumpScale.grid.osis
     import JumpScale.portal
 
-    portalcfgpath = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'cloudbroker', 'cfg', 'portal')
-    portalcfg = j.config.getConfig(portalcfgpath).get('main', {})
-    port = int(portalcfg.get('webserverport', 9999))
-    secret = portalcfg.get('secret')
-    cl = j.core.portal.getClient('127.0.0.1', port, secret)
+    cl = j.core.portal.getClientByInstance('cloudbroker') 
     cloudspaceapi = cl.getActor('cloudapi','cloudspaces')
 
     return cloudspaceapi.deploy(cloudspaceId)
