203fedc174b6fcd3b5770ea5df203cbb15a78752
Jo De Boeck <deboeckj@codescalers.com>
2014-09-28 15:45:18 +0200

    Redo billingengine after mongo changes

--- apps/billingengine/base/billingengine__billingengine/methodclass/billingengine_billingengine.py
+++ apps/billingengine/base/billingengine__billingengine/methodclass/billingengine_billingengine.py
@@ -14,22 +14,9 @@ class billingengine_billingengine(j.code.classGetBase()):
         self._te={}
         self.actorname="billingengine"
         self.appname="billingengine"
-        #billingengine_billingengine_osis.__init__(self)
 
-        osiscl = j.core.osis.getClient(user='root')
-
-        class Class():
-            pass
-
-        self.billingenginemodels = Class()
-        for ns in osiscl.listNamespaceCategories('billing'):
-            self.billingenginemodels.__dict__[ns] = (j.core.osis.getClientForCategory(osiscl, 'billing', ns))
-            self.billingenginemodels.__dict__[ns].find = self.billingenginemodels.__dict__[ns].search
-
-        self.cloudbrokermodels = Class()
-        for ns in osiscl.listNamespaceCategories('cloudbroker'):
-            self.cloudbrokermodels.__dict__[ns] = (j.core.osis.getClientForCategory(osiscl, 'cloudbroker', ns))
-            self.cloudbrokermodels.__dict__[ns].find = self.cloudbrokermodels.__dict__[ns].search
+        self.billingenginemodels = j.core.osis.getClientForNamespace('billing')
+        self.cloudbrokermodels = j.core.osis.getClientForNamespace('cloudbroker')
 
         self._pricing = pricing.pricing()
 
@@ -58,7 +45,7 @@ class billingengine_billingengine(j.code.classGetBase()):
                 number_of_hours_in_previous_calculations = max(0.0,(fromTime - creationTime) / 3600.0)
                 if (number_of_hours_in_previous_calculations < 1.0):
                     number_of_billable_hours = 1.0 - number_of_hours_in_previous_calculations
-        
+
         return number_of_billable_hours
 
     def _update_machine_billingstatement(self, machinebillingstatement, machine, fromTime, untilTime):
@@ -75,47 +62,13 @@ class billingengine_billingengine(j.code.classGetBase()):
 
     def _update_usage(self, billing_statement):
 
-        query = {'fields': ['id', 'name', 'accountId', 'creationTime','deletionTime']}
-        query['query'] ={'filtered':{
-                          "query" : {'term': {'accountId': billing_statement.accountId}},
-                          "filter" : {
-                                        "and" : [
-                                                    {
-                                                        "not":{"range" : {"deletionTime" : {"lt" : billing_statement.fromTime, "gt":0}}}
-                                                    },
-                                                    {
-                                                        "not":{"range" : {"creationTime" : {"gt" : billing_statement.untilTime}}}
-                                                    }
-                                            ]
-                                      }
-                                 }
-                              }
-        
-        
-        results = self.cloudbrokermodels.cloudspace.find(ujson.dumps(query))['result']
-        cloudspaces = [res['fields'] for res in results]
+        query = {'accountId': billing_statement.accountId, 'deletionTime': {'$gt': billing_statement.fromTime, '$ne': 0}, 'creationTime': {'$lt': billing_statement.untilTime}}
+        cloudspaces = self.cloudbrokermodels.cloudspace.search(query)[1:]
 
 
         for cloudspace in cloudspaces:
-            query = {'fields':['id','creationTime','deletionTime','name','cloudspaceId','imageId','sizeId','disks']}
-
-            query['query'] = {'filtered':{
-                          "query" : {"term" : { "cloudspaceId" : cloudspace['id'] }},
-                          "filter" : {
-                                        "and" : [
-                                                    {
-                                                        "not":{"range" : {"deletionTime" : {"lt" : billing_statement.fromTime, "gt":0}}}
-                                                    },
-                                                    {
-                                                        "not":{"range" : {"creationTime" : {"gt" : billing_statement.untilTime}}}
-                                                    }
-                                            ]
-                                      }
-                                 }
-                              }
-
-            queryresult = self.cloudbrokermodels.vmachine.find(ujson.dumps(query))['result']
-            machines = [res['fields'] for res in queryresult]
+            query = {'cloudspaceId': cloudspace['id'], 'deletionTime': {'$gt': billing_statement.fromTime, '$ne': 0}, 'creationTime': {'$lt': billing_statement.untilTime}}
+            machines = self.cloudbrokermodels.vmachine.search(query)[1:]
 
             cloudspacebillingstatement = next((space for space in billing_statement.cloudspaces if space.cloudspaceId == cloudspace['id']), None)
 
@@ -215,20 +168,14 @@ class billingengine_billingengine(j.code.classGetBase()):
         self.cloudbrokermodels.credittransaction.set(creditTransaction)
 
     def _find_earliest_billable_action_time(self, accountId):
-        query = {'fields': ['id', 'name', 'accountId']}
-        query['query'] = {'term': {"accountId": accountId}}
-        results = self.cloudbrokermodels.cloudspace.find(ujson.dumps(query))['result']
-        cloudspaces = [res['fields'] for res in results]
-        cloudspaceterms = []
-        for cloudspace in cloudspaces:
-            cloudspaceterms.append({'term':{'cloudspaceId':cloudspace['id']}})
-        query = {'fields':['id','creationTime']}
-        query['query'] = {'bool':{'should':cloudspaceterms}}
-        query['size'] = 1
-        query['sort'] = [{ "creationTime" : {'order':'asc', 'ignore_unmapped' : True}}]
-
-        results = self.cloudbrokermodels.vmachine.find(ujson.dumps(query))['result']
-        return results[0]['fields']['creationTime'] if len(results) > 0 else None
+        query = {'accountId': accountId}
+        cloudspaces = self.cloudbrokermodels.cloudspace.search(query)[1:]
+        cloudspaceids = [ cloudspace['id'] for cloudspace in cloudspaces ]
+        query = {'$query': {'cloudspaceId': {'$in': cloudspaceids}}, 
+                 '$orderby': {'creationTime': 1}
+                }
+        results = self.cloudbrokermodels.vmachine.search(query, size=1)[1:]
+        return results[0]['creationTime'] if len(results) > 0 else None
 
 
     def _create_empty_billing_statements(self, fromTime, untilTime, accountId):
@@ -281,11 +228,8 @@ class billingengine_billingengine(j.code.classGetBase()):
         Updates the balance for an account given the credit/debit transactions
         param:accountId id of the account
         """
-        #For now, sum here, as of ES 1.0, can be done there
-        query = {'fields': ['time', 'credit', 'status']}
-        query['query'] = {'bool':{'must':[{'term': {"accountId": accountId}}],'must_not':[{'term':{'status':'UNCONFIRMED'.lower()}}]}}
-        results = self.cloudbrokermodels.credittransaction.find(ujson.dumps(query))['result']
-        history = [res['fields'] for res in results]
+        query = {'accountId': accountId, 'status': {'$ne': 'UNCONFIRMED'}}
+        history = self.cloudbrokermodels.credittransaction.search(query)[1:]
         balance = 0.0
         for transaction in history:
             balance += float(transaction['credit'])
