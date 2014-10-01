d2eb0eaf2d5bcbd86ae6bbad9676039f978f8f59
Jo De Boeck <deboeckj@codescalers.com>
2014-10-01 17:21:07 +0200

    Show cloudspaces an user has access to

---
+++ apps/cbportal/base/CBGrid/.macros/page/cbspacesusers/3_cbspaces.py
@@ -0,0 +1,34 @@
+
+def main(j, args, params, tags, tasklet):
+    page = args.page
+    modifier = j.html.getPageModifierGridDataTables(page)
+
+    filters = dict()
+    userId = args.getTag('userId')
+    userId = userId.split('_', 1)[-1]
+    if not userId:
+        pass
+
+    fieldnames = ['ID', 'Name', 'ACL', 'Status', ]
+    nativequery = {'acl.userGroupId' : userId}
+
+    def makeACL(row, field):
+        for acl in row['acl']:
+            if acl['userGroupId'] == userId:
+                return acl['right']
+        return ''
+
+    fieldids = ['id', 'name', 'acl', 'status']
+    fieldvalues = ['[%(id)s|/CBGrid/cloudspace?id=%(id)s]', 'name',
+                   makeACL, 'status' ]
+    tableid = modifier.addTableForModel('cloudbroker', 'cloudspace', fieldids, fieldnames, fieldvalues, filters, nativequery=nativequery)
+    modifier.addSearchOptions('#%s' % tableid)
+    modifier.addSorting('#%s' % tableid, 0, 'desc')
+
+    params.result = page
+
+    return params
+
+
+def match(j, args, params, tags, tasklet):
+    return True
