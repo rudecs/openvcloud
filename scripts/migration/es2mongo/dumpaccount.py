876c303c58e50b58fe9445a927b5ef3c3a602e40
Jo De Boeck <deboeck.jo@gmail.com>
2014-11-28 15:34:51 +0100

    Add migrations scripts

---
+++ scripts/migration/es2mongo/dumpaccount.py
@@ -0,0 +1,60 @@
+import json
+import sys
+from JumpScale import j
+def do(username):
+    data = {}
+    import JumpScale.grid
+    scl = j.core.osis.getClientForNamespace('system')
+    ccl = j.core.osis.getClientForNamespace('cloudbroker')
+    vcl = j.core.osis.getClientForNamespace('vfw')
+    grid = scl.grid.get(j.application.whoAmI.gid)
+    locationname = grid.name
+    user = scl.user.simpleSearch({'id': username})
+    if not user:
+        print "Could not find user"
+        sys.exit(0)
+
+    data['user'] = scl.user.get(user[0]['guid']).dump()
+    data['group'] = scl.group.get(user[0]['guid']).dump()
+    accounts = ccl.account.simpleSearch({'name': username})
+    if not accounts:
+        print 'Could not find account'
+        sys.exit(0)
+    accountId = accounts[0]['id']
+    data['account'] = ccl.account.get(accountId).dump()
+    data['sizes'] = ccl.size.simpleSearch({})
+    data['images'] = ccl.image.simpleSearch({})
+    data['stacks'] = ccl.stack.simpleSearch({})
+    cloudspaces = list()
+    data['cloudspaces'] = cloudspaces
+    for cloudspace in ccl.cloudspace.simpleSearch({'accountId': accountId}):
+        if cloudspace['status'] != 'DESTROYED' and cloudspace['location'] == locationname:
+            cloudspaces.append(cloudspace)
+    vmachines = list()
+    disks = list()
+    data['vmachines'] = vmachines
+    data['disks'] = disks
+    vfws = list()
+    data['vfws'] = vfws
+    for cloudspace in data['cloudspaces']:
+        for vmachine in ccl.vmachine.simpleSearch({'cloudspaceId': cloudspace['id']}):
+            if vmachine['status'] != 'DESTROYED':
+                vmachines.append(vmachine)
+                for disk in vmachines['disks']:
+                    disks.append(ccl.disk.get(disk).dump())
+        for vfw in vcl.virtualfirewall.simpleSearch({'domain': str(cloudspace['id'])}):
+            node = scl.node.get("%(gid)s_%(nid)s" % vfw)
+            vfw['nodename'] = node.name
+            vfws.append(vfw)
+
+    with open('%s.json' % username, 'w') as fd:
+        json.dump(data, fd)
+    #print data
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username')
+    opts = parser.parse_args()
+    do(opts.username)
---
+++ scripts/migration/es2mongo/importaccount.py
@@ -0,0 +1,97 @@
+import json
+import sys
+from JumpScale import j
+def do(username):
+    with open('%s.json' % username) as fd:
+        data = json.load(fd)
+    import JumpScale.grid
+    scl = j.core.osis.getClientForNamespace('system')
+    ccl = j.core.osis.getClientForNamespace('cloudbroker')
+    vcl = j.core.osis.getClientForNamespace('vfw')
+    locations = { location['locationCode']: location['gid'] for location in ccl.location.search({})[1:] }
+    disksbyid = { disk['id']: disk for disk in data['disks'] }
+
+
+    groups = scl.group.simpleSearch({'id': username})
+    if not groups:
+        scl.group.set(data['group'])
+
+    user = scl.user.simpleSearch({'id': username})
+    if not user:
+        scl.user.set(data['user'])
+
+    accounts = ccl.account.simpleSearch({'name': username})
+    if not accounts:
+        data['account'].pop('id', None)
+        accountId, _, _ = ccl.account.set(data['account'])
+    else:
+        accountId = accounts[0]['id']
+    print 'AccountId %s for user %s' % (accountId, username)
+
+    def getNewStackId(gid, stackId):
+        for stack in data['stacks']:
+            if stack['id'] == stackId:
+                stackname = stack['referenceId']
+                stacks = ccl.stack.search({'name': stackname, 'gid': gid})[1:]
+                if not stacks:
+                    print "Could not find stack for gid: %s with name:%s" % (gid, stackname)
+                    return 0
+                return stacks[0]['id']
+
+    def getNewSize(sizeId):
+        for size in data['sizes']:
+            if size['id'] == sizeId:
+                sizes = ccl.size.search({'vcpus': size['vcpus'], 'memory': size['memory']})[1:]
+                if not sizes:
+                    print "Could not find size with id:%" % sizeId
+                    return 0
+                return sizes[0]['id']
+
+    def getNewImage(imageId):
+        for image in data['images']:
+            if image['id'] == imageId:
+                images = ccl.image.search({'name': image['name'], 'size': image['size']})[1:]
+                if not images:
+                    print "Could not find image with id:%" % imageId
+                    return 0
+                return images[0]['id']
+
+    def getNid(gid, name):
+        nodes = scl.node.search({'gid': gid, 'name': name})[1:]
+        if not nodes:
+            print "Could not find node on gid: %s with name: %s" % (gid, name)
+        return nodes[0]['id']
+
+    for cloudspace in data['cloudspaces']:
+        oldspaceid = cloudspace.pop('id', None)
+        cloudspace['gid'] = locations[cloudspace['location']]
+        cloudspace['accountId'] = accountId
+        cloudspaceId, _, _ = ccl.cloudspace.set(cloudspace)
+        for vm in data['vmachines']:
+            if vm['cloudspaceId'] == oldspaceid:
+                vm['cloudspaceId'] = cloudspaceId
+                vm['sizeId'] = getNewSize(vm['sizeId'])
+                vm['imageId'] = getNewImage(vm['imageId'])
+                vm['stackId'] = getNewStackId(cloudspace['gid'], vm['stackId'])
+                diskids = vm['disks']
+                newdiskids = list()
+                vm['disks'] = newdiskids
+                for diskid in diskids:
+                    disk = disksbyid[diskid]
+                    disk.pop('id', None)
+                    newdisksid, _, _ = ccl.disk.set(disk)
+                    newdiskids.append(newdisksid)
+                ccl.vmachine.set(vm)
+
+    for vfw in data['vfws']:
+        nodename = vfw.pop('nodename')
+        nid = getNid(vfw['gid'], nodename)
+        vfw['nid'] = nid
+        vcl.virtualfirewall.set(vfw)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username')
+    opts = parser.parse_args()
+    do(opts.username)
