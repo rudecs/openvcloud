6cc324dfb03b5225ac6d5ede59821f1a3e661f22
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2014-12-22 10:53:58 +0100

    add location to vmachines macro

--- apps/cbportal/base/CBGrid/.macros/page/vmachines/3_vmachines.py
+++ apps/cbportal/base/CBGrid/.macros/page/vmachines/3_vmachines.py
@@ -21,7 +21,7 @@ def main(j, args, params, tags, tasklet):
         if images:
             filters['imageId'] = images[0]['id']
 
-    fieldnames = ['Name', 'Status', 'Host Name', 'Created at', 'Cloud Space', 'Stack']
+    fieldnames = ['Name', 'Status', 'Host Name', 'Created at', 'Cloud Space', 'Stack', 'Location']
 
     def makeTime(row, field):
         time = datetime.datetime.fromtimestamp(row[field]).strftime('%m-%d %H:%M:%S') or ''
@@ -35,9 +35,25 @@ def main(j, args, params, tags, tasklet):
 
     def spaceLinkify(row, field):
         return '[%s|cloudspace?id=%s]' % (row[field], row[field])
+    
+    locations = dict()
+    def locationLinkify(row, field):
+        sid = int(row[field])
+        if sid == 0:
+            return 'N/A'
+        gid = locations.get(sid)
+        if not gid:
+            ccl = j.core.osis.getClientForNamespace('cloudbroker') 
+            stack = ccl.stack.get(sid)
+            locations[sid] = stack.gid
+            gid = stack.gid
+        if gid:
+            return '[%s|location?id=%d]' % (gid,gid)
+        else:
+            return 'N/A'
 
-    fieldids = ['name', 'status', 'hostName', 'creationTime', 'cloudspaceId', 'stackId']
-    fieldvalues = [nameLinkify, 'status', 'hostName', makeTime, spaceLinkify, stackLinkify]
+    fieldids = ['name', 'status', 'hostName', 'creationTime', 'cloudspaceId', 'stackId', 'stackId']
+    fieldvalues = [nameLinkify, 'status', 'hostName', makeTime, spaceLinkify, stackLinkify, locationLinkify]
     tableid = modifier.addTableForModel('cloudbroker', 'vmachine', fieldids, fieldnames, fieldvalues, filters, nativequery)
     modifier.addSearchOptions('#%s' % tableid)
     modifier.addSorting('#%s' % tableid, 0, 'desc')
