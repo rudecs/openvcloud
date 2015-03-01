b2183c7e87708cb6ad3c01b317ee3b790f2a4ee5
Karim Ali <karim.ali.dev@gmail.com>
2015-03-01 13:53:08 +0200

    loader messages fix

--- apps/ms1_fe/base/wiki_gcb/.files/js/controllers/Machines/MachineEditController.js
+++ apps/ms1_fe/base/wiki_gcb/.files/js/controllers/Machines/MachineEditController.js
@@ -24,6 +24,7 @@ angular.module('cloudscalers.controllers')
         changeSelectedTab($routeParams.activeTab);
         
         var retrieveMachineHistory = function() {
+            $scope.machineHistory = {};
             Machine.getHistory($routeParams.machineId)
                 .then(function(data) {
                     $scope.machineHistory = data;
