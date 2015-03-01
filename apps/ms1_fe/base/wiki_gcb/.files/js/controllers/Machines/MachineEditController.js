d3826513cfea874c429aaff747ef5c88b1dc9a3a
Karim Ali <karim.ali.dev@gmail.com>
2015-03-01 13:51:27 +0200

    fix typo

--- apps/ms1_fe/base/wiki_gcb/.files/js/controllers/Machines/MachineEditController.js
+++ apps/ms1_fe/base/wiki_gcb/.files/js/controllers/Machines/MachineEditController.js
@@ -89,7 +89,7 @@ angular.module('cloudscalers.controllers')
                 if (machine){
                     machine.status = 'DESTROYED';
                 }
-                $scope.machines.splice( _.lwhere($scope.machines, {id: $scope.machine.id}) , 1);
+                $scope.machines.splice( _.where($scope.machines, {id: $scope.machine.id}) , 1);
                 $location.path("/list");
             });
         };
