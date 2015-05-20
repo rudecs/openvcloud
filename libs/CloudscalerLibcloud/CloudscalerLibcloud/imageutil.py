6ed716b3b6e718d122ff12ec9d2e2aa02d10e1c4
Jo De Boeck <deboeck.jo@gmail.com>
2015-05-20 09:34:28 +0200

    Convert images into ovs storage

--- libs/CloudscalerLibcloud/CloudscalerLibcloud/imageutil.py
+++ libs/CloudscalerLibcloud/CloudscalerLibcloud/imageutil.py
@@ -1,20 +1,42 @@
 from JumpScale import j
 import JumpScale.grid.osis
 import requests
+import sys
 
-def registerImage(jp, name, imagepath, type, disksize, username = None):
+
+def copyImageToOVS(imagepath):
+    sys.path.append('/opt/OpenvStorage')
+    from ovs.lib.vdisk import VDiskController
+    from ovs.dal.lists.vdisklist import VDiskList
+    from ovs.dal.lists.vpoollist import VPoolList
+
+    newimagepath = imagepath.replace('.qcow2', '.raw')
+    dstdir = '/mnt/vmstor/templates'
+    j.system.fs.createDir(dstdir)
+    src = j.system.fs.joinPaths('/opt/jumpscale7/var/tmp/templates/', imagepath)
+    dst = j.system.fs.joinPaths(dstdir, newimagepath)
+
+    if not j.system.fs.exists(dst):
+        j.system.platform.qemu_img.convert(src, 'qcow2', dst, 'raw')
+
+    pool = VPoolList.get_vpool_by_name('vmstor')
+    disk = VDiskList.get_by_devicename_and_vpool('templates/%s' % newimagepath, pool)
+    VDiskController.set_as_template(disk.guid)
+    return disk.guid, newimagepath
+
+def registerImage(jp, name, imagepath, type, disksize, username=None):
+    templateguid, imagepath = copyImageToOVS(imagepath)
     #register image on cloudbroker
     node_id = "%s_%s" % (j.application.whoAmI.gid, j.application.whoAmI.nid)
-    image_id = str(int(j.tools.hash.md5_string(jp.name)[0:9], 16))
     osiscl = j.clients.osis.getByInstance('main')
     catimageclient = j.clients.osis.getCategory(osiscl, 'libvirt', 'image')
     catresourceclient = j.clients.osis.getCategory(osiscl, 'libvirt', 'resourceprovider')
 
     installed_images = catimageclient.list()
-    if image_id not in installed_images:
+    if templateguid not in installed_images:
         image = dict()
         image['name'] = name
-        image['id'] = image_id
+        image['id'] = templateguid
         image['UNCPath'] = imagepath
         image['type'] = type
         image['size'] = disksize
@@ -27,11 +49,11 @@ def registerImage(jp, name, imagepath, type, disksize, username = None):
         rp['cloudUnitType'] = 'CU'
         rp['id'] = str(j.application.whoAmI.nid)
         rp['gid'] = j.application.whoAmI.gid
-        rp['images'] = [image_id]
+        rp['images'] = [templateguid]
     else:
         rp = catresourceclient.get(node_id)
-        if not image_id in rp.images:
-            rp.images.append(image_id)
+        if not templateguid in rp.images:
+            rp.images.append(templateguid)
     catresourceclient.set(rp)
 
     return True
