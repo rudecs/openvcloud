b7b24b56005e04765dce40a63659d35a268db647
Jo De Boeck <deboeck.jo@gmail.com>
2015-06-02 08:26:21 +0200

    Check on job status of ping result

--- libs/agent-scripts/cloudbroker/monitor/vms_check.py
+++ libs/agent-scripts/cloudbroker/monitor/vms_check.py
@@ -77,7 +77,9 @@ def action(gid=None):
     for idx, (vm_id, job) in enumerate(ping_jobs.iteritems()):
         print 'Waiting for %s/%s pingjobs' % (idx, len(ping_jobs))
         result = accl.waitJumpscript(job=job, timeout=0)
-        if result['result']:
+        if result['status'] != 'OK':
+            vmachines_data[vm_id]['ping'] = False
+        elif isinstance(result['result'], bool):
             vmachines_data[vm_id]['ping'] = result['result']
 
     for idx, (vm_id, job) in enumerate(disk_check_jobs.iteritems()):
