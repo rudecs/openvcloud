ecd3b586a32f2612b2f2afd7b43f6347116c2ff2
Karim Ali <karim.ali.dev@gmail.com>
2015-06-23 00:06:10 +0200

    Remove mountain top login page for oauth login sake & sync related process

--- apps/ms1_fe/base/wiki_gcb/.files/js/controllers/AuthenticatedSessionController.js
+++ apps/ms1_fe/base/wiki_gcb/.files/js/controllers/AuthenticatedSessionController.js
@@ -1,8 +1,35 @@
 angular.module('cloudscalers.controllers')
-    .controller('AuthenticatedSessionController', ['$scope', 'User', 'Account', 'CloudSpace', 'LoadingDialog', '$route', '$window','$timeout', '$location', function($scope, User, Account, CloudSpace, LoadingDialog, $route, $window, $timeout, $location) {
-        $scope.currentUser = User.current();
-        $scope.currentSpace = CloudSpace.current();
-        $scope.currentAccount = $scope.currentSpace ? {id:$scope.currentSpace.accountId, name:$scope.currentSpace.accountName, userRightsOnAccount: $scope.currentSpace.acl, userRightsOnAccountBilling: $scope.currentSpace.userRightsOnAccountBilling} : {id:''};
+    .controller('AuthenticatedSessionController', ['$scope', 'User', 'Account', 'CloudSpace', 'LoadingDialog', '$route', '$window','$timeout', '$location', 'ipCookie', function($scope, User, Account, CloudSpace, LoadingDialog, $route, $window, $timeout, $location, ipCookie) {
+
+
+        var portal_session_cookie = ipCookie("beaker.session.id");
+        if(portal_session_cookie){
+            if(!User.current() || User.current().api_key != portal_session_cookie){
+                User.getPortalLoggedinUser().then(function(username) {
+                    if(username != "guest"){
+                        loginAndRedirect(username);
+                    }
+                },
+                function(reason){
+                    $scope.login_error = reason.status
+                });
+
+            }else{
+                loginAndRedirect(User.current().username);
+            }
+        }
+
+        function loginAndRedirect(username) {
+            User.portalLogin(username, portal_session_cookie);
+            $scope.currentUser = User.current();
+            $scope.currentSpace = CloudSpace.current();
+            $scope.currentAccount = $scope.currentSpace ? {id:$scope.currentSpace.accountId, name:$scope.currentSpace.accountName, userRightsOnAccount: $scope.currentSpace.acl, userRightsOnAccountBilling: $scope.currentSpace.userRightsOnAccountBilling} : {id:''};
+            var target = 'Decks';
+            var uri = new URI($window.location);
+            uri.filename(target);
+            $window.location = uri.toString();
+        }
+
 
         $scope.setCurrentCloudspace = function(space) {
         	if (space == null)
@@ -61,24 +88,26 @@ angular.module('cloudscalers.controllers')
         };
 
         $scope.$watch('currentSpace', function () {
-            CloudSpace.get($scope.currentSpace.id).then(function (data) {
-                if($scope.currentSpace.acl.length != data.acl.length){
-                    $scope.currentSpace.acl = data.acl;
-                }
-            });
-            if($scope.currentUser.username && $scope.currentSpace.acl){
-                var currentUserAccessright =  _.find($scope.currentSpace.acl , function(acl) { return acl.userGroupId == $scope.currentUser.username; })
-                if(currentUserAccessright){
-                    currentUserAccessright = currentUserAccessright.right.toUpperCase();
-                    if(currentUserAccessright == "R"){
-                        $scope.currentUserAccessrightOnCloudSpace = 'Read';
-                    }else if( currentUserAccessright.indexOf('R') != -1 && currentUserAccessright.indexOf('C') != -1 && currentUserAccessright.indexOf('X') != -1 && currentUserAccessright.indexOf('D') == -1 && currentUserAccessright.indexOf('U') == -1){
-                        $scope.currentUserAccessrightOnCloudSpace = "ReadWrite";
-                    }else if(currentUserAccessright.indexOf('R') != -1 && currentUserAccessright.indexOf('C') != -1 && currentUserAccessright.indexOf('X') != -1 && currentUserAccessright.indexOf('D') != -1 && currentUserAccessright.indexOf('U') != -1){
-                        $scope.currentUserAccessrightOnCloudSpace = "Admin";
+            if($scope.currentSpace && $scope.currentUser){
+                CloudSpace.get($scope.currentSpace.id).then(function (data) {
+                    if($scope.currentSpace.acl.length != data.acl.length){
+                        $scope.currentSpace.acl = data.acl;
+                    }
+                });
+                if($scope.currentUser.username && $scope.currentSpace.acl){
+                    var currentUserAccessright =  _.find($scope.currentSpace.acl , function(acl) { return acl.userGroupId == $scope.currentUser.username; })
+                    if(currentUserAccessright){
+                        currentUserAccessright = currentUserAccessright.right.toUpperCase();
+                        if(currentUserAccessright == "R"){
+                            $scope.currentUserAccessrightOnCloudSpace = 'Read';
+                        }else if( currentUserAccessright.indexOf('R') != -1 && currentUserAccessright.indexOf('C') != -1 && currentUserAccessright.indexOf('X') != -1 && currentUserAccessright.indexOf('D') == -1 && currentUserAccessright.indexOf('U') == -1){
+                            $scope.currentUserAccessrightOnCloudSpace = "ReadWrite";
+                        }else if(currentUserAccessright.indexOf('R') != -1 && currentUserAccessright.indexOf('C') != -1 && currentUserAccessright.indexOf('X') != -1 && currentUserAccessright.indexOf('D') != -1 && currentUserAccessright.indexOf('U') != -1){
+                            $scope.currentUserAccessrightOnCloudSpace = "Admin";
+                        }
                     }
                 }
             }
-        });
 
-    }]);
+        });
+}]);
