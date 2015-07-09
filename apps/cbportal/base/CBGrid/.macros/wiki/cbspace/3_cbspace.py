00517371901e5a66131ac85c60cce6af458beca7
Jo De Boeck <deboeck.jo@gmail.com>
2015-07-09 23:17:05 +0200

    CBGRID: show permissions in an uniform way

--- apps/cbportal/base/CBGrid/.macros/wiki/cbspace/3_cbspace.py
+++ apps/cbportal/base/CBGrid/.macros/wiki/cbspace/3_cbspace.py
@@ -22,7 +22,7 @@ def main(j, args, params, tags, tasklet):
     space = cbclient.cloudspace.get(id)
 
     obj = space.dump()
-    
+
     accountid = obj['accountId']
     account = cbclient.account.get(accountid).dump() if cbclient.account.exists(accountid) else {'name':'N/A'}
     obj['accountname'] = account['name']
@@ -31,8 +31,6 @@ def main(j, args, params, tags, tasklet):
     for k, v in obj['resourceLimits'].iteritems():
         resourceLimits.append(' *%s*: %s'% (k, str(v)))
     obj['resourceLimits'] = str(', '.join(resourceLimits))
-
-    obj['acl'] = str(', '.join([' *%s*:%s' % (acl['userGroupId'], acl['right']) for acl in obj['acl']]))
 
     vfwkey = "%(gid)s_%(networkId)s" % (obj)
     if vcl.virtualfirewall.exists(vfwkey):
