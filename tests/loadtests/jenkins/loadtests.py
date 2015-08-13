1f9bb49ac8f8dec9104a0a6711d2508da82a9b02
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-08-13 13:16:58 +0200

    Auto PEP8

--- tests/loadtests/jenkins/loadtests.py
+++ tests/loadtests/jenkins/loadtests.py
@@ -15,13 +15,15 @@ URL = 'http://cpu01.lenoir1.vscalers.com/'
 USERNAME = 'loadtester'
 PASSWORD = 'rooterR00t3r'
 CLOUD_SPACE_ID = 91
-SIZEIDS = [2]
+UBUNNTU_SIZE_IDS = [2]
+WINDOWS_SIZE_IDS = [4]
 UBUNTU_IMAGE_ID = 2
 WINDOWS_IMAGE_ID = 4
 WINDOWS_DISK_SIZE = 16
 UBUNTU_DISK_SIZE = 10
 
 PUBLICIP = '192.198.94.16'
+
 
 def _clearIpFromAuthorizedHosts(ip, port):
     """
@@ -30,33 +32,37 @@ def _clearIpFromAuthorizedHosts(ip, port):
     but this causes problem when trying to use ssh because Same IP address has different fingerprint.
     We need to clear the IP first from authorized hosts every time
     """
-    home =  expanduser("~")
+    home = expanduser("~")
     knownhostspath = os.path.join(home, '.ssh', 'known_hosts')
-    process = subprocess.Popen(['ssh-keygen', '-f', knownhostspath, '-R' ,'[%s]:%s' % (ip, port)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(['ssh-keygen', '-f', knownhostspath, '-R',
+                                '[%s]:%s' % (ip, port)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return process.communicate()
-    
-def _getPackageName(sizeId):
+
+
+def _getPackageName(size_id):
     """
-    Returns Package cpu/ram in one word using sizeID
+    Returns Package cpu/ram in one word using size_id
     """
-    if sizeId == 1:
+    if size_id == 1:
         return '512G_1CPU'
-    elif sizeId == 2:
+    elif size_id == 2:
         return '1G_1CPU'
-    elif sizeId == 3:
+    elif size_id == 3:
         return '2G_2CPU'
-    elif sizeId == 4:
+    elif size_id == 4:
         return '4G_2CPU'
-    elif sizeId == 5:
+    elif size_id == 5:
         return '8G_4CPU'
-    elif sizeId == 6:
+    elif size_id == 6:
         return '16G_8CPU'
 
+
 def _get_random_port(exposedmachines):
-    exposed_ports = [int(x['publicPort']) for x in exposedmachines ]
+    exposed_ports = [int(x['publicPort']) for x in exposedmachines]
     for port in range(1024, 30000):
         if port not in exposed_ports:
             return port
+
 
 def _keyGen(url, username, password):
     try:
@@ -69,10 +75,12 @@ def _keyGen(url, username, password):
         else:
             raise
 
+
 def createMachine(id, imageid, disksize, sizeid, authkey):
-    name = "%s.Test.%s" % (str(id), uuid.uuid4()) 
-    createurl = '%s/restmachine/cloudapi/machines/create?cloudspaceId=%s&name=%s&description=&sizeId=%s&imageId=%s&disksize=%s&authkey=%s' % (
-            URL, CLOUD_SPACE_ID, name, sizeid, imageid, disksize, authkey)
+    name = "%s.Test.%s" % (str(id), uuid.uuid4())
+    createurl = ('%s/restmachine/cloudapi/machines/create?cloudspaceId=%s&name=%s&' +
+                 'description=&sizeId=%s&imageId=%s&disksize=%s&authkey=%s') % (
+        URL, CLOUD_SPACE_ID, name, sizeid, imageid, disksize, authkey)
     res = requests.get(createurl)
     if not res.ok:
         raise Exception(res.text)
@@ -84,24 +92,26 @@ def createMachine(id, imageid, disksize, sizeid, authkey):
 def exposeMachine(machineid, authkey):
     # Make sure Machine has Ip address
     getMachine(machineid, authkey)
-    
+
     listurl = '%s/restmachine/cloudapi/portforwarding/list?cloudspaceid=%s&protocol=tcp&authkey=%s' % (
-            URL, CLOUD_SPACE_ID, authkey)
-    
+        URL, CLOUD_SPACE_ID, authkey)
+
     res = requests.get(listurl)
     if not res.ok:
         raise Exception(res.text)
 
     publicport = _get_random_port(res.json())
-    
-    exposeurl = '%s/restmachine/cloudapi/portforwarding/create?cloudspaceid=%s&protocol=tcp&localPort=22&vmid=%s&publicIp=%s&publicPort=%s&authkey=%s' % (
-            URL, CLOUD_SPACE_ID, machineid, PUBLICIP, publicport, authkey)
+
+    exposeurl = ('%s/restmachine/cloudapi/portforwarding/create?cloudspaceid=%s&' +
+                 'protocol=tcp&localPort=22&vmid=%s&publicIp=%s&publicPort=%s&authkey=%s') % (
+        URL, CLOUD_SPACE_ID, machineid, PUBLICIP, publicport, authkey)
     res = requests.get(exposeurl)
     if not res.ok:
         raise Exception(res.text)
-    print "Exposed machine: %s through IP %s and port %s" % (str(machineid),PUBLICIP, str(publicport))
+    print "Exposed machine: %s through IP %s and port %s" % (str(machineid), PUBLICIP, str(publicport))
     sys.stdout.flush()
     return publicport
+
 
 def getMachine(machineid, authkey):
     host = 'Undefined'
@@ -110,7 +120,8 @@ def getMachine(machineid, authkey):
         print "Trying to Get IP address for machine : %s" % machineid
         sys.stdout.flush()
         time.sleep(5)
-        geturl = '%s/restmachine/cloudapi/machines/get?machineId=%s&authkey=%s' % (URL, machineid, authkey)
+        geturl = '%s/restmachine/cloudapi/machines/get?machineId=%s&authkey=%s' % (
+            URL, machineid, authkey)
         res = requests.get(geturl)
         vm = res.json()
         host = vm['interfaces'][0]['ipAddress']
@@ -118,42 +129,46 @@ def getMachine(machineid, authkey):
     sys.stdout.flush()
     return vm
 
+
 def installAndRunUnixBench(machineids, authkey, hosts_public_ports):
     for machineid in machineids:
         sshhost = PUBLICIP
         sshport = hosts_public_ports[machineid]
         _clearIpFromAuthorizedHosts(sshhost, sshport)
-        
+
         print "Installing UnixBench on machine %s" % str(machineid)
         sys.stdout.flush()
         vm = getMachine(machineid, authkey)
         sshpassword = vm['accounts'][0]['password']
         sshusername = vm['accounts'][0]['login']
-        
+
         # First time SSH we need to say yes to continue communication
         ssh_newkey = 'Are you sure you want to continue connecting'
-        p=pexpect.spawn('ssh %s@%s -p %s' % (sshusername, sshhost, sshport))
-        i=p.expect([ssh_newkey,'password:',pexpect.EOF])
-        if i==0:
+        p = pexpect.spawn('ssh %s@%s -p %s' % (sshusername, sshhost, sshport))
+        i = p.expect([ssh_newkey, 'password:', pexpect.EOF])
+        if i == 0:
             print "Continue connection"
             p.sendline('yes')
-        
+
         ssh = pxssh.pxssh()
         if not ssh.login(sshhost, sshusername, sshpassword, port=sshport):
             print "SSH session failed on login."
             print str(ssh)
         else:
             print "SSH session login successful"
-            ssh.sendline('echo %s | sudo -S apt-get install tmux' % sshpassword)
+            ssh.sendline('echo %s | sudo -S apt-get install tmux' %
+                         sshpassword)
             ssh.prompt()
             print ssh.before
-            ssh.sendline('wget https://byte-unixbench.googlecode.com/files/UnixBench5.1.3.tgz')
+            ssh.sendline(
+                'wget https://byte-unixbench.googlecode.com/files/UnixBench5.1.3.tgz')
             ssh.prompt()
             print ssh.before
             ssh.sendline('tar -xzvf UnixBench5.1.3.tgz')
             ssh.prompt()
             print ssh.before
-            ssh.sendline('echo %s | sudo -S apt-get install -y gcc make' % sshpassword)
+            ssh.sendline(
+                'echo %s | sudo -S apt-get install -y gcc make' % sshpassword)
             ssh.prompt()
             print ssh.before
             ssh.sendline('cd UnixBench')
@@ -162,14 +177,17 @@ def installAndRunUnixBench(machineids, authkey, hosts_public_ports):
             ssh.sendline('echo %s | sudo -S make' % sshpassword)
             ssh.prompt()
             print ssh.before
-            ssh.sendline('tmux new-session -s Test -d "echo %s | sudo -S ./Run >> UnixBench5screen"' % sshpassword)
+            ssh.sendline(
+                'tmux new-session -s Test -d "echo %s | sudo -S ./Run >> UnixBench5screen"' % sshpassword)
             ssh.prompt()
             print ssh.before
             ssh.logout()
-        
+
+
 def collectNixStats(machineids, authkey, hosts_public_ports):
     """
-    fab -H 192.198.94.16 -u cloudscalers -p r48mC47bD --port 23 -- 'cat UnixBench/UnixBench5screen | grep -i "System Benchmarks Index Score" |  grep -o [0-9].*'
+    fab -H 192.198.94.16 -u cloudscalers -p r48mC47bD --port 23 -- 'cat UnixBench/UnixBench5screen |
+        grep -i "System Benchmarks Index Score" |  grep -o [0-9].*'
     """
     result_single = []
     result_multi = []
@@ -182,8 +200,9 @@ def collectNixStats(machineids, authkey, hosts_public_ports):
         vm = getMachine(machineid, authkey)
         sshpassword = vm['accounts'][0]['password']
         sshusername = vm['accounts'][0]['login']
-        
-        fabb_conn = 'fab -H %s -u %s -p %s --port=%s -- cat UnixBench/UnixBench5screen | grep -i "System Benchmarks Index Score" |  grep -o [0-9].*' % (
+
+        fabb_conn = ('fab -H %s -u %s -p %s --port=%s -- cat UnixBench/UnixBench5screen |' +
+                     ' grep -i "System Benchmarks Index Score" |  grep -o [0-9].*') % (
             sshhost, sshusername, sshpassword, sshport)
         output = ""
         try:
@@ -193,22 +212,24 @@ def collectNixStats(machineids, authkey, hosts_public_ports):
             sys.stdout.flush()
             return []
         result = output.split()
-        indexes = [i for i,x in enumerate(result) if x == 'Score']
+        indexes = [i for i, x in enumerate(result) if x == 'Score']
         if len(indexes) == 1:
-            scoresingle = result[indexes[0]+1]
+            scoresingle = result[indexes[0] + 1]
             result_single.append(scoresingle)
         elif len(indexes) == 2:
-            scoresingle = result[indexes[0]+1]
-            scoremulti = result[indexes[1]+1]
+            scoresingle = result[indexes[0] + 1]
+            scoremulti = result[indexes[1] + 1]
             result_single.append(scoresingle)
             result_multi.append(scoremulti)
         else:
-            raise  ("Invslid data" + output)
-    final = [reduce(add, [float(x) for x in result_single]) / len(result_single)]
+            raise ("Invslid data" + output)
+    final = [reduce(add, [float(x)
+                          for x in result_single]) / len(result_single)]
     if result_multi:
         m = reduce(add, [float(x) for x in result_multi]) / len(result_multi)
         final.append(m[0])
     return final
+
 
 def generateStatsCSVs(stats):
     """
@@ -218,9 +239,9 @@ def generateStatsCSVs(stats):
     sys.stdout.flush()
     for system, packagestat in stats.iteritems():
         for packagename, values in packagestat.iteritems():
-            cwd = os.path.dirname(os.path.abspath(__file__))              
-            filename = os.path.join(cwd, '%s.%s.csv' % (system, packagename)) 
-            csv_file = open(filename, 'wa')                               
+            cwd = os.path.dirname(os.path.abspath(__file__))
+            filename = os.path.join(cwd, '%s.%s.csv' % (system, packagename))
+            csv_file = open(filename, 'wa')
             csv_out = csv.writer(csv_file, delimiter=',')
             if not len(values):
                 continue
@@ -228,7 +249,8 @@ def generateStatsCSVs(stats):
                 csv_out.writerow(['One Single Run'])
                 csv_out.writerow([values[0]])
             else:
-                csv_out.writerow(['One Single Run', 'Parallel Runs across all cores'])
+                csv_out.writerow(
+                    ['One Single Run', 'Parallel Runs across all cores'])
                 csv_out.writerow([values[0], values[1]])
 
 if __name__ == '__main__':
@@ -237,34 +259,44 @@ if __name__ == '__main__':
         linuxMachinesCount = int(sys.argv[2])
         total = winMachinesCount + linuxMachinesCount
         authkey = _keyGen(URL, USERNAME, PASSWORD)
-        MACHINES = {'ubuntu':{}, 'windows':{}}
-        STATS = {'ubuntu':{}, 'windows':{}}
-        PUBLIC_PORTS = {}
-        for sizeid in SIZEIDS:
+        machines = {'ubuntu': {}, 'windows': {}}
+        stats = {'ubuntu': {}, 'windows': {}}
+        public_ports = {}
+        for sizeid in UBUNNTU_SIZE_IDS:
             packagename = _getPackageName(sizeid)
-            MACHINES['ubuntu'][packagename] = []
-            MACHINES['windows'][packagename] = []
-            STATS['ubuntu'][packagename] = []
-            STATS['windows'][packagename] = []
+            machines['ubuntu'][packagename] = []
+            machines['windows'][packagename] = []
+            stats['ubuntu'][packagename] = []
+            stats['windows'][packagename] = []
 
             for i in range(linuxMachinesCount):
-                mid = createMachine(i, UBUNTU_IMAGE_ID, UBUNTU_DISK_SIZE, sizeid, authkey)
+                mid = createMachine(
+                    i, UBUNTU_IMAGE_ID, UBUNTU_DISK_SIZE, sizeid, authkey)
                 pubport = exposeMachine(mid, authkey)
-                PUBLIC_PORTS[mid] = pubport
-                MACHINES['ubuntu'][packagename].append(mid)
-        
+                public_ports[mid] = pubport
+                machines['ubuntu'][packagename].append(mid)
+
+        # for sizeif in WINDOWS_SIZE_IDS:
+        #     for i in range(winMachinesCount):
+        #         mid = createMachine(
+        #             i, UBUNTU_IMAGE_ID, UBUNTU_DISK_SIZE, sizeid, authkey)
+        #         pubport = exposeMachine(mid, authkey)
+        #         public_ports[mid] = pubport
+        #         machines['ubuntu'][packagename].append(mid)
+
         # Install benchmark on all machines
         all_linux_machines = []
-        for _, v in MACHINES['ubuntu'].iteritems():
+        for _, v in machines['ubuntu'].iteritems():
             all_linux_machines.extend(v)
-        installAndRunUnixBench(all_linux_machines, authkey, PUBLIC_PORTS)
+        installAndRunUnixBench(all_linux_machines, authkey, public_ports)
 
-        for packagename in MACHINES['ubuntu'].keys():
-            STATS['ubuntu'][packagename] =  collectNixStats(MACHINES['ubuntu'][packagename], authkey, PUBLIC_PORTS)
-#             STATS['ubuntu'][packagename] =  collectNixStats([169], authkey, PUBLIC_PORTS)
-    
-        generateStatsCSVs(STATS)
-        
+        for packagename in machines['ubuntu'].keys():
+            stats['ubuntu'][packagename] = collectNixStats(
+                machines['ubuntu'][packagename], authkey, public_ports)
+#             STATS['ubuntu'][packagename] =  collectNixStats([169], authkey, public_ports)
+
+        generateStatsCSVs(stats)
+
     except (ValueError, IndexError):
         print "USAGE: python loadtests.py numOfwindowsmachines numOfLinuxMachines"
         sys.stdout.flush()
