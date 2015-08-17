8e9c9709d500f47a66e812c8b8a2c30cfeac2400
Henri Verroken <henriverroken@gmail.com>
2015-08-17 15:56:41 +0200

    Setup angular material

--- apps/oauthserver/static/app.js
+++ apps/oauthserver/static/app.js
@@ -2,14 +2,19 @@
 
 angular.module('oauthserver', [
   'ngRoute',
+  'ngMaterial',
 ]);
 
 angular.module('oauthserver').config([
   '$routeProvider',
-  function($routeProvider) {
+  '$mdThemingProvider',
+  function($routeProvider, $mdThemingProvider) {
     $routeProvider
       .when('/login/oauth/authorize', {
         templateUrl: 'login/login.html',
       });
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('grey');
   }
 ]);
