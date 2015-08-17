d5c4a39c663dff6378e5209f768dbc55e4d40edc
Henri Verroken <henriverroken@gmail.com>
2015-08-17 15:08:15 +0200

    Setup routing in angular app

--- apps/oauthserver/static/app.js
+++ apps/oauthserver/static/app.js
@@ -3,3 +3,13 @@
 angular.module('oauthserver', [
   'ngRoute',
 ]);
+
+angular.module('oauthserver').config([
+  '$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/login/oauth/authorize', {
+        templateUrl: 'login/login.html',
+      });
+  }
+]);
