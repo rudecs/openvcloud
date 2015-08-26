be56aca9032b0a527a2282162019a9b31adafdb0
hamdy <hamdy.farag@codescalers.com>
2015-08-26 11:30:19 +0300

    vmachines macro, cloudspace name instead of cloudspace id

--- apps/cbportal/base/CBGrid/.macros/page/vmachines/3_vmachines.py
+++ apps/cbportal/base/CBGrid/.macros/page/vmachines/3_vmachines.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 def main(j, args, params, tags, tasklet):
     page = args.page
@@ -30,25 +31,29 @@ def main(j, args, params, tags, tasklet):
         stacksids = [ stack['id'] for stack in stacks ]
         filters['stackId'] = stacksids
 
-    fieldnames = ['Name', 'Status', 'Host Name', 'Created at', 'Cloud Space', 'CPU Node']
+    fieldnames = ['Name', 'Status', 'Host Name', 'Created at', 'Cloud Space Name', 'Stack']
 
-    def stackLinkify(row, field):
-        return '[%s|stack?id=%s]' % (row[field], row[field])
-
-    def nameLinkify(row, field):
-        return '[%s|vmachine?id=%s]' % (row[field], row['id'])
-
-    def spaceLinkify(row, field):
-        return '[%s|cloudspace?id=%s]' % (row[field], row[field])
-
-    fieldids = ['name', 'status', 'hostName', 'creationTime', 'cloudspaceId', 'stackId']
-    fieldvalues = [nameLinkify, 'status', 'hostName', modifier.makeTime, spaceLinkify, stackLinkify]
-    tableid = modifier.addTableForModel('cloudbroker', 'vmachine', fieldids, fieldnames, fieldvalues, filters)
+    def _formatData():
+        res = []
+        cbcl = j.clients.osis.getNamespace('cloudbroker')
+        for vmid in cbcl.vmachine.list():
+            vm = cbcl.vmachine.get(vmid)
+            name = '<a href="vmachine?id=%s">%s</a>' % (vm.id, vm.name)
+            csname = cbcl.cloudspace.get(int(vm.cloudspaceId)).name
+            cloudspace = '<a href="cloudspace?id=%s">%s</a>' % (vm.cloudspaceId,csname)
+            stack = ''
+            if vm.stackId:
+                stack = '<a href="stack?id=%s">%s</a>' % (vm.stackId, vm.stackId)
+            time = '<div class="jstimestamp" data-ts="%s"></div>' % vm.creationTime
+            res.append([name, vm.status, vm.hostName, time, cloudspace, stack])
+        return json.dumps(res)
+    
+    fieldvalues = _formatData()
+    tableid = modifier.addTableFromData(fieldvalues, fieldnames)
     modifier.addSearchOptions('#%s' % tableid)
     modifier.addSorting('#%s' % tableid, 0, 'desc')
 
     params.result = page
-
     return params
 
 
