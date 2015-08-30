4e0305d17bab323416d3b7a7369220499a5f4df2
Jo De Boeck <deboeck.jo@gmail.com>
2015-08-30 09:57:45 +0200

    Add pages to display public network details

---
+++ apps/cbportal/base/CBGrid/.macros/page/publicnetworks/3_publicnetworks.py
@@ -0,0 +1,27 @@
+def main(j, args, params, tags, tasklet):
+    page = args.page
+    modifier = j.html.getPageModifierGridDataTables(page)
+
+    filters = dict()
+    gid = args.getTag('gid')
+    if gid:
+        filters['gid'] = int(gid)
+
+    fieldnames = ['Network', 'GID', 'Netmask', 'Free']
+
+    def getFreeIPS(row, id):
+        return str(len(row[id]))
+
+    fieldids = ['id', 'gid', 'netmask', 'pubips']
+    fieldvalues = ['[%(id)s|publicnetwork?networkid=%(id)s]', 'gid', 'subnetmask', getFreeIPS]
+    tableid = modifier.addTableForModel('cloudbroker', 'publicipv4pool', fieldids, fieldnames, fieldvalues, filters)
+    modifier.addSearchOptions('#%s' % tableid)
+    modifier.addSorting('#%s' % tableid, 0, 'desc')
+
+    params.result = page
+
+    return params
+
+
+def match(j, args, params, tags, tasklet):
+    return True
---
+++ apps/cbportal/base/CBGrid/.macros/wiki/publicnetwork/3_publicnetwork.py
@@ -0,0 +1,31 @@
+def main(j, args, params, tags, tasklet):
+    import netaddr
+
+    id = args.getTag('networkid')
+    if not id:
+        out = 'Missing networkid param "id"'
+        params.result = (out, args.doc)
+        return params
+    
+    cbclient = j.clients.osis.getNamespace('cloudbroker')
+
+    if not cbclient.publicipv4pool.exists(id):
+        params.result = ('Account with id %s not found' % id, args.doc)
+        return params
+
+    network = cbclient.publicipv4pool.get(id).dump()
+    network['breadcrumbname'] = network['id']
+    network['spaces'] = list()
+
+    networkpool = netaddr.IPNetwork(network['id'])
+
+    for space in cbclient.cloudspace.search({'$query': {'gid': network['gid'], 'status': 'DEPLOYED'}, '$fields': ['id', 'publicipaddress']})[1:]:
+        if netaddr.IPNetwork(space['publicipaddress']).ip in networkpool:
+            network['spaces'].append(space)
+
+    args.doc.applyTemplate(network)
+    params.result = (args.doc, args.doc)
+    return params
+
+def match(j, args, params, tags, tasklet):
+    return True
