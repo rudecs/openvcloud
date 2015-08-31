b911f222a784e6bdc803ec7890a2e3b0213b0d77
hamdy <hamdy.farag@codescalers.com>
2015-08-31 12:48:09 +0300

    hypervisor type to be added to vmachine info

--- apps/cbportal/base/CBGrid/.macros/wiki/vmachine/3_vmachine.py
+++ apps/cbportal/base/CBGrid/.macros/wiki/vmachine/3_vmachine.py
@@ -98,7 +98,7 @@ def main(j, args, params, tags, tasklet):
         data['spacename'] = space['name']
         data['stackrefid'] = stack['referenceId'] or 'N/A'
         data['breadcrumbname'] = obj.name
-        data['hypervisortype'] = obj.hypervisorType
+        data['hypervisortype'] = stack['type']
 
         for k, v in stats.iteritems():
             if k == 'epoch':
