fd072cef8dca88101b22c459b3600ac3bb34db99
Jo De Boeck <deboeck.jo@gmail.com>
2015-08-31 09:01:20 +0200

    Fix links to grid node

--- apps/cbportal/base/CBGrid/.macros/page/cbnodes/3_cbnodes.py
+++ apps/cbportal/base/CBGrid/.macros/page/cbnodes/3_cbnodes.py
@@ -30,7 +30,7 @@ def main(j, args, params, tags, tasklet):
         return str(', '.join(row[field]))
 
     fieldids = ['id', 'name', 'ipaddr']
-    fieldvalues = ['[%(id)s|/Grid/node?id=%(id)s&gid=%(gid)s]', 'name', makeIPs]
+    fieldvalues = ['[%(id)s|/Grid/grid node?id=%(id)s&gid=%(gid)s]', 'name', makeIPs]
     tableid = modifier.addTableForModel('system', 'node', fieldids, fieldnames, fieldvalues, filters)
     modifier.addSearchOptions('#%s' % tableid)
     modifier.addSorting('#%s' % tableid, 0, 'desc')
