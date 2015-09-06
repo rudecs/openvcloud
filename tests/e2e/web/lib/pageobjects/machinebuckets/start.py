84bfea29ece033b408d1d1b5a3a5260613c05def
kristof de spiegeleer <kristof@incubaid.com>
2015-09-06 10:50:34 +0200

    test code moved to other repo

--- tests/e2e/web/lib/pageobjects/machinebuckets/start.py
+++
@@ -1,9 +0,0 @@
-from pageobjects.pageobject import PageObject
-from newmachine import NewMachine
-
-class Start(PageObject):
-    
-    def create_machine(self):
-        self.waitXpath('//button[@id="singlebutton"]')
-        self.driver.find_element_by_id('singlebutton').click()
-        return NewMachine(self.driver)
---
+++ tests/readme.md
@@ -0,0 +1,2 @@
+code moved to
+https://git.aydo.com/despiegk/OpenVCloud_2_0_tests
