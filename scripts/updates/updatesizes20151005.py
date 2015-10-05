c2fd19dd876964c0db51d61eee4e3527e524b23f
Jo De Boeck <deboeck.jo@gmail.com>
2015-10-05 14:54:27 +0200

    Remove obsolete data

--- scripts/updates/updatesizes20151005.py
+++ scripts/updates/updatesizes20151005.py
@@ -12,20 +12,20 @@ for sizeid in ccl.size.list():
 
 # recreate sizes in libvirt
 lcl = j.clients.osis.getNamespace('libvirt')
-sizecbs = [('10GB at SSD Speed, Unlimited Transfer - 7.5 USD/month', 512, 1),
-         ('10GB at SSD Speed, Unlimited Transfer - 15 USD/month', 1024, 1),
-         ('10GB at SSD Speed, Unlimited Transfer - 18 USD/month', 2048, 2),
-         ('10GB at SSD Speed, Unlimited Transfer - 36 USD/month', 4096, 2),
-         ('10GB at SSD Speed, Unlimited Transfer - 70 USD/month', 8192, 4),
-         ('10GB at SSD Speed, Unlimited Transfer - 140 USD/month', 16384, 8)]
+sizecbs = [(512, 1),
+         (1024, 1),
+         (2048, 2),
+         (4096, 2),
+         (8192, 4),
+         (16384, 8)]
 lcl.size.deleteSearch({})
 for i in disksizes:
     for sizecb in sizecbs:
-        if lcl.size.search({'memory': sizecb[1], 'disk': i})[0]:
+        if lcl.size.search({'memory': sizecb[0], 'disk': i})[0]:
             continue
         size = dict()
         size['disk'] = i
-        size['memory'] = sizecb[1]
-        size['name'] = '%i-%i' % (sizecb[1], i)
-        size['vcpus'] = sizecb[2]
+        size['memory'] = sizecb[0]
+        size['name'] = '%i-%i' % (sizecb[0], i)
+        size['vcpus'] = sizecb[1]
         lcl.size.set(size)
