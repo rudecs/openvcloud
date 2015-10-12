38af2d1d789a7c2a05582e0b5fc1fe604937faad
Jo De Boeck <deboeck.jo@gmail.com>
2015-10-12 21:30:23 +0200

    Make some improvements on networkperformance test

--- libs/agent-scripts/cloudbroker/monitor/networkperformance.py
+++ libs/agent-scripts/cloudbroker/monitor/networkperformance.py
@@ -8,19 +8,21 @@ order = 1
 enable = True
 async = True
 log = True
-queue ='io'
-roles = ['openvstorage']
-category = 'monitor.healthcheck'
+queue = 'io'
+interval = 30 * 600
+roles = ['storage']
+category = 'monitoring'
 
-class OpennStorage():
+class OpenvStorage():
     def __init__(self):
         self._localIp = None
         self._clusterIps = []
         self._internetSpeed = None
+        self._runingServer = None
         # appened opverstorage to python path
         sys.path.append('/opt/OpenvStorage')
         j.logger.log('Installing iperf', 1)
-        j.system.platform.ubuntu.install('iperf')
+        j.system.platform.ubuntu.checkInstall('iperf', 'iperf')
         
     
     @property
@@ -40,14 +42,18 @@ class OpennStorage():
     
     def runIperfServer(self):
         j.logger.log('Running iperf server', 1)
-        j.system.process.executeAsync('iperf -s -D')
+        self._runingServer = j.system.process.executeAsync('iperf', ['-s'])
+
+    def stopIperfServer(self):
+        if self._runingServer:
+            self._runingServer.kill()
     
     def getbandwidthState(self, bandwidth):
         """
         Assuming 10Gbits network
         """
-        bandwidth = float(bandwidth)
-        if bandwidth < 50:
+        bandwidth = bandwidth
+        if bandwidth < 5000:
             return 'ERROR'
         elif bandwidth < 9000:
             return 'WARNING'
@@ -56,26 +62,30 @@ class OpennStorage():
     def getClusterBandwidths(self):
         final = []
         for ip in self.clusterIps:
-            result = {'category':'Bandwidth Test'}
-            sshclient =  j.remote.cuisine.connect(ip, 22, 'rooter')
+            result = {'category': 'Bandwidth Test'}
+            sshclient = j.remote.cuisine.connect(ip, 22, 'rooter')
             j.logger.log('Installing iperf on %s' % ip, 1)
-            sshclient.run('apt-get install iperf')
-            tempfile = open('/tmp/tempfile.txt', 'w')
-            output = sshclient.subprocess.call(['iperf', '--format', 'm', '-c', self.localIp ,'-r'], stdout=tempfile)
-            tempfile.close()
-            output = open('/tmp/tempfile.txt', 'r').read()
-            tempfile.close()
+            if not sshclient.command_check('iperf'):
+                sshclient.run('apt-get install iperf')
+            output = sshclient.run('iperf -c %s --format m ' % self.localIp)
             output = output.split(' ')
-            bandwidth = output[-2]
-            result['message'] = json.dumps({'IP':ip, 'bandwidth': ' '.join([bandwidth, 'Mbits/s'])})
+            bandwidth = float(output[-2])
+            result['message'] = json.dumps({'IP': ip, 'bandwidth': '%s %s' % (bandwidth, 'Mbits/s')})
             result['state'] = self.getbandwidthState(bandwidth)
+            if result['state'] != 'OK':
+                msg = "Bandwith between %s and %s is not sufficient reached %s" % (self.localIp, ip)
+                print msg
+                eco = j.errorconditionhandler.getErrorConditionObject(msg=msg, category='monitoring', level=1, type='OPERATIONS')
+                eco.process()
             final.append(result)
         return final
 
-def action(data):
-    ovs = OpennStorage()
+def action():
+    ovs = OpenvStorage()
     ovs.runIperfServer()
-    return ovs.getClusterBandwidths()
+    results = ovs.getClusterBandwidths()
+    ovs.stopIperfServer()
+    return results
 
 if __name__ == '__main__':
-    action({})
+    action()
