f700bef94803f3b365f9614f51b2a411e7b6166d
Maxime Daniel <root@maxux.net>
2015-10-14 13:40:51 +0200

    add version tracking during update of nodes

--- scripts/updates/update-ays.py
+++ scripts/updates/update-ays.py
@@ -1,4 +1,5 @@
 from JumpScale import j
+import time
 
 def update(nodessh):
     print ''
@@ -12,8 +13,16 @@ def restart(nodessh):
     print ''
     nodessh.execute('ays restart')
 
+def header(hostname):
+    data  = "## Host: %s\n\n" % hostname
+    data += "| Account | Repository | Version (commit) |\n"
+    data += "|----|----|----|\n"
+    
+    return data
+
 # Local update
 sshservices = j.atyourservice.findServices(name='node.ssh')
+sshservices.sort(key = lambda x: x.instance)
 
 def findService(name):
     for ns in sshservices:
@@ -53,5 +62,45 @@ for ns in sshservices:
     if ns.instance not in hosts:
         restart(ns)
 
-
 print '[+] update completed'
+
+
+# Updating version file
+print '[+] grabbing version'
+
+# get our local repository path
+settings = j.application.getAppInstanceHRD(name='ovc_setup', instance='main', domain='openvcloud')
+repopath = settings.getStr('instance.ovc.path')
+
+versionfile = '%s/version.md' % repopath
+statuscmd = 'jscode status -n "*" | grep lrev: | sed s/lrev://g | awk \'{ printf "| %s | %s | %s |\\n", $1, $2, $9 }\''
+
+# header
+data  = "# Updated on %s\n\n" % time.strftime("%Y-%m-%d, %H:%M:%S")
+data += header("ovc_git")
+output = j.system.process.run(statuscmd, False, True)
+data += output[1]
+data += "\n"
+
+# grab version
+for host in hosts:
+    ns = findService(host)
+    if ns:
+        data += header(host)
+        data += ns.execute(statuscmd)
+        data += "\n\n"
+
+for ns in sshservices:
+    if ns.instance not in hosts:
+        data += header(ns.instance)
+        data += ns.execute(statuscmd)
+        data += "\n\n"
+
+with open(versionfile, 'w') as f:
+    f.write(data)
+
+output = j.system.process.run("cd %s; git add %s" % (repopath, versionfile), True, False)
+output = j.system.process.run("cd %s; git commit -m 'environement updated'" % repopath, True, False)
+output = j.system.process.run("cd %s; git push" % repopath, True, False)
+
+print '[+] update committed'
