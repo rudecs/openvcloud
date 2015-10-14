1efb697f6e265d298368d2063166d73529a6d37f
Maxime Daniel <root@maxux.net>
2015-10-14 16:52:53 +0200

    improved version logger for updater

--- scripts/updates/update-ays.py
+++ scripts/updates/update-ays.py
@@ -13,13 +13,88 @@ def restart(nodessh):
     print ''
     nodessh.execute('ays restart')
 
-def header(hostname):
-    data  = "## Host: %s\n\n" % hostname
-    data += "| Account | Repository | Version (commit) |\n"
-    data += "|----|----|----|\n"
+"""
+Markdown stuff
+"""
+def parse(content):
+    hosts = []
+    versions = {}
+    
+    # first: building hosts/repo list
+    for host in content:
+        hosts.append(host)
+        
+        for index, line in enumerate(content[host]):
+            if line == '':
+                del content[host][index]
+                continue
+            
+            item = line.split(',')
+            repo = '%s/%s' % (item[0], item[1])
+            
+            versions[repo] = {}
+    
+    # building version list
+    for host in content:
+        for line in content[host]:
+            item = line.split(',')
+            repo = '%s/%s' % (item[0], item[1])
+            commit = item[2][:8]
+            
+            versions[repo][host] = {'name': commit, 'valid': True}
+    
+    # checking if version are valid or not
+    for version in versions:        
+        for hostname in versions[version]:
+            for hostmatch in versions[version]:
+                # no version for this host
+                if hostmatch not in versions[version]:
+                    continue
+                
+                # version mismatch
+                if versions[version][hostmatch]['name'] != versions[version][hostname]['name']:
+                    versions[version][hostname]['valid'] = False
+            
+    
+    # sorting hosts alphabeticaly
+    hosts.sort()
+    
+    return hosts, versions
+
+def addCellString(value):
+    return value + ' | '
+    
+def addCell(item):
+    if not item['valid']:
+        return addCellString('**' + item['name'] + '**')
+        
+    return addCellString(item['name'])
+
+def build(content):
+    hosts, versions = parse(content)
+    
+    # building header
+    data  = "# Updated on %s\n\n" % time.strftime("%Y-%m-%d, %H:%M:%S")
+    
+    data += "| Repository | " + ' | '.join(hosts) + " |\n"
+    data += "|" + ("----|" * (len(hosts) + 1)) + "\n"
+    
+    for repo in versions:
+        data += '| ' + addCellString(repo)
+        
+        for host in hosts:
+            if host in versions[repo]:
+                data += addCell(versions[repo][host])
+            else:
+                data += addCellString('')
+        
+        data += "\n"
     
     return data
 
+"""
+Update stuff
+"""
 # Local update
 sshservices = j.atyourservice.findServices(name='node.ssh')
 sshservices.sort(key = lambda x: x.instance)
@@ -73,28 +148,43 @@ settings = j.application.getAppInstanceHRD(name='ovc_setup', instance='main', do
 repopath = settings.getStr('instance.ovc.path')
 
 versionfile = '%s/version.md' % repopath
-statuscmd = 'jscode status -n "*" | grep lrev: | sed s/lrev://g | awk \'{ printf "| %s | %s | %s |\\n", $1, $2, $9 }\''
+statuscmd = 'jscode status -n "*" | grep lrev: | sed s/lrev://g | awk \'{ printf "%s,%s,%s\\n", $1, $2, $9 }\''
 
-# header
-data  = "# Updated on %s\n\n" % time.strftime("%Y-%m-%d, %H:%M:%S")
-data += header("ovc_git")
+content = {}
+
 output = j.system.process.run(statuscmd, False, True)
-data += output[1]
-data += "\n"
+content['ovc_git'] = output[1].split("\n")
 
 # grab version
 for host in hosts:
     ns = findService(host)
     if ns:
-        data += header(host)
-        data += ns.execute(statuscmd)
-        data += "\n\n"
+        content[host] = ns.execute(statuscmd).split("\r\n")
 
 for ns in sshservices:
     if ns.instance not in hosts:
-        data += header(ns.instance)
-        data += ns.execute(statuscmd)
-        data += "\n\n"
+        content[ns.instance] = ns.execute(statuscmd).split("\r\n")
+
+data = build(content)
+
+"""
+Append old version
+"""
+
+data += "\n\n\n"
+
+for host in content:
+    data += "## %s\n\n" % host
+    data += "| Repository | Version (commit) |\n"
+    data += "|----|----|\n"
+    
+    
+    for line in content[host]:
+        items = line.split(',')
+        repo = '%s/%s' % (items[0], items[1])
+        data += "| %s | %s |\n" % (repo, items[2])
+    
+    data += "\n\n"
 
 with open(versionfile, 'w') as f:
     f.write(data)
