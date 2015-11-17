99b541b5b877406b857d4ef4a5719ae35f3a16be
Jo De Boeck <deboeck.jo@gmail.com>
2015-11-17 09:24:58 +0200

    Revert "Merge branch 'sco' into 'master'"
    This reverts commit 795d0dc48d69223e04a00540f7eebf240686ffc1, reversing
    changes made to d7e28bd595335f0a7739749fabff8d802faca9a8.

--- libs/agent-scripts/libvirt/createvolume.py
+++ libs/agent-scripts/libvirt/createvolume.py
@@ -17,37 +17,10 @@ async = True
 def action(name, size):
     import sys
     import os
-    sys.path.append('/opt/OpenvStorage')
-    from ovs.lib.vdisk import VDiskController
-    from ovs.dal.lists.vpoollist import VPoolList
-    from ovs.dal.hybrids.vdisk import VDisk
-    
     volumepath = '/mnt/vmstor/volumes'
-    
-    vdisk = VDisk()
-    filename = 'volume_%s.raw' % name
-    vdisk.name = filename
-    vdisk.devicename = j.system.fs.joinPaths(volumepath, filename)
-    vdisk.vpool = VPoolList.get_vpool_by_name('vmstor')
-    vdisk.size = size
-    vdisk.save()
-    params = {
-        'dtl_mode': 'async',
-        'sco_size': 64,
-        'dedupe_mode': 'non-dedupe',
-        'dtl_enabled': False,
-        'write_buffer': 1024,
-        'cache_strategy': 'on_read',
-        'readcache_limit': 10}
-    
-    VDiskController.set_config_params(vdisk.guid, params)
-    
     j.system.fs.createDir(volumepath)
     filepath = j.system.fs.joinPaths(volumepath, 'volume_%s.raw' % name)
     fd = os.open(filepath, os.O_RDWR | os.O_CREAT)
     os.ftruncate(fd, size)
     os.close(fd)
     return filepath
-
-if __name__ == '__main__':
-    action('hamdy_testdisk', 20)
