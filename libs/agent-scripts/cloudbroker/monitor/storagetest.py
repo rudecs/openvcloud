ada58c1739a0d1a3d07e24598a6fc04b0ffddb6d
Jo De Boeck <deboeck.jo@gmail.com>
2015-12-08 18:05:02 +0200

    Reuse storagetest vm for 24hours

--- libs/agent-scripts/cloudbroker/monitor/storagetest.py
+++ libs/agent-scripts/cloudbroker/monitor/storagetest.py
@@ -22,7 +22,7 @@ def action():
     ACCOUNTNAME = 'test_storage'
     pcl = j.clients.portal.getByInstance('main')
     ccl = j.clients.osis.getNamespace('cloudbroker')
-    accounts = ccl.account.search({'name': ACCOUNTNAME})[1:]
+    accounts = ccl.account.search({'name': ACCOUNTNAME, 'status': 'CONFIRMED'})[1:]
     loc = ccl.location.search({})[1]['locationCode']
     images = ccl.image.search({'name': 'Ubuntu 14.04 x64'})[1:]
     if not images:
@@ -39,7 +39,7 @@ def action():
                                         'status': {'$in': ['VIRTUAL', 'DEPLOYED']}
                                        })[1:]
     if not cloudspaces:
-        msg = "Not cloudspace available for account %s, disabling test" % ACCOUNTNAME
+        msg = "No cloudspace available for account %s, disabling test" % ACCOUNTNAME
         return [{'message': msg, 'category': 'Storage Test', 'state': 'OK'}]
     else:
         cloudspace = cloudspaces[0]
@@ -47,6 +47,7 @@ def action():
     if cloudspace['status'] == 'VIRTUAL':
         j.console.echo('Deploying CloudSpace', log=True)
         pcl.actors.cloudbroker.cloudspace.deployVFW(cloudspace['id'])
+        cloudspace = ccl.cloudspace.get(cloudspace['id']).dump()
 
     size = ccl.size.search({'memory': 512})[1]
     sizeId = size['id']
@@ -55,21 +56,33 @@ def action():
 
     stack = ccl.stack.search({'referenceId': str(j.application.whoAmI.nid), 'gid': j.application.whoAmI.gid})[1]
     name = '%s on %s' % (timestamp, stack['name'])
-    j.console.echo('Deleting existing vms', log=True)
-    vms = ccl.vmachine.search({'stackId': stack['id'], 'cloudspaceId': cloudspace['id'], 'status': {'$ne': 'DESTROYED'}})[1:]
+    j.console.echo('Deleting vms older then 24h', log=True)
+    vms = ccl.vmachine.search({'stackId': stack['id'],
+                               'cloudspaceId': cloudspace['id'],
+                               'status': {'$nin': ['ERROR', 'DESTROYED']}
+                               })[1:]
     for vm in vms:
         try:
-            pcl.actors.cloudapi.machines.delete(vm['id'])
+            if time.time() - vm['creationTime'] > 3600 * 24:
+                j.console.echo('Deleting %s' % vm['name'], log=True)
+                pcl.actors.cloudapi.machines.delete(vm['id'])
         except Exception, e:
             j.console.echo('Failed to delete vm %s' % e, log=True)
-    j.console.echo('Deploying VM', log=True)
-    vmachineId = pcl.actors.cloudbroker.machine.createOnStack(cloudspaceId=cloudspace['id'], name=name,
+    vms = ccl.vmachine.search({'stackId': stack['id'], 'cloudspaceId': cloudspace['id'], 'status': 'RUNNING'})[1:]
+    if vms:
+        vmachineId = vms[0]['id']
+        vmachine = pcl.actors.cloudapi.machines.get(vmachineId)
+        j.console.echo('Found VM %s' % vmachine['name'], log=True)
+    else:
+        j.console.echo('Deploying VM', log=True)
+        vmachineId = pcl.actors.cloudbroker.machine.createOnStack(cloudspaceId=cloudspace['id'], name=name,
                                                  imageId=imageId, sizeId=sizeId,
                                                  disksize=diskSize, stackid=stack['id'])
+        vmachine = pcl.actors.cloudapi.machines.get(vmachineId)
     now = time.time()
     status = 'OK'
     try:
-        ip = 'Undefined'
+        ip = vmachine['interfaces'][0]['ipAddress']
         while now + 60 > time.time() and ip == 'Undefined':
             j.console.echo('Waiting for IP', log=True)
             time.sleep(5)
@@ -78,12 +91,17 @@ def action():
 
         j.console.echo('Got IP %s' % ip, log=True)
         publicports = [1999 + j.application.whoAmI.nid * 100]
+        publicport = 0
         for forward in pcl.actors.cloudapi.portforwarding.list(cloudspace['id']):
+            if forward['localIp'] == ip and forward['localPort'] == '22':
+                publicport = forward['publicPort']
+                break
             publicports.append(int(forward['publicPort']))
-        publicport = max(publicports) + 1
+        if publicport == 0:
+            publicport = max(publicports) + 1
+            j.console.echo('Creating portforward', log=True)
+            pcl.actors.cloudapi.portforwarding.create(cloudspace['id'], cloudspace['publicipaddress'], publicport, vmachineId, 22, 'tcp')
 
-        j.console.echo('Creating portforward', log=True)
-        pcl.actors.cloudapi.portforwarding.create(cloudspace['id'], cloudspace['publicipaddress'], publicport, vmachineId, 22, 'tcp')
         publicip = str(netaddr.IPNetwork(cloudspace['publicipaddress']).ip)
         account = vmachine['accounts'][0]
         j.console.echo('Waiting for public connection', log=True)
@@ -96,7 +114,7 @@ def action():
             connection = j.remote.cuisine.connect(publicip, publicport, account['password'], account['login'])
             connection.user(account['login'])
             j.console.echo('Running dd', log=True)
-            output = connection.run("dd if=/dev/zero of=500mb.dd bs=4k count=128k")
+            output = connection.run("rm -f 500mb.dd; dd if=/dev/zero of=500mb.dd bs=4k count=128k")
             try:
                 j.console.echo('Perfoming internet test', log=True)
                 connection.run('ping -c 1 8.8.8.8')
@@ -114,10 +132,11 @@ def action():
         if status != 'OK':
             eco = j.errorconditionhandler.getErrorConditionObject(msg=msg, category='monitoring', level=1, type='OPERATIONS')
             eco.process()
-    finally:
+    except:
         j.console.echo('Deleting test vm', log=True)
         pcl.actors.cloudapi.machines.delete(vmachineId)
         j.console.echo('Finished deleting test vm', log=True)
+        raise
     return [{'message': msg, 'category': 'Storage Test', 'state': status}]
 
 if __name__ == '__main__':
