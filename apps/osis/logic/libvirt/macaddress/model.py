47eafa627d67fd9fe336fb183ec18318fba7a908
Jo De Boeck <deboeck.jo@gmail.com>
2015-12-14 20:55:26 +0200

    Fix macadress model

--- apps/osis/logic/libvirt/macaddress/model.py
+++ apps/osis/logic/libvirt/macaddress/model.py
@@ -1,13 +1,4 @@
 from JumpScale import j
-OsisBaseObject=j.core.osis.getOSISBaseObjectComplexType()
-from libvirt_node_osismodelbase import libvirt_node_osismodelbase
-class libvirt_node(OsisBaseObject,libvirt_node_osismodelbase):
-
+class libvirt_macaddress(j.code.classGetJSRootModelBase()):
     """
     """
-
-    def __init__(self, ddict={}):
-        # OsisBaseObject.__init__(self)
-        libvirt_node_osismodelbase.__init__(self)
-        if ddict <> {}:
-            self.load(ddict)
