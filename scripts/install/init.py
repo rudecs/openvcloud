01e6449e92df7936d8287f1ad6467fddc2506a7c
Maxime Daniel <root@maxux.net>
2015-12-20 05:06:33 +0100

    working dynamic ms1/docker deploy script

--- scripts/install/init.py
+++ scripts/install/init.py
@@ -1,44 +1,108 @@
 from JumpScale import j
+from optparse import OptionParser
+from optparse import OptionGroup
+import random
+import string
 
-# Docker settings
-usedocker  = False
-dockerhost = '172.17.0.1'
-dockerport = 2375
+parser = OptionParser()
+group = OptionGroup(parser, "global options")
+parser.add_option("-b", "--backend", dest="backend", help="backend: ms1, docker")
+parser.add_option("-G", "--git-user", dest="gituser", help="gitlab username")
+parser.add_option("-P", "--git-pass", dest="gitpass", help="gitlab password")
+parser.add_option("-e", "--environment", dest="environment", help="environment name (eg: be-test-1)")
 
-# Mothership1 credentials and data
-usems1      = True
-ms1username = ''
-ms1password = ''
-ms1location = 'eu1'
-cloudspace  = 'testing'
+group = OptionGroup(parser, "mothership1 backend")
+group.add_option("-u", "--user", dest="user", help="api username")
+group.add_option("-p", "--pass", dest="passwd", help="api password")
+group.add_option("-c", "--cloudspace", dest="cloudspace", help="cloudspace name")
+group.add_option("-l", "--location", dest="location", help="cloudspace location")
+parser.add_option_group(group)
 
-# GitLab credentials and data
-gitspace    = 'be-test-1'
-gitusername = ''
-gitpassword = ''
+group = OptionGroup(parser, "mothership1 backend")
+group.add_option("-r", "--remote", dest="remote", help="daemon remote ip")
+group.add_option("-o", "--port", dest="port", help="daemon remote port")
+group.add_option("-i", "--public", dest="public", help="public host ip address")
+parser.add_option_group(group)
+(options, args) = parser.parse_args()
 
+targets = ['ms1', 'docker']
+
+"""
+Stage 1: options parsing: global settings
+"""
+# global settings
+optlist = {
+	'git-user': options.gituser,
+	'git-pass': options.gitpass,
+	'environment': options.environment,
+}
+
+for item in optlist:
+	if optlist[item] == None:
+		print '[-] missing global argument: %s' % item
+		j.application.stop()
+
+if options.backend not in targets:
+	print '[-] unknown or missing backend'
+	j.application.stop()
+
+
+"""
+Stage 2: settings hardcoded values
+"""
 # Settings
-gitlink     = 'https://git.aydo.com/openvcloudEnvironments/%s' % gitspace
-vmpassword  = 'rooter'
+gitlink = 'https://git.aydo.com/openvcloudEnvironments/%s' % options.environment
 
-print '[+] installing environement: %s' % gitspace
-print '[+] mothership1 cloudspace : %s' % cloudspace
+choice     = string.ascii_letters + string.digits
+vmpassword = ''.join(random.choice(choice) for _ in range(12))
 
+print '[+] installing environement: %s' % options.environment
+print '[+] master password generated: %s' % vmpassword
+
+
+"""
+Stage 3: initializer
+"""
 ovc = j.clients.openvcloud.get()
 
-if usems1 and usedocker:
-	print '[-] cannot use docker and mothership1 in the same time'
-	j.application.stop()
 
-if not usems1 and not usedocker:
-	print '[-] no backend selected'
-	j.application.stop()
+"""
+Stage 4: options parsing: backend validator
+"""
+# mothership1
+if options.backend == 'ms1':
+	optlist = {
+		'user': options.user,
+		'pass': options.passwd,
+		'cloudspace': options.cloudspace,
+		'location': options.location
+	}
+	
+	for item in optlist:
+		if optlist[item] == None:
+			print '[-] missing mothiership1 argument: %s' % item
+			j.application.stop()
+	
+	machine = ovc.initMothership(options.user, options.passwd, options.location, options.cloudspace)
 
-if usems1:
-	machine = ovc.initMothership(ms1username, ms1password, ms1location, cloudspace)
+# docker
+if options.backend == 'docker':
+	optlist = {
+		'remote': options.remote,
+		'port': options.port,
+		'public': options.public,
+	}
+	
+	for item in optlist:
+		if optlist[item] == None:
+			print '[-] missing docker argument: %s' % item
+			j.application.stop()
+	
+	machine = ovc.initDocker(options.remote, options.port, options.public)
 
-if usedocker:
-	machine = ovc.initDocker(dockerhost, dockerport)
 
-ovc.initAYSGitVM(machine, gitlink, gitusername, gitpassword, vmpassword, delete=True)
+"""
+Stage 5: installing stuff
+"""
+ovc.initAYSGitVM(machine, gitlink, options.gituser, options.gitpass, vmpassword, delete=True)
 
