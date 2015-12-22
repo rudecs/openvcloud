c0589249320a7d304ac3971acdf1902071d1638f
Karim El-Sayed <elsayedk@codescalers.com>
2015-12-22 13:21:52 +0200

    Updating getMatchingUsernames to directly use  map

--- apps/cloudbroker/base/cloudapi__users/methodclass/cloudapi_users.py
+++ apps/cloudbroker/base/cloudapi__users/methodclass/cloudapi_users.py
@@ -263,18 +263,13 @@ class cloudapi_users(BaseActor):
         :param limit: the number of usernames to return
         :return: list of dicts with the username and url of the gravatar of the user
         """
-        #Should we filter out invalid username characters???
         matchingusers = self.systemodel.user.search({'id': {'$regex': usernameregex}},
                                                     size=limit)[1:]
 
         if matchingusers:
-            usernamegravatarlist= list()
-            for user in matchingusers:
-                primaryemail = user['emails'][0]
-                gravatarurl = 'http://www.gravatar.com/avatar/%s' % \
-                              md5.md5(primaryemail).hexdigest()
-                usernamegravatarlist.append({'username': user['id'],
-                                             'gravatarurl': gravatarurl})
-            return usernamegravatarlist
+            return map(lambda u: {'username': u['id'],
+                                  'gravatarurl': 'http://www.gravatar.com/avatar/%s' %
+                                                 md5.md5(u['emails'][0]).hexdigest()},
+                       matchingusers)
         else:
             return []
