b233548137bd92ddf016eab8c8fc221a5b011150
Karim Ali <karim.ali.dev@gmail.com>
2015-12-29 12:47:36 +0200

    Account ACLs WIP

--- apps/ms1_fe/base/wiki_gcb/.files/js/controllers/AccountSettingsController.js
+++ apps/ms1_fe/base/wiki_gcb/.files/js/controllers/AccountSettingsController.js
@@ -1,48 +1,130 @@
 angular.module('cloudscalers.controllers')
-    .controller('AccountSettingsController', ['$scope', 'Account', function($scope, Account) {
-        $scope.resetUser = function() {
-            // For now, we will grant the new user all permissions. See http://<server>/specifications/Security
-            $scope.newUser = {
-                nameOrEmail: '',
-                access: {
-                    R: true,
-                    X: true,
-                    C: true,
-                    D: true,
-                    U: true,
-                    A: true
-                }
-            };
-        };
+    .controller('AccountSettingsController', ['$scope', 'Account', '$ErrorResponseAlert', '$modal', '$timeout', function($scope, Account, $ErrorResponseAlert, $modal, $timeout) {
+      $scope.$watch("currentAccount", function(){
+        if($scope.currentAccount){
+          $scope.loadAccountAcl();
+        }
+      });
+      $scope.shareAccountMessage = false;
+      $scope.accessTypes = Account.accountAccessRights();
 
-        $scope.loadAcl = function() {
-            // When this controller is loaded, the currentAccount is not set yet, but currentSpace is.
-            return Account.get($scope.currentSpace.accountId).then(function(account) {
-                $scope.currentAccount.acl = account.acl;
-            });
-        };
+      function userMessage(message, style) {
+          $scope.shareAccountMessage = true;
+          $scope.shareAccountStyle = style;
+          $scope.shareAccountTxt = message;
+          $scope.resetUser();
+          $timeout(function () {
+              $scope.shareAccountMessage = false;
+          }, 3000);
+      }
 
-        $scope.resetUser();
-        $scope.loadAcl();
-        $scope.userError = false;
+      $scope.resetUser = function() {
+          $scope.newUser = {
+              nameOrEmail: '',
+              access: $scope.accessTypes[0].value
+          };
+      };
 
-        $scope.addUser = function() {
-            return Account.addUser($scope.currentAccount, $scope.newUser.nameOrEmail, $scope.newUser.access).then(function() {
-                $scope.loadAcl().then(function() {
-                    $scope.resetUser();
-                });
-                $scope.userError = false;
-            }, function(result) {
-                if (result.status == 404)
-                    $scope.userError = 'User not found';
-                else
-                    $scope.userError = 'An error has occurred';
-            });
-        };
+      $scope.loadAccountAcl = function() {
+          return Account.get($scope.currentAccount.id).then(function(account) {
+              $scope.currentAccount.userRightsOnAccount = account.acl;
+          }, function(reason){
+            $ErrorResponseAlert(reason);
+          });
+      };
 
-        $scope.deleteUser = function(space, user) {
-            Account.deleteUser($scope.currentAccount, user.userGroupId).then(function() {
-                $scope.loadAcl();
-            });
-        };
+      $scope.resetUser();
+      $scope.loadAccountAcl();
+      $scope.userError = false;
+
+      $scope.addUser = function() {
+          if($scope.currentAccount.userRightsOnAccount){
+              var userInAcl = _.find($scope.currentAccount.userRightsOnAccount, function(acl) { return acl.userGroupId == $scope.newUser.nameOrEmail; });
+              if( userInAcl ){
+                  userMessage($scope.newUser.nameOrEmail + " already have access rights.", 'danger');
+              }else{
+                  Account.addUser($scope.currentAccount.id, $scope.newUser.nameOrEmail, $scope.newUser.access).then(function() {
+                      $scope.currentAccount.userRightsOnAccount.push({userGroupId: $scope.newUser.nameOrEmail, right: $scope.newUser.access});
+                      userMessage("Assigned access rights successfully to " + $scope.newUser.nameOrEmail , 'success');
+                      $scope.resetUser();
+                  }, function(reason) {
+                      if (reason.status == 404)
+                          userMessage($scope.newUser.nameOrEmail + ' not found', 'danger');
+                      else
+                          $ErrorResponseAlert(reason);
+                  });
+              }
+          }
+      };
+
+      $scope.deleteUser = function(user) {
+          var modalInstance = $modal.open({
+              templateUrl: 'deleteUserDialog.html',
+              controller: function($scope, $modalInstance){
+                  $scope.ok = function () {
+                      $modalInstance.close('ok');
+                  };
+                  $scope.cancelRemoveUser = function () {
+                      $modalInstance.dismiss('cancel');
+                  };
+              },
+              resolve: {
+              }
+          });
+
+          modalInstance.result.then(function (result) {
+              Account.deleteUser($scope.currentAccount.id, user.userGroupId).
+                  then(function(data) {
+                      if(data === "true"){
+                        var userInACL = _.findWhere($scope.currentAccount.userRightsOnAccount, {userGroupId: user.userGroupId});
+                        $scope.currentAccount.userRightsOnAccount.splice([$scope.currentAccount.userRightsOnAccount.indexOf(userInACL)] ,1);
+                        $scope.resetUser();
+                        userMessage("Assigned access right removed successfully for " + user.userGroupId , 'success');
+                      }else if(data === "false"){
+                        userMessage("Last admin for account can not be deleted " , 'danger');
+                      }
+                  },
+                  function(reason){
+                      $ErrorResponseAlert(reason);
+                  });
+          });
+      };
+
+      $scope.loadEditUser = function(currentAccount, user, right) {
+          var modalInstance = $modal.open({
+              templateUrl: 'editUserDialog.html',
+              controller: function($scope, $modalInstance){
+                  $scope.accessTypes = Account.accountAccessRights();
+                  $scope.editUserAccess = right;
+                  $scope.userName = user;
+                  $scope.changeAccessRight = function(accessRight) {
+                      $scope.editUserAccess = accessRight.value;
+                  };
+                  $scope.ok = function (editUserAccess) {
+                      $modalInstance.close({
+                          currentAccountId: currentAccount.id,
+                          user: user,
+                          editUserAccess: editUserAccess
+                      });
+                  };
+                  $scope.cancelEditUser = function () {
+                      $modalInstance.dismiss('cancel');
+                  };
+              },
+              resolve: {
+              }
+          });
+          modalInstance.result.then(function (accessRight) {
+              Account.updateUser(currentAccount.accountId, accessRight.user, accessRight.editUserAccess).
+              then(function() {
+                  var userInACL = _.findWhere($scope.currentAccount.userRightsOnAccount, {userGroupId: accessRight.user});
+                  _.findWhere($scope.currentAccount.userRightsOnAccount, {userGroupId: accessRight.user}).right = accessRight.editUserAccess;
+                  userMessage("Access right updated successfully for " + user , 'success');
+                  $scope.resetUser();
+              },
+              function(reason){
+                  $ErrorResponseAlert(reason);
+              });
+          });
+      };
     }]);
