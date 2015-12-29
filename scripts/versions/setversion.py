85cf0ec595b600cb5b7b62cf8a0400ac0111b010
Jo De Boeck <deboeck.jo@gmail.com>
2015-12-29 11:22:33 +0200

    Fix identation

--- scripts/versions/setversion.py
+++ scripts/versions/setversion.py
@@ -13,63 +13,59 @@ parser.add_option("-t", "--tag", dest="tag", help="freeze to that tag name")
 (options, args) = parser.parse_args()
 
 def getServicesFiles(path):
-	matches = []
+    matches = []
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, 'service.hrd'):
+            matches.append(os.path.join(root, filename))
 
-	for root, dirnames, filenames in os.walk(path):
-		for filename in fnmatch.filter(filenames, 'service.hrd'):
-			matches.append(os.path.join(root, filename))
-
-	return matches
+    return matches
 
 def freeze(export, target):
-	keys = ['revision', 'branch', 'tag']
-	
-	for key in keys:
-		if export.get(key):
-			print '[+]   discarding %s: %s' % (key, export[key])
-			export.pop(key)
-		
-		if target[key]:
-			print '[+]   setting %s: %s' % (key, target[key])
-			export[key] = target[key]
-	
-	return export
+    keys = ['revision', 'branch', 'tag']
+    for key in keys:
+        if export.get(key):
+            print '[+]   discarding %s: %s' % (key, export[key])
+            export.pop(key)
+
+        if target[key]:
+            print '[+]   setting %s: %s' % (key, target[key])
+            export[key] = target[key]
+
+    return export
 
 # options check
 if not options.directory:
-	print '[-] missing directory'
-	j.application.stop()
+    print '[-] missing directory'
+        j.application.stop()
 
 if not options.repository:
-	print '[-] missing repository'
-	j.application.stop()
+    print '[-] missing repository'
+        j.application.stop()
 
 if not options.branch and not options.revision and not options.tag:
-	print '[-] missing target (branch, revision or tag)'
-	j.application.stop()
+    print '[-] missing target (branch, revision or tag)'
+        j.application.stop()
 
 directory = options.directory
 services = getServicesFiles(directory)
 gitrepo = options.repository
 
 target = {
-	'branch': options.branch,
-	'revision': options.revision,
-	'tag': options.tag
-}
+        'branch': options.branch,
+        'revision': options.revision,
+        'tag': options.tag
+        }
 
 for service in services:
-	print '[+] updating: %s' % service
-	
-	hrd = j.core.hrd.get(service)
-	exports = hrd.getDictFromPrefix('git.export')
-	
-	for index in exports:
-		export = exports[index]
-		
-		if export['url'].endswith(gitrepo) or export['url'].endswith('%s.git' % gitrepo):
-			export = freeze(export, target)
-			hrd.set('git.export.%s' % index, export)
-			hrd.save()
+    print '[+] updating: %s' % service
+    hrd = j.core.hrd.get(service)
+    exports = hrd.getDictFromPrefix('git.export')
+
+    for index in exports:
+        export = exports[index]
+        if export['url'].endswith(gitrepo) or export['url'].endswith('%s.git' % gitrepo):
+            export = freeze(export, target)
+            hrd.set('git.export.%s' % index, export)
+            hrd.save()
 
 print '[+] repository updated: %s' % directory
