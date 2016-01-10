d55152f868e1002393dedf1fa0e5ff45f0b90d91
Jo De Boeck <deboeck.jo@gmail.com>
2016-01-10 12:55:05 +0200

    Dont show error when network is not available anymore

--- apps/cbportal/base/CBGrid/.macros/wiki/network/3_network.py
+++ apps/cbportal/base/CBGrid/.macros/wiki/network/3_network.py
@@ -15,10 +15,19 @@ def main(j, args, params, tags, tasklet):
     id = int(id)
     vcl = j.clients.osis.getNamespace('vfw')
     scl = j.clients.osis.getNamespace('system')
+    ccl = j.clients.osis.getNamespace('cloudbroker')
     key = "%s_%s" % (gid, id)
 
     if not vcl.virtualfirewall.exists(key):
-        params.result = ('Network with id %s not found' % key, args.doc)
+        # check if cloudspace with id exists
+        cloudspaces = ccl.cloudspace.search({'gid': int(gid), 'networkId': int(id), 'status': 'VIRTUAL'})[1:]
+        data = {}
+        if cloudspaces:
+            data['cloudspaceId'] = cloudspaces[0]['id']
+            data['cloudspaceName'] = cloudspaces[0]['name']
+
+        args.doc.applyTemplate(data)
+        params.result = (args.doc, args.doc)
         return params
 
     network = vcl.virtualfirewall.get(key)
