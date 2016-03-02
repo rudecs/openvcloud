ce9eb51aa18707462bf006c88ce80e610b5032f7
Jo De Boeck <deboeck.jo@gmail.com>
2016-03-02 13:10:42 +0200

    Dont crash if not ovs yet

--- apps/cbportal/base/CBGrid/.macros/wiki/ovsversion/3_ovsversion.py
+++ apps/cbportal/base/CBGrid/.macros/wiki/ovsversion/3_ovsversion.py
@@ -2,8 +2,11 @@ def main(j, args, params, tags, tasklet):
     acl = j.clients.agentcontroller.get()
     jobinfo = acl.executeJumpscript('cloudscalers', 'ovspackages', role='storagenode', gid=j.application.whoAmI.gid)
     wiki = []
-    for name, version in jobinfo['result'].iteritems():
-        wiki.append("* %s: %s" % (name, version))
+    if jobinfo['state'] == 'NOWORK':
+        wiki.append("Not installed")
+    else:
+        for name, version in jobinfo['result'].iteritems():
+            wiki.append("* %s: %s" % (name, version))
 
     params.result = ('\n'.join(wiki), args.doc)
     return params
