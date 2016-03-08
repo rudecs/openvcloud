49630d2027d25d31ce011a1642a27020a0317a44
Jo De Boeck <deboeck.jo@gmail.com>
2016-03-08 20:17:30 +0200

    Make orphan script a healtscript

--- libs/agent-scripts/cloudbroker/monitor/vm_orphan.py
+++ libs/agent-scripts/cloudbroker/monitor/vm_orphan.py
@@ -5,20 +5,20 @@ Check for orphan vms
 """
 
 organization = 'jumpscale'
+category = "monitor.healthcheck"
 name = 'vm_orphan'
 author = "deboeckj@codescalers.com"
 version = "1.0"
-category = "monitor.vm"
 
 period = 3600 # 1 hrs 
 enable = True
 async = True
 roles = ['cpunode',]
 queue = 'process'
-log = False
 
 def action():
     import libvirt
+    result = []
     cbcl = j.clients.osis.getNamespace('cloudbroker', j.core.osis.client)
     vcl = j.clients.osis.getNamespace('vfw', j.core.osis.client)
     stacks = cbcl.stack.search({'gid': j.application.whoAmI.gid, 'referenceId': str(j.application.whoAmI.nid)})[1:]
@@ -37,18 +37,23 @@ def action():
             if name.startswith('routeros'):
                 networkid = int(name.split('_')[-1], 16)
                 if networkid not in networkids:
-                    orphans.append("- %s" % domain.name())
+                    orphans.append("%s" % domain.name())
             elif domain.UUIDString() not in vmsbyguid:
-                orphans.append("- %s" % domain.name())
+                orphans.append("%s" % domain.name())
     finally:
         con.close()
 
     if orphans:
         message = "Found following orphan machines\n" + "\n".join(orphans)
+        for orphan in orphans:
+            result.append({'state': 'ERROR', 'category': 'Orphanage', 'message': 'Found orphan machine %s' % orphan})
         print(message)
         j.errorconditionhandler.raiseOperationalWarning(message, 'monitoring')
+    else:
+        result.append({'state': 'OK', 'category': 'Orphanage', 'message': 'No orphan machines found'})
+
+    return result
 
 if __name__ == '__main__':
-    import JumpScale.grid.osis
     j.core.osis.client = j.clients.osis.getByInstance('main')
     action()
