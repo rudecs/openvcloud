a43e9300176ea38305e33efd61f1f32d8d4da0b4
Jo De Boeck <deboeck.jo@gmail.com>
2016-04-11 15:26:47 +0200

    Dont pass account name in actors
    Fixes #51

--- apps/cbportal/base/cloudbroker__finance/methodclass/cloudbroker_finance.py
+++ apps/cbportal/base/cloudbroker__finance/methodclass/cloudbroker_finance.py
@@ -1,6 +1,7 @@
 from JumpScale import j
 import time
 from JumpScale.portal.portal.auth import auth
+from JumpScale.portal.portal import exceptions
 from cloudbrokerlib.baseactor import BaseActor
 
 class cloudbroker_finance(BaseActor):
@@ -8,23 +9,14 @@ class cloudbroker_finance(BaseActor):
     gateway to grid
     """
     @auth(['finance',])
-    def changeCredit(self, accountname, amount, message, **kwargs):
-        accounts = self.models.account.search({'name': accountname})[1:]
-        if not accounts:
-            ctx = kwargs["ctx"]
-            headers = [('Content-Type', 'application/json'), ]
-            ctx.start_response("404", headers)
-            return 'Account name not found'
-        account_id = accounts[0]['id']
-
+    def changeCredit(self, accountId, amount, message, **kwargs):
+        if not self.models.account.exists(accountId):
+            raise exceptions.NotFound('Account not found')
         if len(message) > 30:
-            ctx = kwargs["ctx"]
-            headers = [('Content-Type', 'application/json'), ]
-            ctx.start_response("400", headers)
-            return 'Message is too long'
+            raise exceptions.BadRequest('Message is too long')
 
         credittransaction = self.models.credittransaction.new()
-        credittransaction.accountId = account_id
+        credittransaction.accountId = accountId
         credittransaction.amount = float(amount)
         credittransaction.credit = float(amount)
         credittransaction.currency = 'USD'
