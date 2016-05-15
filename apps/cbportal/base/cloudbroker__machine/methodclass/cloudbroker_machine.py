rudecs/openvcloud

commit 1a08ea24cc2749042a7854aa1456635dcfbc031a

clonename cannot be empty (#133)
* clonename cannot be empty

* fix possible NPE in machine name assertion

apps/cloudbroker/base/cloudapi__machines/methodclass/cloudapi_machines.py
@@ -540,8 +540,7 @@ class cloudapi_machines(BaseActor):
         """
         machine = self._getMachine(machineId)
         if name:
-            if not self.cb.machine._assertName(machine.cloudspaceId, name, **kwargs):
-                raise exceptions.Conflict('Selected name already exists')
+            self.cb.machine._assertName(machine.cloudspaceId, name, **kwargs):
             machine.name = name
         if description:
             machine.descr = description
@@ -580,8 +579,7 @@ class cloudapi_machines(BaseActor):
         if machine.cloneReference:
             raise exceptions.MethodNotAllowed('Cannot clone a cloned machine.')
 
-        if not self.cb.machine._assertName(machine.cloudspaceId, name, **kwargs):
-            raise exceptions.Conflict('Selected name already exists')
+        self.cb.machine._assertName(machine.cloudspaceId, name, **kwargs):
         clone = self.models.vmachine.new()
         clone.cloudspaceId = machine.cloudspaceId
         clone.name = name

apps/cloudbroker/cloudbrokerlib/cloudbroker.py
@@ -399,8 +399,7 @@ class Machine(object):
         models.vmachine.delete(machine.id)
 
     def validateCreate(self, cloudspace, name, sizeId, imageId, disksize, minimum_days_of_credit_required):
-        if not self._assertName(cloudspace.id, name):
-            raise exceptions.Conflict('Selected name already exists')
+        self._assertName(cloudspace.id, name):
         if not disksize:
             raise exceptions.BadRequest("Invalid disksize %s" % disksize)
 
@@ -417,8 +416,11 @@ class Machine(object):
 
 
     def _assertName(self, cloudspaceId, name, **kwargs):
+        if not name or not name.strip():
+            raise ValueError("Machine name can not be empty")
         results = models.vmachine.search({'cloudspaceId': cloudspaceId, 'name': name, 'status': {'$nin': ['DESTROYED', 'ERROR']}})[1:]
-        return False if results else True
+        if results:
+            raise exceptions.Conflict('Selected name already exists')
 
     def createModel(self, name, description, cloudspace, imageId, sizeId, disksize, datadisks):
         datadisks = datadisks or []

commit c4c9a1886adc54a0afef5907e6f833af990f6163

Fix issues caused by pr #133

apps/cbportal/base/cloudbroker__machine/methodclass/cloudbroker_machine.py
@@ -44,7 +44,7 @@ class cloudbroker_machine(BaseActor):
         result bool
         """
         cloudspace = self.models.cloudspace.get(cloudspaceId)
-        self.cb.machine.validateCreate(cloudspace, name, sizeId, imageId, disksize, 0)
+        self.cb.machine.validateCreate(cloudspace, name, sizeId, imageId, disksize)
         size = self.models.size.get(sizeId)
         j.apps.cloudapi.cloudspaces.checkAvailableMachineResources(cloudspace.id, size.vcpus,
                                                                    size.memory/1024.0, disksize)

apps/cloudbroker/base/cloudapi__machines/methodclass/cloudapi_machines.py
@@ -540,7 +540,7 @@ class cloudapi_machines(BaseActor):
         """
         machine = self._getMachine(machineId)
         if name:
-            self.cb.machine._assertName(machine.cloudspaceId, name, **kwargs):
+            self.cb.machine.assertName(machine.cloudspaceId, name, **kwargs)
             machine.name = name
         if description:
             machine.descr = description
@@ -579,7 +579,7 @@ class cloudapi_machines(BaseActor):
         if machine.cloneReference:
             raise exceptions.MethodNotAllowed('Cannot clone a cloned machine.')
 
-        self.cb.machine._assertName(machine.cloudspaceId, name, **kwargs):
+        self.cb.machine.assertName(machine.cloudspaceId, name)
         clone = self.models.vmachine.new()
         clone.cloudspaceId = machine.cloudspaceId
         clone.name = name

apps/cloudbroker/cloudbrokerlib/cloudbroker.py
@@ -399,8 +399,8 @@ class Machine(object):
             models.disk.delete(diskid)
         models.vmachine.delete(machine.id)
 
-    def validateCreate(self, cloudspace, name, sizeId, imageId, disksize, minimum_days_of_credit_required):
-        self._assertName(cloudspace.id, name)
+    def validateCreate(self, cloudspace, name, sizeId, imageId, disksize):
+        self.assertName(cloudspace.id, name)
         if not disksize:
             raise exceptions.BadRequest("Invalid disksize %s" % disksize)
 
@@ -416,7 +416,7 @@ class Machine(object):
             raise exceptions.BadRequest("Disk size of {}GB is invalid for sizeId {}.".format(disksize, sizeId))
 
 
-    def _assertName(self, cloudspaceId, name, **kwargs):
+    def assertName(self, cloudspaceId, name):
         if not name or not name.strip():
             raise ValueError("Machine name can not be empty")
         results = models.vmachine.search({'cloudspaceId': cloudspaceId, 'name': name, 'status': {'$nin': ['DESTROYED', 'ERROR']}})[1:]