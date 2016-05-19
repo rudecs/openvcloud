5b9b54c0aa013d6bd5657e7422babe88684b3ecf
Jo De Boeck <deboeck.jo@gmail.com>
2016-05-19 18:14:27 +0200

    Add disk orphanage

---
+++ libs/agent-scripts/cloudbroker/monitor/disk_orphan_schedule.py
@@ -0,0 +1,35 @@
+from JumpScale import j
+
+descr = """
+Check for orphan disk
+"""
+
+organization = 'cloudscalers'
+category = "monitor.healthcheck"
+author = "deboeckj@codescalers.com"
+version = "1.0"
+
+enable = True
+async = True
+period = 3600 # 1 hrs
+roles = ['master',]
+queue = 'process'
+
+
+def action():
+    acl = j.clients.agentcontroller.get()
+    ccl = j.clients.osis.getNamespace('cloudbroker')
+
+    results = []
+    for location in ccl.location.search({})[1:]:
+        job = acl.executeJumpscript('cloudscalers', 'disk_orphan', role='cpunode', gid=location['gid'])
+        if job['state'] == 'OK':
+            results.extend(job['result'])
+
+    if not results:
+        results.append({'state': 'OK', 'category': 'Orphanage', 'message': 'No orphan disks found.'})
+    return results
+
+if __name__ == '__main__':
+    j.core.osis.client = j.clients.osis.getByInstance('main')
+    print action()
