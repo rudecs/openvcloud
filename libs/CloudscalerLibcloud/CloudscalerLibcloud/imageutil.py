2f6a2376240c94f1c973097e90bb0dc3e7a0d95b
Jo De Boeck <deboeck.jo@gmail.com>
2016-07-05 13:59:17 +0200

    Alter imageutil to only create the image
    Dont register the image to a stack or cpunode
    This is to allow installing images on storage nodes instead of cpunodes

--- libs/CloudscalerLibcloud/CloudscalerLibcloud/imageutil.py
+++ libs/CloudscalerLibcloud/CloudscalerLibcloud/imageutil.py
@@ -11,8 +11,6 @@ def registerImage(service, name, type, disksize, username=None):
     templateguid, imagepath = openvstorage.copyImage(imagepath)
     templateguid = templateguid.replace('-', '') # osis strips dashes
     #register image on cloudbroker
-    node_id = "%s_%s" % (j.application.whoAmI.gid, j.application.whoAmI.nid)
-    osiscl = j.clients.osis.getByInstance('main')
     lcl = j.clients.osis.getNamespace('libvirt')
     ccl = j.clients.osis.getNamespace('cloudbroker')
 
@@ -27,18 +25,6 @@ def registerImage(service, name, type, disksize, username=None):
         image['username'] = username
         lcl.image.set(image)
 
-    if not node_id in lcl.resourceprovider.list():
-        rp = dict()
-        rp['cloudUnitType'] = 'CU'
-        rp['id'] = str(j.application.whoAmI.nid)
-        rp['gid'] = j.application.whoAmI.gid
-        rp['images'] = [templateguid]
-    else:
-        rp = lcl.resourceprovider.get(node_id)
-        if not templateguid in rp.images:
-            rp.images.append(templateguid)
-    lcl.resourceprovider.set(rp)
-
     images = ccl.image.search({'referenceId': templateguid})[1:]
     if not images:
         image = ccl.image.new()
@@ -49,14 +35,6 @@ def registerImage(service, name, type, disksize, username=None):
         image.username = username
         image.provider_name = 'libvirt'
         image.status = 'CREATED'
-        imageId = ccl.image.set(image)[0]
-    else:
-        imageId = images[0]['id']
-    stacks = ccl.stack.search({'referenceId': str(j.application.whoAmI.nid)})[1:]
-    if stacks:
-        stack = stacks[0]
-        if imageId not in stack.get('images', []):
-            stack.setdefault('images', []).append(imageId)
-            ccl.stack.set(stack)
+        ccl.image.set(image)
 
     return True
