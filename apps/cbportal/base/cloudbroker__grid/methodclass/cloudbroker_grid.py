fca2e288919cdfc1ca909998f65dee23edfb7e34
Jo De Boeck <deboeck.jo@gmail.com>
2016-08-02 09:11:21 +0200

    Add possibility to add/edit loations

--- apps/cbportal/base/cloudbroker__grid/methodclass/cloudbroker_grid.py
+++ apps/cbportal/base/cloudbroker__grid/methodclass/cloudbroker_grid.py
@@ -1,10 +1,11 @@
 from JumpScale import j
 from JumpScale.portal.portal.auth import auth
-import JumpScale.grid.agentcontroller
+from JumpScale.portal.portal import exceptions
 
 class cloudbroker_grid(object):
 
     def __init__(self):
+        self.models = j.clients.osis.getNamespace('cloudbroker')
         self.acl = j.clients.agentcontroller.get()
 
     @auth(['level1', 'level2', 'level3'])
@@ -19,3 +20,26 @@ class cloudbroker_grid(object):
                 gid = int(nodeid.split('_')[0])
                 self.acl.executeJumpscript('jumpscale', 'vms_check', gid=gid, role='master', wait=False)
         return 'Scheduled check on VMS'
+
+
+    @auth(['level1', 'level2', 'level3'])
+    def rename(self, name, gid, **kwargs):
+        location = next(iter(self.models.location.search({'gid': gid})[1:]), None)
+        if not location:
+            raise exceptions.NotFound('Could not find location with gid %s' % gid)
+        location['name'] = name
+        self.models.location.set(location)
+        return True
+
+    @auth(['level1', 'level2', 'level3'])
+    def add(self, name, gid, locationcode, **kwargs):
+        location = next(iter(self.models.location.search({'gid': gid})[1:]), None)
+        if location:
+            raise exceptions.Conflict("Location with gid %s already exists" % gid)
+        location = self.models.location.new()
+        location.gid = gid
+        location.flag = 'black'
+        location.locationCode = locationcode
+        location.name = name
+        self.models.location.set(location)
+        return 'Location has been added successfully, do not forget to add networkids and public IPs'
