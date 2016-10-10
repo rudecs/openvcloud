0ce5a9c6ab9b77933d1b058ed2496191cae19aa5
Jo De Boeck <deboeck.jo@gmail.com>
2016-10-10 23:05:30 +0200

    Import JumpScale

--- scripts/ops/change_vdisk_settings.py
+++ scripts/ops/change_vdisk_settings.py
@@ -1,3 +1,5 @@
+from JumpScale import j
+
 scl = j.clients.osis.getNamespace('system')
 grid = scl.grid.get(j.application.whoAmI.gid)
 ovscred = grid.settings['ovs_credentials']
@@ -5,7 +7,6 @@ ovscred = grid.settings['ovs_credentials']
 ovscl = j.clients.openvstorage.get(ovscred['ips'], (ovscred['client_id'], ovscred['client_secret']))
 
 pools = {pool['guid']: pool['name'] for pool in ovscl.get('/vpools', params={'contents': 'true'})['data']}
-
 
 vmstorset = '''{"new_config_params":{"sco_size":4,"dtl_mode":"no_sync","dedupe_mode":"non_dedupe","write_buffer":128,"dtl_target":[],"cache_strategy":"none","readcache_limit":null}}'''
 dataset = '''{"new_config_params":{"sco_size":32,"dtl_mode":"no_sync","dedupe_mode":"non_dedupe","write_buffer":256,"dtl_target":[],"cache_strategy":"none","readcache_limit":null}}'''
