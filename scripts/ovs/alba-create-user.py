9e756e1a111f41b57e2b51aabdd4a1b4d3b10d5a
Jo De Boeck <deboeck.jo@gmail.com>
2016-10-10 11:25:25 +0200

    Make sure we always get the latest ovs credentials

--- scripts/ovs/alba-create-user.py
+++ scripts/ovs/alba-create-user.py
@@ -69,9 +69,11 @@ if __name__ == '__main__':
     grid = scl.grid.get(j.application.whoAmI.gid)
     credentials = grid.settings.get('ovs_credentials')
     ips = set()
+    ovscreds = user()
     if credentials is None:
-        credentials = user()
+        credentials = ovscreds
     else:
+        credentials.update(ovscreds)
         ips = set(credentials.get('ips', []))
 
     ips.add(get_management_ip())
