4f2359a29a0fbc5ddc8b09d5d3ca761f266fcf45
Geert Audenaert <geert@mobicage.com>
2016-10-13 14:30:16 +0200

    Update of vdisk-distribution script including totals

--- scripts/ovs/vdisk-distribution.py
+++ scripts/ovs/vdisk-distribution.py
@@ -9,8 +9,19 @@ def update():
     result = list()
     for sd in storage_drivers:
         result.append(dict(vpool=sd['mountpoint'].split('/')[-1], vpoolguid=sd['vpool_guid'], storagerouterguid=sd['storagerouter_guid'], diskcount=len(sd['vdisks_guids']), storageip=sd['storage_ip']))
-	result.sort(key=lambda r: r['vpool'])
+    storage_routers = set()
+    vpools = set()
     for r in result:
-        print r['vpool'], r['storageip'], r['diskcount']
-
+        storage_routers.add(r['storageip'])
+        vpools.add(r['vpool'])
+    max_vpool_name_length = max((len(vp) for vp in vpools))
+    storage_routers = sorted(list(storage_routers))
+    vpools = sorted(list(vpools))
+    header = "{} | {} | totals".format("".ljust(max_vpool_name_length), " | ".join(storage_routers))
+    print header
+    for vpool in vpools:
+        print "".ljust(len(header), '-')
+        print "{} | {} | {}".format(vpool.ljust(max_vpool_name_length), " | ".join((str(next((r['diskcount'] for r in result if r['vpool'] == vpool and r['storageip'] == srip), '')).ljust(len(srip)) for srip in storage_routers)), sum((next((r['diskcount'] for r in result if r['vpool'] == vpool and r['storageip'] == srip), 0)) for srip in storage_routers))
+    print "".ljust(len(header), '-')
+    print "{} | {}".format('totals'.ljust(max_vpool_name_length), " | ".join((str(sum((next((r['diskcount'] for r in result if r['vpool'] == vp and r['storageip'] == srip), 0)) for vp in vpools)).ljust(len(srip)) for srip in storage_routers)))
 update()
