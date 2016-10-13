e00ab03bd94a2ea32b22319fe7b8792c7573a8d3
Geert Audenaert <geert@mobicage.com>
2016-10-13 11:14:13 +0200

    Added script to monitor vdisk distribution on ovs

---
+++ scripts/ovs/vdisk-distribution.py
@@ -0,0 +1,15 @@
+from JumpScale import j
+
+scl = j.clients.osis.getNamespace('system')
+ovs = scl.grid.get(j.application.whoAmI.gid).settings['ovs_credentials']
+ovc = j.clients.openvstorage.get(ovs['ips'], (ovs['client_id'], ovs['client_secret']))
+
+def update():
+    storage_drivers = ovc.get('/storagedrivers', params={'contents':'vdisks_guids,vpool,storagerouter'})['data']
+    result = list()
+    for sd in storage_drivers:
+        result.append(dict(vpool=sd['mountpoint'].split('/')[-1], vpoolguid=sd['vpool_guid'], storagerouterguid=sd['storagerouter_guid'], diskcount=len(sd['vdisks_guids']), storageip=sd['storage_ip']))
+    for r in result:
+        print r['vpool'], r['storageip'], r['diskcount']
+
+update()
