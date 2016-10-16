3912e4747b214107b92f11df4977373d5001ed39
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2016-10-16 21:53:48 +0200

    bug fixes in aggregate script

--- libs/agent-scripts/resmonitoring/aggregate_account_data.py
+++ libs/agent-scripts/resmonitoring/aggregate_account_data.py
@@ -73,32 +73,31 @@ def action(gid=None):
 
 
     for account_id, dates in accounts.iteritems():
-        account = resources_capnp.Account.new_message()
 
-        for i, (date, members) in enumerate(dates.iteritems()):
-            for member in members:
-                if member.name.endswith("bin"):
-                    year, month, day, hour = date
-                    account.accountId = int(account_id)
-                    cloudspaces = account.init("cloudspaces", len(members))
+        for date, members in dates.iteritems():
+            account = resources_capnp.Account.new_message()
+            year, month, day, hour = date
+            account.accountId = int(account_id)
+            cloudspaces = account.init("cloudspaces", len(members))
 
-                    # read the capnp file obj.
-                    binary_content = tar.extractfile(member).read()
-                    cloudspace_obj = resources_capnp.CloudSpace.from_bytes(binary_content)
-                    cloudspaces[i] = cloudspace_obj
-                    filepath = '/opt/jumpscale7/var/resourcetracking/%s/' % os.path.join(account_id,
-                                                                                         year,
-                                                                                         month,
-                                                                                         day,
-                                                                                         hour)
-                    try:
-                        os.makedirs(filepath)
-                    except OSError as err:
-                        if err.errno != 17:
-                            raise err
+            for i, member in enumerate(members):
+                # read the capnp file obj.
+                binary_content = tar.extractfile(member).read()
+                cloudspace_obj = resources_capnp.CloudSpace.from_bytes(binary_content)
+                cloudspaces[i] = cloudspace_obj
+                filepath = '/opt/jumpscale7/var/resourcetracking/%s/' % os.path.join(account_id,
+                                                                                     year,
+                                                                                     month,
+                                                                                     day,
+                                                                                     hour)
+                try:
+                    os.makedirs(filepath)
+                except OSError as err:
+                    if err.errno != 17:
+                        raise
 
-                    with open(os.path.join(filepath, "account_capnp.bin"), 'w+b') as f:
-                        account.write(f)
+            with open(os.path.join(filepath, "account_capnp.bin"), 'w+b') as f:
+                account.write(f)
 
 if __name__ == '__main__':
     print(action())
