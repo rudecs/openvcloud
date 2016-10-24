419218fbc0b4adfdbf06d0c47d5f04c78f738eb7
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2016-10-24 13:02:21 +0200

    fixed bug in addsize api to append to size if alreeady exists (#520)
    relates to #519

--- apps/cbportal/base/cloudbroker__iaas/methodclass/cloudbroker_iaas.py
+++ apps/cbportal/base/cloudbroker__iaas/methodclass/cloudbroker_iaas.py
@@ -147,13 +147,23 @@ class cloudbroker_iaas(BaseActor):
         size['disks'] = disks
         size['memory'] = memory
         size['vcpus'] = vcpus
-        if self.lcl.size.count(size) >= 1:
-            raise exceptions.BadRequest('Size with disk memory vcpu combination already exists.')
-        size['name'] = name
-        self.models.size.set(size)
+        locations = self.models.location.search({"$query": {}, "$fields": ['gid']})
+        size['gids'] = [location['gid'] for location in locations[1:]]
         for disk in disks:
-            name = '%s-%s-%s' % (vcpus, memory, disk)
-            self.lcl.size.set({"disk": disk, "memory": memory, "vcpus": vcpus, 'name': name})
+            if self.lcl.size.count({"vcpus": vcpus, "memory": memory, "disk": disk}) >= 1:
+                raise exceptions.BadRequest('Size with disk memory vcpu combination already exists.')
+            for gid in size['gids']:
+                name = '%s-%s-%s-%s' % (vcpus, memory, disk, gid)
+                self.lcl.size.set({"disk": disk, "memory": memory, "vcpus": vcpus, 'name': name, "gid": gid})
+        size['name'] = name
+        cloudbroker_size = next(iter(self.models.size.search({"vcpus": vcpus, "memory": memory})[1:]), None)
+
+        if not cloudbroker_size:
+            self.models.size.set(size)
+        else:
+            cloudbroker_size['disks'].extend(disks)
+            self.models.size.set(cloudbroker_size)
+
         return True
 
     @auth(['level1', 'level2', 'level3'])
