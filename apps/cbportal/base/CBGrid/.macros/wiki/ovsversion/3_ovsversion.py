4de0f1d712adac11b7c66cb1ebca688b843084e8
Jo De Boeck <deboeck.jo@gmail.com>
2016-11-27 09:08:03 +0200

    Handle job failure when retreiving ovs version

--- apps/cbportal/base/CBGrid/.macros/wiki/ovsversion/3_ovsversion.py
+++ apps/cbportal/base/CBGrid/.macros/wiki/ovsversion/3_ovsversion.py
@@ -4,12 +4,15 @@ def main(j, args, params, tags, tasklet):
     wiki = []
     if jobinfo['state'] == 'NOWORK':
         wiki.append("Not installed")
+    elif jobinfo['state'] != 'OK':
+        wiki.append("Failed to retreive OVS version.")
     else:
         for name, version in jobinfo['result'].iteritems():
             wiki.append("* %s: %s" % (name, version))
 
     params.result = ('\n'.join(wiki), args.doc)
     return params
+
 
 def match(j, args, params, tags, tasklet):
     return True
