144521303d2dfe1cd3ae6d55ed09b1b04cada023
Jo De Boeck <deboeck.jo@gmail.com>
2016-12-13 13:51:12 +0200

    Change storage link

--- scripts/proxy/update-config.py
+++ scripts/proxy/update-config.py
@@ -5,6 +5,7 @@ locations = set()
 
 refsrv = j.atyourservice.findServices(name='node.ssh', instance='ovc_reflector')
 if len(refsrv) == 0:
+    j.console.notice('Searching for nginx services')
     for service in j.atyourservice.findServices(name='node.ssh'):
         if service.parent is None:
             # skip nodes that dont have parents
@@ -19,13 +20,14 @@ if len(refsrv) == 0:
             port = "2001"  # assume there is no reflector
             locations.add(envname)
             if ovsservice:
-                print '[+] Adding ovs {} {}'.format(service.instance, envname)
+                j.console.info('Adding ovs {} {}'.format(service.instance, envname))
                 upstreams.setdefault('ovs-' + envname, []).append('%s:%s' % (host, port))
             if cpuservice:
-                print '[+] Adding cpu {} {}'.format(service.instance, envname)
+                j.console.info('Adding cpu {} {}'.format(service.instance, envname))
                 upstreams.setdefault(envname, []).append('%s:%s' % (host, port))
 
 else:
+    j.console.notice('Searching for http proxies')
     for service in j.atyourservice.findServices(name='autossh', instance='http_proxy'):
         location = service.parent.parent.instance
         host = service.hrd.getStr('instance.remote.bind')
@@ -48,7 +50,7 @@ for location, hosts in upstreams.iteritems():
     offloader.hrd.set('instance.generated.%s' % location, hosts)
 offloader.hrd.save()
 
-print '[+] patching nginx configuration'
+j.console.notice('Patching nginx configuration')
 
 # FIXME: better way to reinstall it
 proxy.execute('rm -f /opt/jumpscale7/hrd/apps/openvcloud__ssloffloader__main/installed.version')
@@ -56,18 +58,25 @@ offloader.consume('node', proxy.instance)
 offloader.install(reinstall=True)
 
 # update portal entries
+j.console.notice('Updating portal config')
+masteraioconfig = j.application.getAppInstanceHRD(name='cb_master_aio', instance='main', domain='openvcloud')
+baseurl = masteraioconfig.get('instance.param.ovs.url')
 master = j.atyourservice.get(name='node.ssh', instance='ovc_master')
 portal = j.atyourservice.get(name='portal', parent=master)
 ovs = portal.hrd.getDict('instance.navigationlinks.ovs')
-ovs['children'] = 'instance.ovslinks'
-ovs['baseurl'] = ovs.get('url') or ovs.get('baseurl')
-ovs['url'] = ''
-ovslinks = {}
-for location in locations:
-    ovslinks[location] = ovs['baseurl'] + '/ovcinit/%s' % location
+ovs['external'] = 'blank'
+if len(locations) == 1:
+    ovs.pop('children', None)
+    ovs['url'] = baseurl + '/ovcinit/%s' % list(locations)[0]
+else:
+    ovs['children'] = 'instance.ovslinks'
+    ovs['url'] = ''
+    ovslinks = {}
+    for location in locations:
+        ovslinks[location] = baseurl + '/ovcinit/%s' % location
+    portal.hrd.set('instance.ovslinks', ovslinks)
 
 portal.hrd.set('instance.navigationlinks.ovs', ovs)
-portal.hrd.set('instance.ovslinks', ovslinks)
 
 master.execute('rm -f /opt/jumpscale7/hrd/apps/jumpscale__portal__main/installed.version')
 portal.consume('node', master.instance)
