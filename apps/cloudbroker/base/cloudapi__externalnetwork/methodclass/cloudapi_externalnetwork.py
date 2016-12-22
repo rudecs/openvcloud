aa6d9a12ae27445cb2c73035ad219dd2c693e914
Jo De Boeck <deboeck.jo@gmail.com>
2016-12-22 08:47:54 +0100

    Fix listing of external network when specifiying an accountId
    See: #640

--- apps/cloudbroker/base/cloudapi__externalnetwork/methodclass/cloudapi_externalnetwork.py
+++ apps/cloudbroker/base/cloudapi__externalnetwork/methodclass/cloudapi_externalnetwork.py
@@ -4,12 +4,11 @@ from cloudbrokerlib.baseactor import BaseActor
 
 class cloudapi_externalnetwork(BaseActor):
 
-
     def list(self, accountId, **kwargs):
         """
-        result 
+        result
         """
         query = {}
         if accountId:
-            query['accountId'] = {'$in': [None, accountId]}
+            query['accountId'] = {'$in': [None, accountId, 0]}
         return self.models.externalnetwork.search({'$query': query, '$fields': ['id', 'name']})[1:]
