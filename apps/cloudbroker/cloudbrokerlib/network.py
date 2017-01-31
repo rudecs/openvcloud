c05b588d4ab4e2616d1d7822b006c76246d27cdc
Jo De Boeck <deboeck.jo@gmail.com>
2017-01-31 16:04:08 +0200

    Get and release public ips atomicly

--- apps/cloudbroker/cloudbrokerlib/network.py
+++ apps/cloudbroker/cloudbrokerlib/network.py
@@ -6,27 +6,17 @@ class Network(object):
         self.models = models
 
     def getExternalIpAddress(self, gid, externalnetworkId=None):
-        with self.models.externalnetwork.lock(str(gid)):
-            query = {'gid': gid}
-            if externalnetworkId is not None:
-                query['id'] = externalnetworkId
-            for pool in self.models.externalnetwork.search(query)[1:]:
-                if pool['ips']:
-                    pool = self.models.externalnetwork.get(pool['id'])
-                    ip = pool.ips.pop(0)
-                    self.models.externalnetwork.set(pool)
-                    return pool, netaddr.IPNetwork("%s/%s" % (ip, pool.subnetmask))
+        query = {'gid': gid}
+        if externalnetworkId is not None:
+            query['id'] = externalnetworkId
+        for pool in self.models.externalnetwork.search(query)[1:]:
+            for ip in pool['ips']:
+                res = self.models.externalnetwork.updateSearch({'id': pool['id']},
+                                                               {'$pull': {'ips': ip}})
+                if res['nModified'] == 1:
+                    return pool, netaddr.IPNetwork("%s/%s" % (ip, pool['subnetmask']))
 
     def releaseExternalIpAddress(self, externalnetworkId, ip):
         net = netaddr.IPNetwork(ip)
-        try:
-            pool = self.models.externalnetwork.get(externalnetworkId)
-        except:
-            pool = None
-        if not pool:
-            return
-        with self.models.externalnetwork.lock(str(pool.gid)):
-            ips = set(pool.ips)
-            ips.add(str(net.ip))
-            pool.ips = list(ips)
-            self.models.externalnetwork.set(pool)
+        self.models.externalnetwork.updateSearch({'id': externalnetworkId},
+                                                 {'$addToSet': {'ips': str(net.ip)}})
