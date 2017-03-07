f5beb6f0c3d12634d3770e02d01a6db978b07ecb
Jo De Boeck <deboeck.jo@gmail.com>
2017-03-07 13:27:26 +0200

    Fix keyerror in errorhandling
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- apps/cloudbroker/cloudbrokerlib/baseactor.py
+++ apps/cloudbroker/cloudbrokerlib/baseactor.py
@@ -1,9 +1,9 @@
 from . import cloudbroker
-#from JumpScale.baselib.http_client.HttpClient import HTTPError
 from JumpScale.portal.portal import exceptions
 from JumpScale.portal.portal.PortalClient2 import ApiError
 
 from JumpScale import j
+
 
 class BaseActor(object):
     def __init__(self):
@@ -15,12 +15,13 @@ class BaseActor(object):
             packagename = 'cbportal'
         self.hrd = j.atyourservice.get(name=packagename, instance='main').hrd
 
+
 def wrap_remote(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except ApiError as e:
-            ctype = e.response.headers['Content-Type'] or 'text/plain'
+            ctype = e.response.headers.get('Content-Type', 'text/plain')
             headers = [('Content-Type', ctype), ]
             statuscode = e.response.status_code or 500
             raise exceptions.BaseError(statuscode, headers, e.response.content)
