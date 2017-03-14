6ae03ddabd8a935b4dc98fec034586674bc4a1c2
Jo De Boeck <deboeck.jo@gmail.com>
2017-03-14 18:24:59 +0200

    Make it possible to update iyo secret and id
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- scripts/ops/updateiyo.py
+++ scripts/ops/updateiyo.py
@@ -2,31 +2,35 @@
 from JumpScale import j
 import argparse
 
-
-def update(clientid):
-    cbmasteraio = j.atyourservice.get(name='cb_master_aio')
-    ovciyo = j.atyourservice.get(name='ovc_itsyouonline')
+def update(clientid, ovcsecret, ovssecret):
+    j.system.fs.createDir('services/jumpscale__node.ssh__ovc_master/jumpscale__oauth_client__itsyouonline/')
+    configure = j.atyourservice.get(name='ovc_configure_aio')
+    configure.actions.prepare(configure)
+    configure.actions.copyBack('ovc_master', 'jumpscale__oauth_client__itsyouonline')
     oauth = j.atyourservice.get(name='oauth_client', instance='itsyouonline')
 
-    ovciyodict = cbmasteraio.hrd.getDict('dep.args.ovc_itsyouonline')
-    ovciyodict['param.client_id'] = clientid
-    cbmasteraio.hrd.set('dep.args.ovc_itsyouonline', ovciyodict)
-    cbmasteraio.hrd.set('instance.param.itsyouonline.client_id', clientid)
-    ovciyo.hrd.set('instance.param.client_id', clientid)
     oauth.hrd.set('instance.oauth.client.id', clientid)
     scopes = 'user:name,user:email'
     for group in ['admin', 'level1', 'level2', 'level3', 'ovs_admin', 'user']:
         scopes += ',user:memberof:{}.{}'.format(clientid, group)
-
     oauth.hrd.set('instance.oauth.client.scope', scopes)
+    if ovcsecret:
+        oauth.hrd.set('instance.oauth.client.secret', ovcsecret)
 
+    oauth.install(reinstall=True)
     portal = j.atyourservice.get(name='portal')
     portal.restart()
 
+    for service in j.atyourservice.findServices(name='openvstorage_oauth'):
+        service.hrd.set('instance.oauth.id', clientid)
+        service.hrd.set('instance.oauth.secret', ovssecret)
+        service.install(reinstall=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--clientid', required=True)
+    parser.add_argument('-s', '--ovcsecret', default=None)
+    parser.add_argument('-o', '--ovssecret', default=None)
     options = parser.parse_args()
-    update(options.clientid)
+    update(options.clientid, options.ovcsecret, options.ovssecret)
