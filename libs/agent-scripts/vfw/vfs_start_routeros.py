2663e6cb4872be17ff0f3baae200ff40eb350e38
Jo De Boeck <deboeck.jo@gmail.com>
2017-09-13 15:38:50 +0200

    Make vfs_start_routeros faster when it's already running
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- libs/agent-scripts/vfw/vfs_start_routeros.py
+++ libs/agent-scripts/vfw/vfs_start_routeros.py
@@ -16,10 +16,12 @@ queue = 'hypervisor'
 
 def action(networkid, vlan):
     import os
-    createnetwork = j.clients.redisworker.getJumpscriptFromName('greenitglobe', 'createnetwork')
-    createnetwork.executeLocal(networkid=networkid)
-    create_external_network = j.clients.redisworker.getJumpscriptFromName('greenitglobe', 'create_external_network')
-    bridgename = create_external_network.executeLocal(vlan=vlan)
+
+    def createnetwork():
+        createnetwork = j.clients.redisworker.getJumpscriptFromName('greenitglobe', 'createnetwork')
+        createnetwork.executeLocal(networkid=networkid)
+        create_external_network = j.clients.redisworker.getJumpscriptFromName('greenitglobe', 'create_external_network')
+        return create_external_network.executeLocal(vlan=vlan)
     import libvirt
     con = libvirt.open()
     try:
@@ -30,9 +32,11 @@ def action(networkid, vlan):
             if domain.state()[0] == libvirt.VIR_DOMAIN_RUNNING:
                 return True
             else:
+                createnetwork()
                 domain.create()
                 return True
         except:
+            bridgename = createnetwork()
             import jinja2
             networkidHex = '%04x' % int(networkid)
             imagedir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps/routeros/template/')
@@ -51,3 +55,12 @@ def action(networkid, vlan):
     finally:
         con.close()
     return True
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--network-id', dest='networkid', required=True)
+    parser.add_argument('-v', '--vlan', type=int, required=True)
+    args = parser.parse_args()
+    action(args.networkid, args.vlan)
