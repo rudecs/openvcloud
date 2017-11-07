2ee1b698d534c5a90df380cf7e914e4a06a12794
Jo De Boeck <deboeck.jo@gmail.com>
2017-11-07 22:24:42 +0200

    Update capacityplanning to consider reserved memory
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- libs/CloudscalerLibcloud/CloudscalerLibcloud/utils/gridconfig.py
+++ libs/CloudscalerLibcloud/CloudscalerLibcloud/utils/gridconfig.py
@@ -3,23 +3,37 @@ from psutil import virtual_memory
 
 # for thin provisioning
 TOTAL_MEM = int(virtual_memory().total >> 30)
-if TOTAL_MEM <= 64:
-    RESERVED_MEM = 1024  # 1 GB
-elif 64 < TOTAL_MEM < 196:
-    RESERVED_MEM = 2048  # 2 GB
-else:
-    RESERVED_MEM = 4096  # 4 GB
 
-class GridConfig:
 
-    def __init__(self):
-        self.scl = j.clients.osis.getNamespace('system')
+def get_reserved_memory_default(total_mem):
+    if total_mem <= 64:
+        return 1024  # 1 GB
+    elif 64 < TOTAL_MEM < 196:
+        return 2048  # 2 GB
+    else:
+        return 4096  # 4 GB
+
+RESERVED_MEM = get_reserved_memory_default(TOTAL_MEM)
+
+class GridConfig(object):
+    def __init__(self, grid=None, total_mem=int(virtual_memory().total >> 30)):
+        self._scl = None
         self.default = {
-            "reserved_mem": RESERVED_MEM,
+            "reserved_mem": get_reserved_memory_default(total_mem),
         }
-        self.gid = j.application.whoAmI.gid
-        self.grid = self.scl.grid.get(self.gid)
+        if grid is None:
+            self.gid = j.application.whoAmI.gid
+            self.grid = self.scl.grid.get(self.gid)
+        else:
+            self.gid = grid.id
+            self.grid = grid
         self.settings = self.grid.settings
+
+    @property
+    def scl(self):
+        if self._scl is None:
+            self._scl = j.clients.osis.getNamespace('system')
+        return self._scl
 
     def refresh_settings(self):
         self.grid = self.scl.grid.get(self.gid)
