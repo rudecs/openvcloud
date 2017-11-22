0740a2c5c2bc6cb81f5c150a8fc42597cb149695
Jo De Boeck <deboeck.jo@gmail.com>
2017-11-22 11:45:10 +0200

    Make version page multi location aware
    This fixes an issue when master is setup with a diffrent grid id then
    the nodes
    Fixes: #971
    
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- apps/cbportal/base/CBGrid/.macros/wiki/ovsversion/3_ovsversion.py
+++ apps/cbportal/base/CBGrid/.macros/wiki/ovsversion/3_ovsversion.py
@@ -1,14 +1,17 @@
 def main(j, args, params, tags, tasklet):
     acl = j.clients.agentcontroller.get()
-    jobinfo = acl.executeJumpscript('cloudscalers', 'ovspackages', role='storagedriver', gid=j.application.whoAmI.gid)
+    ccl = j.clients.osis.getNamespace('cloudbroker')
     wiki = []
-    if jobinfo['state'] == 'NOWORK':
-        wiki.append("Not installed")
-    elif jobinfo['state'] != 'OK':
-        wiki.append("Failed to retreive OVS version.")
-    else:
-        for name, version in jobinfo['result'].iteritems():
-            wiki.append("* %s: %s" % (name, version))
+    for location in ccl.location.search({})[1:]:
+        wiki.append('h3. OpenvStorage {}'.format(location['name']))
+        jobinfo = acl.executeJumpscript('cloudscalers', 'ovspackages', role='storagedriver', gid=location['gid'], timeout=10)
+        if jobinfo['state'] == 'NOWORK':
+            wiki.append("Not installed")
+        elif jobinfo['state'] != 'OK':
+            wiki.append("Failed to retreive OVS version.")
+        else:
+            for name, version in jobinfo['result'].iteritems():
+                wiki.append("* %s: %s" % (name, version))
 
     params.result = ('\n'.join(wiki), args.doc)
     return params
