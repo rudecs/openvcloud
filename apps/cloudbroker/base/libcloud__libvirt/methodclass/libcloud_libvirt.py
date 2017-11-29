caa305e606ad9ab4b10f4576fa8a5a833319af0b
Jo De Boeck <deboeck.jo@gmail.com>
2017-11-29 16:11:59 +0200

    Fix limit of 200 sizes (#1016)

--- apps/cloudbroker/base/libcloud__libvirt/methodclass/libcloud_libvirt.py
+++ apps/cloudbroker/base/libcloud__libvirt/methodclass/libcloud_libvirt.py
@@ -61,7 +61,7 @@ class libcloud_libvirt(object):
         List the available sizes, a size is a combination of compute capacity(memory, cpu) and the disk capacity.
         result
         """
-        sizes = self._models.size.search({})[1:]
+        sizes = self._models.size.search({}, size=0)[1:]
         return sizes
 
     def addFreeSubnet(self, subnet, networkid, **kwargs):
