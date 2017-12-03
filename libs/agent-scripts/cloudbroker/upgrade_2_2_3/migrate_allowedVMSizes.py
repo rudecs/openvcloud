fae3e23d0fd0358784507bd5ff2a11e25efc1090
alichaddad <chaddada@greenitglobe.com>
2017-12-03 14:34:06 +0200

    fix for deleting and adding allowed sizes

---
+++ libs/agent-scripts/cloudbroker/upgrade_2_2_3/migrate_allowedVMSizes.py
@@ -0,0 +1,18 @@
+from JumpScale import j
+
+descr = """
+Upgrade script
+Will update all allowedVMSizes of a cloudspace that were NULL to an empty list
+"""
+
+category = "libvirt"
+organization = "greenitglobe"
+author = "chaddada@greenitglobe.com"
+license = "bsd"
+version = "2.0"
+roles = ['master']
+async = True
+
+def action():
+    osis_cl = j.clients.osis.getNamespace('cloudbroker')
+    osis_cl.cloudspace.updateSearch({'allowedVMSizes': None}, {'$set': {'allowedVMSizes': []}})
