6a0c28f6c61d33741972feeede1890ca8991e2c5
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-07-16 18:22:07 +0200

    fix run_test script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -o pipefail
 
 TESTSUITE=${1}
 TESTS_PATH=${2}
@@ -6,14 +7,13 @@ LOGFILE="/tmp/${TESTSUITE}.log"
 
 if [[ ${TESTSUITE} == "acl" ]] || [[ ${TESTSUITE} == "ovc" ]]; then
 
-    cd tests
     export PYTHONPATH=/opt/jumpscale7/lib:/opt/jumpscale7/lib/lib-dynload/:/opt/jumpscale7/bin:/opt/jumpscale7/lib/python.zip:/opt/jumpscale7/lib/plat-x86_64-linux-gnu
     nosetests-2.7 -s -v ${TESTS_PATH} --tc-file config.ini 2>&1 | tee ${LOGFILE}
 
 elif [[ ${TESTSUITE} == "portal" ]]; then
 
-    cd tests/ovc_master_hosted/Portal
     export PYTHONPATH=./
+    cd ovc_master_hosted/Portal
     su - test -c "xvfb-run -a nosetests -s -v ${TESTS_PATH} --tc-file config.ini 2>&1 | tee ${LOGFILE}" 
 
 else
