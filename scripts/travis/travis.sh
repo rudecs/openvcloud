ac379fdfa7d690ec65f1dca46161a8424ea8ee8f
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-07-16 16:16:56 +0200

    push results after test

--- scripts/travis/travis.sh
+++ scripts/travis/travis.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
-execute(){
+function execute() {
     sshpass -p ${password} ssh -o StrictHostKeyChecking=no -t ${username}@${ipaddress} ${1}
 }
 
-echo "[+] Joining zerotier network ${zerotier_network} ..."
-sudo zerotier-cli join ${zerotier_network}; sleep 5
-memberid=$(sudo zerotier-cli info | awk '{print $3}')
-curl -s -H "Content-Type: application/json" -H "Authorization: Bearer ${zerotier_token}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${zerotier_network}/member/${memberid} > /dev/null
-sudo ifconfig "$(ls /sys/class/net | grep zt)" mtu 1280 
+function join_zerotier() {
+    sudo zerotier-cli join ${ZEROTIER_NETWORK}; sleep 5
+    memberid=$(sudo zerotier-cli info | awk '{print $3}')
+    curl -s -H "Content-Type: application/json" -H "Authorization: Bearer ${ZEROTIER_TOKEN}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${ZEROTIER_NETWORK}/member/${memberid} > /dev/null
+    sudo ifconfig "$(ls /sys/class/net | grep zt)" mtu 1280 
+    for i in {1..20}; do
+        ping -c1 ${ipaddress} > /dev/null && break
+    done
+    if [ $? -gt 0 ]; then
+        echo "Can't reach ip address ${ipaddress}"; exit 1
+    fi
+}
 
-for i in {1..20}; do
-    ping -c1 ${ipaddress} > /dev/null && break
+function run_tests() {
+    echo "Joining zerotier network ${ZEROTIER_NETWORK} ..."
+    join_zerotier 
+
+    echo "Running tests ..."
+    execute "cd /opt/code/github/0-complexity/openvcloud/tests; bash run_tests.sh ${1} ${2}"
+}
+
+function push_results() {
+    TESTSUITE=${1}
+    TIMESTAMP=$(date "+%m/%d/%y")
+    LOGFILE="/tmp/${TESTSUITE}.log"
+    REMOTE_LOGFILE="/tmp/${TESTSUITE}_${TIMESTAMP}.log"
+    
+    echo "Uploading results ..."
+    execute "mv ${LOGFILE} ${REMOTE_LOGFILE}; s3cmd --access_key=${S3_KEY} --secret_key=${S3_SECRET} --host=${S3_HOST} --no-ssl put ${REMOTE_LOGFILE} s3://${S3_LOGS_BUCKET}"
+}
+
+while true; do
+  case "$1" in
+    --run-test) run_tests ${2} ${3}; shift ;;
+    --push-result) push_results ${2}; shift ;;
+    * ) break ;;
+  esac
+  shift
 done
-
-if [ $? -gt 0 ]; then
-    echo "Can't reach ip address ${ipaddress}"; exit 1
-fi
-
-echo "Running tests ..."
-execute "cd /opt/code/github/0-complexity/openvcloud/tests; bash run_tests.sh -t ${1} -p ${2}"
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-function usage()
-{
-    echo "usage:"
-    echo "   -t: testsuite name"
-    echo "   -p: tests path"
-    exit;
-}
-
-while getopts ":t:p:" opt; do
-  case ${opt} in
-    t) TESTSUITE=$OPTARG;;
-    p) TESTS_PATH=$OPTARG;;
-    \?) usage;;
-  esac
-done
+TESTSUITE=${1}
+TESTS_PATH=${2}
+LOGFILE="/tmp/${TESTSUITE}.log"
 
 if [[ ${TESTSUITE} == "acl" ]] || [[ ${TESTSUITE} == "ovc" ]]; then
 
     cd tests
     export PYTHONPATH=/opt/jumpscale7/lib:/opt/jumpscale7/lib/lib-dynload/:/opt/jumpscale7/bin:/opt/jumpscale7/lib/python.zip:/opt/jumpscale7/lib/plat-x86_64-linux-gnu
-    nosetests-2.7 -s -v ${TESTS_PATH} --tc-file config.ini
+    nosetests-2.7 -s -v ${TESTS_PATH} --tc-file config.ini 2>&1 | tee ${LOGFILE}
 
 elif [[ ${TESTSUITE} == "portal" ]]; then
 
     cd tests/ovc_master_hosted/Portal
     export PYTHONPATH=./
-    su - test -c "xvfb-run -a nosetests -s -v ${TESTS_PATH} --tc-file config.ini" 
-    
+    su - test -c "xvfb-run -a nosetests -s -v ${TESTS_PATH} --tc-file config.ini 2>&1 | tee ${LOGFILE}" 
+
 else
-
     echo "Invalid testsuite name"
-
-fi
+fi 
